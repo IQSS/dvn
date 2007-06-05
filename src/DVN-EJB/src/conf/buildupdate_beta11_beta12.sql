931acabaf64bdcc922a79372ea85f1da064decc1
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2007-06-05 18:36:29 +0000

    Change studytopicclass value to type text

--- src/DVN-EJB/src/conf/buildupdate_beta11_beta12.sql
+++ src/DVN-EJB/src/conf/buildupdate_beta11_beta12.sql
@@ -23,3 +23,6 @@ ALTER TABLE harvestingdataverse ALTER COLUMN lastharvesttime SET STORAGE PLAIN;
 ALTER TABLE study ALTER COLUMN geographiccoverage type text;
 
 UPDATE vdcnetwork set authority='TEST';
+
+ALTER TABLE studytopicclass ALTER COLUMN value type text;
+

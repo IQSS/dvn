593802686eca71ef9ff33ae61b3248c730f5e7c4
Gustavo Durand <gdurand@hmdc.harvard.edu>
2007-06-07 19:59:35 +0000

    Made lots of changes to move study related logic from stateful bean to stateless bean and to Study constructor (stateful bean calls those as well)

--- src/DVN-EJB/src/java/edu/harvard/hmdc/vdcnet/study/EditStudyServiceBean.java
+++ src/DVN-EJB/src/java/edu/harvard/hmdc/vdcnet/study/EditStudyServiceBean.java
@@ -115,34 +115,26 @@ public class EditStudyServiceBean implements edu.harvard.hmdc.vdcnet.study.EditS
     
     public void newStudy(Long vdcId, Long userId) {
         newStudy=true;
+        
         VDC vdc = em.find(VDC.class, vdcId);
-        if (vdc==null) {
-            throw new IllegalArgumentException("Unknown VDC id: "+vdcId);
-        }
-        VDCNetwork vdcNetwork = vdcNetworkService.find();
-        
-        study = new Study();
-        em.persist(study);
-        
-        study.setOwner(vdc);
         VDCUser creator = em.find(VDCUser.class,userId);
-        study.setCreator(creator);
-        study.setCreateTime( new Date() );
-        study.setProtocol(vdcNetwork.getProtocol());
-        study.setAuthority(vdcNetwork.getAuthority());
-        
-        
-        
+        ReviewState reviewState = reviewStateService.findByName(ReviewStateServiceLocal.REVIEW_STATE_NEW);
         
         // If user is network admin, or has a vdc role > contributor, set the study to IN REVIEW
         // else (user is a contributor), set the study to NEW
         if ((creator.getNetworkRole()!=null && creator.getNetworkRole().getName().equals(NetworkRoleServiceLocal.ADMIN))
         || (creator.getVDCRole(vdc)!=null && !creator.getVDCRole(vdc).getRole().getName().equals(RoleServiceLocal.CONTRIBUTOR))) {
-            study.setReviewState(reviewStateService.findByName(ReviewStateServiceLocal.REVIEW_STATE_IN_REVIEW));
-        } else  {
-            study.setReviewState(reviewStateService.findByName(ReviewStateServiceLocal.REVIEW_STATE_NEW));
-        }
-        study.setTemplate(vdc.getDefaultTemplate());
+            reviewState = reviewStateService.findByName(ReviewStateServiceLocal.REVIEW_STATE_IN_REVIEW);
+        }          
+        
+        study = new Study(vdc, creator, reviewState);
+        em.persist(study);
+        
+        // set default protocol and authority
+        VDCNetwork vdcNetwork = vdcNetworkService.find();        
+        study.setProtocol(vdcNetwork.getProtocol());
+        study.setAuthority(vdcNetwork.getAuthority());
+        
     }
     
     public void removeCollectionElement(Collection coll, Object elem) {
@@ -180,20 +172,10 @@ public class EditStudyServiceBean implements edu.harvard.hmdc.vdcnet.study.EditS
     @Remove
     @TransactionAttribute(TransactionAttributeType.REQUIRED)
     public void save(Long vdcId, Long userId) {
+        
+        
         VDCUser user = em.find(VDCUser.class,userId);
         try {
-            // Add Study to root collection of it's VDC owner.
-            if (study.getOwner()!=null) {
-                if (!study.getStudyColls().contains(study.getOwner().getRootCollection())) {
-                    study.getStudyColls().add(study.getOwner().getRootCollection());
-                    study.getOwner().getRootCollection().getStudies().add(study);
-                }
-            }
-            study.setLastUpdateTime(new Date());
-            study.setLastUpdater(user);
-            
-            
-            
             // If user is a Contributor and is editing a study with review state "Released", then
             // revert the review state back to In Review.
             if (!isNewStudy()
@@ -212,15 +194,11 @@ public class EditStudyServiceBean implements edu.harvard.hmdc.vdcnet.study.EditS
                 editFiles();
             }
             
-            setDisplayOrders();
-            // Call flush so we can use the study primary key to call the Indexer
-            em.flush();
+            studyService.saveStudy(study, userId);
             
             
             
-            
-            
-            indexService.updateStudy(study.getId());
+            //indexService.updateStudy(study.getId());
         } catch (EJBException e) {
             System.out.println("EJBException "+e.getMessage()+" saving study "+study.getId()+" edited by " + user.getUserName() + " at "+ new Date().toString());
             e.printStackTrace();
@@ -238,85 +216,6 @@ public class EditStudyServiceBean implements edu.harvard.hmdc.vdcnet.study.EditS
         
     }
     
-    private void setDisplayOrders() {
-        int i=0;
-        for (Iterator it = study.getStudyAuthors().iterator(); it.hasNext();) {
-            StudyAuthor elem = (StudyAuthor) it.next();
-            elem.setDisplayOrder(i);
-            i++;
-            
-        }
-        i=0;
-        for (Iterator it = study.getStudyAbstracts().iterator(); it.hasNext();) {
-            StudyAbstract elem = (StudyAbstract) it.next();
-            elem.setDisplayOrder(i);
-            i++;
-            
-        }
-        i=0;
-        for (Iterator it = study.getStudyDistributors().iterator(); it.hasNext();) {
-            StudyDistributor elem = (StudyDistributor) it.next();
-            elem.setDisplayOrder(i);
-            i++;
-            
-        }
-        i=0;
-        for (Iterator it = study.getStudyGeoBoundings().iterator(); it.hasNext();) {
-            StudyGeoBounding elem = (StudyGeoBounding) it.next();
-            elem.setDisplayOrder(i);
-            i++;
-            
-        }
-        i=0;
-        for (Iterator it = study.getStudyGrants().iterator(); it.hasNext();) {
-            StudyGrant elem = (StudyGrant) it.next();
-            elem.setDisplayOrder(i);
-            i++;
-        }
-        
-        i=0;
-        for (Iterator it = study.getStudyKeywords().iterator(); it.hasNext();) {
-            StudyKeyword elem = (StudyKeyword) it.next();
-            elem.setDisplayOrder(i);
-            i++;
-        }
-        
-        i=0;
-        for (Iterator it = study.getStudyNotes().iterator(); it.hasNext();) {
-            StudyNote elem = (StudyNote) it.next();
-            elem.setDisplayOrder(i);
-            i++;
-        }
-        i=0;
-        for (Iterator it = study.getStudyOtherIds().iterator(); it.hasNext();) {
-            StudyOtherId elem = (StudyOtherId) it.next();
-            elem.setDisplayOrder(i);
-            i++;
-        }
-        i=0;
-        for (Iterator it = study.getStudyProducers().iterator(); it.hasNext();) {
-            StudyProducer elem = (StudyProducer) it.next();
-            elem.setDisplayOrder(i);
-            i++;
-        }
-        i=0;
-        for (Iterator it = study.getStudySoftware().iterator(); it.hasNext();) {
-            StudySoftware elem = (StudySoftware) it.next();
-            elem.setDisplayOrder(i);
-            i++;
-        }
-        
-        i=0;
-        for (Iterator it = study.getStudyTopicClasses().iterator(); it.hasNext();) {
-            StudyTopicClass elem = (StudyTopicClass) it.next();
-            elem.setDisplayOrder(i);
-            i++;
-        }
-        
-        
-        
-        
-    }
     
     private void addFiles(VDCUser user) {
         // step 0: start with some initialization
@@ -508,291 +407,6 @@ public class EditStudyServiceBean implements edu.harvard.hmdc.vdcnet.study.EditS
     
     public boolean isNewStudy() {
         return newStudy;
-    }
-    
-    
-    public void importHarvestStudy(File metadataFile) {
-        importStudy(metadataFile, false, false,true);
-    }
-    
-    public void importLegacyStudy(File xmlFile) {
-        importStudy(xmlFile,true, true, false);
-    }
-  /*  
-    public void importStudy( Node xmlNode, boolean checkRestrictions, boolean generateStudyId, boolean allowUpdates ) {
-        
-        // now import
-        doImportStudy( xmlNode, checkRestrictions, generateStudyId,allowUpdates );
-        
-        // last create XML file for storage
-        // create, if needed, the directory
-        File newDir = new File(FileUtil.getStudyFileDir(), study.getAuthority() + File.separator + study.getStudyId());
-        if (!newDir.exists()) {
-            newDir.mkdirs();
-        }
-        try {
-            TransformerFactory tFactory = TransformerFactory.newInstance();
-            Transformer transformer = tFactory.newTransformer();
-            
-            DOMSource source = new DOMSource(xmlNode);
-            FileOutputStream out = new FileOutputStream( new File(newDir, "original_imported_study.xml") );
-            StreamResult result = new StreamResult( out );
-            transformer.transform(source, result);
-            
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            String msg = "ImportStudy failed: ";
-            if (ex.getMessage()!=null) {
-                msg+=ex.getMessage();
-            }
-            EJBException e = new EJBException(msg );
-            e.initCause(ex);
-            throw e;
-        }
-        
-    }
-    
-    */
-    public void importStudy( File xmlFile, boolean checkRestrictions, boolean generateStudyId, boolean allowUpdates ) {
-        
-        doImportStudy( xmlFile, checkRestrictions, generateStudyId, allowUpdates );
-        
-        // lastly, copy XML file
-        try {
-            // create, if needed, the directory
-            File newDir = new File(FileUtil.getStudyFileDir(), study.getAuthority() + File.separator + study.getStudyId());
-            if (!newDir.exists()) {
-                newDir.mkdirs();
-            }
-            
-            FileUtil.copyFile( xmlFile, new File(newDir, "original_imported_study.xml"));
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            String msg = "ImportStudy failed: ";
-            if (ex.getMessage()!=null) {
-                msg+=ex.getMessage();
-            }
-            EJBException e = new EJBException(msg );
-            e.initCause(ex);
-            throw e;
-        }
-    }
-    
-    public void doImportStudy( Object obj, boolean checkRestrictions, boolean generateStudyId,  boolean allowUpdates ) {
-        CodeBook _cb = null;
-        if (obj instanceof CodeBook) {
-            _cb = (CodeBook)obj;
-        } else if (obj instanceof Node || obj instanceof File ) {
-            // first unmarshall the XML
-            try {
-                JAXBContext jc = JAXBContext.newInstance("edu.harvard.hmdc.vdcnet.jaxb.ddi20");
-                Unmarshaller unmarshaller = jc.createUnmarshaller();
-                if (obj instanceof Node) {
-                    _cb  = (CodeBook) unmarshaller.unmarshal( (Node)obj );
-                } else {
-                    Object unmarshalObj= unmarshaller.unmarshal( (File)obj );
-                    _cb  = (CodeBook)unmarshalObj; 
-                  
-                }
-            } catch(JAXBException ex) {
-                EJBException e = new EJBException("Import Study failed: "+ex.getMessage() );
-                e.initCause(ex);
-                throw e;
-            }
-            
-                   
-        } else {
-            throw new EJBException("Invalid type for parameter obj: "+obj.getClass().getName()+". obj must instance of Node or File.");
-        }
-        
-      
-        
-        
-        ddiService.mapDDI( _cb, study, allowUpdates );
-        _cb=null;
-        System.gc();
-        
-        if (study.getStudyId()==null || study.getStudyId().equals("")) {
-            if (generateStudyId) {
-                VDCNetwork vdcNetwork = vdcNetworkService.find();
-                study.setProtocol(vdcNetwork.getProtocol());
-                study.setAuthority(vdcNetwork.getAuthority());
-                study.setStudyId(studyService.generateStudyIdSequence(study.getProtocol(),study.getAuthority()));
-            } else {
-                throw new EJBException("ImportStudy failed: DDI does not specify a handle for this study.");
-            }
-        }
-        
-        // set to released
-        ReviewState released = reviewStateService.findByName(ReviewStateServiceLocal.REVIEW_STATE_RELEASED);
-        study.setReviewState(released);
-        
-        // If study comes from old VDC, set restrictions based on  restrictions in VDC repository
-        if (checkRestrictions) {
-            setImportedStudyRestrictions(study);
-        }
-    }
-    
-    @Remove
-    @TransactionAttribute(TransactionAttributeType.REQUIRED)
-    public void retrieveFilesAndSave( Long vdcId, Long userId ) {
-        try {
-            
-            // create, if needed, the directory
-            File newDir = new File(FileUtil.getStudyFileDir(), study.getAuthority() + File.separator + study.getStudyId());
-            if (!newDir.exists()) {
-                newDir.mkdirs();
-            }
-            
-            // flush to create IDs
-            em.flush();
-            
-            Iterator iter = study.getStudyFiles().iterator();
-            while (iter.hasNext()) {
-                StudyFile file = (StudyFile) iter.next();
-                File inputFile = determineLegacyFile( file );
-                file.setFileSystemName( inputFile.getName() );
-                File outputFile = new File(newDir, file.getFileSystemName() );
-                FileUtil.copyFile( inputFile, outputFile );
-                file.setFileSystemLocation( outputFile.getAbsolutePath() );
-                if (file.isSubsettable()) {
-                    file.setFileType( "text/tab-separated-values" );
-                } else {
-                    // we need to incorprate something like JHOVE to determine otherMat filetypes
-                }
-            }
-            
-            save(vdcId, userId);
-            
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            EJBException e= new EJBException("RetrieveFilesAndSave failed: " + ex.getMessage() );
-            e.initCause(ex);
-            throw e;
-        }
-    }
-    
-    
-    
-    
-    private File determineLegacyFile(StudyFile file) {
-        
-        String legacyFileDir = FileUtil.getLegacyFileDir();
-        
-        int startIndex = file.getFileSystemLocation().indexOf("/hdl:") + 5;
-        String parsedFileLocation = file.getFileSystemLocation().substring( startIndex );
-        
-        // this line is just for when testing on a windows machine!
-        parsedFileLocation = parsedFileLocation.replace('/', File.separatorChar);
-        
-        File legacyFile = new File(legacyFileDir, parsedFileLocation);
-        
-        return legacyFile;
-    }
-    
-    
-    private void setImportedStudyRestrictions(Study study) {
-        RepositoryWrapper repositoryWrapper = new RepositoryWrapper();
-        try {
-            String studyObjectHandle = study.getProtocol()+":"+study.getAuthority()+"/"+study.getStudyId();
-            if (repositoryWrapper.isObjectRestricted(studyObjectHandle)) {
-                study.setRestricted(true);
-            }
-            for (Iterator it = study.getFileCategories().iterator(); it.hasNext();) {
-                FileCategory cat = (FileCategory) it.next();
-                for (Iterator it2 = cat.getStudyFiles().iterator(); it2.hasNext();) {
-                    StudyFile studyFile = (StudyFile) it2.next();
-                    String fileSystemName = determineLegacyFile(studyFile).getName();
-                    if (repositoryWrapper.isObjectRestricted(studyObjectHandle+"/"+fileSystemName)) {
-                        studyFile.setRestricted(true);
-                    }
-                }
-                
-            }
-        } catch (IOException e) {
-            EJBException ex = new EJBException("Exception setting restrictions for study "+study.getGlobalId());
-            ex.initCause(e);
-            throw ex;
-            
-            
-        } catch (SAXException e) {
-            EJBException ex = new EJBException("Exception setting restrictions for study "+study.getGlobalId());
-            ex.initCause(e);
-            throw ex;
-            
-        }
-        
-        
-    }
-    
-    
-    private void clearStudy() {
-        // should this be done with bulk deletes??
-        
-        for (Iterator iter = study.getFileCategories().iterator(); iter.hasNext();) {
-            FileCategory elem = (FileCategory) iter.next();
-            removeCollectionElement(iter,elem);
-        }
-        for (Iterator iter = study.getStudyAbstracts().iterator(); iter.hasNext();) {
-            StudyAbstract elem = (StudyAbstract) iter.next();
-            removeCollectionElement(iter,elem);
-        }
-        for (Iterator iter = study.getStudyAuthors().iterator(); iter.hasNext();) {
-            StudyAuthor elem = (StudyAuthor) iter.next();
-            removeCollectionElement(iter,elem);
-        }
-        for (Iterator iter = study.getStudyDistributors().iterator(); iter.hasNext();) {
-            StudyDistributor elem = (StudyDistributor) iter.next();
-            removeCollectionElement(iter,elem);
-        }
-        for (Iterator iter = study.getStudyGeoBoundings().iterator(); iter.hasNext();) {
-            StudyGeoBounding elem = (StudyGeoBounding) iter.next();
-            removeCollectionElement(iter,elem);
-        }
-        for (Iterator iter = study.getStudyGrants().iterator(); iter.hasNext();) {
-            StudyGrant elem = (StudyGrant) iter.next();
-            removeCollectionElement(iter,elem);
-        }
-        for (Iterator iter = study.getStudyKeywords().iterator(); iter.hasNext();) {
-            StudyKeyword elem = (StudyKeyword) iter.next();
-            removeCollectionElement(iter,elem);
-        }
-        for (Iterator iter = study.getStudyNotes().iterator(); iter.hasNext();) {
-            StudyNote elem = (StudyNote) iter.next();
-            removeCollectionElement(iter,elem);
-        }
-        for (Iterator iter = study.getStudyOtherIds().iterator(); iter.hasNext();) {
-            StudyOtherId elem = (StudyOtherId) iter.next();
-            removeCollectionElement(iter,elem);
-        }
-        for (Iterator iter = study.getStudyOtherRefs().iterator(); iter.hasNext();) {
-            StudyOtherRef elem = (StudyOtherRef) iter.next();
-            removeCollectionElement(iter,elem);
-        }
-        for (Iterator iter = study.getStudyProducers().iterator(); iter.hasNext();) {
-            StudyProducer elem = (StudyProducer) iter.next();
-            removeCollectionElement(iter,elem);
-        }
-        for (Iterator iter = study.getStudyRelMaterials().iterator(); iter.hasNext();) {
-            StudyRelMaterial elem = (StudyRelMaterial) iter.next();
-            removeCollectionElement(iter,elem);
-        }
-        for (Iterator iter = study.getStudyRelPublications().iterator(); iter.hasNext();) {
-            StudyRelPublication elem = (StudyRelPublication) iter.next();
-            removeCollectionElement(iter,elem);
-        }
-        for (Iterator iter = study.getStudyRelStudies().iterator(); iter.hasNext();) {
-            StudyRelStudy elem = (StudyRelStudy) iter.next();
-            removeCollectionElement(iter,elem);
-        }
-        for (Iterator iter = study.getStudySoftware().iterator(); iter.hasNext();) {
-            StudySoftware elem = (StudySoftware) iter.next();
-            removeCollectionElement(iter,elem);
-        }
-        for (Iterator iter = study.getStudyTopicClasses().iterator(); iter.hasNext();) {
-            StudyTopicClass elem = (StudyTopicClass) iter.next();
-            removeCollectionElement(iter,elem);
-        }
     }
     
 }

52c80f99c59dd412a3ee2630d064fbb22f0413f4
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2007-06-08 18:09:00 +0000

    change study column datatypes from varchar(255) to text

---
+++ src/DVN-EJB/src/conf/buildupdate_beta12_beta12b.sql
@@ -0,0 +1,36 @@
+--DB Changes for Beta_12b :
+
+
+
+
+ALTER TABLE study ALTER COLUMN geographiccoverage type text;
+ALTER TABLE study ALTER COLUMN unf type text;
+ALTER TABLE study ALTER COLUMN title type text;
+ALTER TABLE study ALTER COLUMN fundingagency type text;
+ALTER TABLE study ALTER COLUMN productiondate type text;
+ALTER TABLE study ALTER COLUMN seriesinformation type text;
+ALTER TABLE study ALTER COLUMN productionplace type text;
+ALTER TABLE study ALTER COLUMN timeperiodcoveredstart type text;
+ALTER TABLE study ALTER COLUMN dateofcollectionstart type text;
+ALTER TABLE study ALTER COLUMN distributiondate type text;
+ALTER TABLE study ALTER COLUMN country type text;
+ALTER TABLE study ALTER COLUMN distributorcontact type text;
+ALTER TABLE study ALTER COLUMN geographicunit type text;
+ALTER TABLE study ALTER COLUMN distributorcontactaffiliation type text;
+ALTER TABLE study ALTER COLUMN distributorcontactemail type text;
+ALTER TABLE study ALTER COLUMN depositor type text;
+ALTER TABLE study ALTER COLUMN dateofdeposit type text;
+ALTER TABLE study ALTER COLUMN versiondate type text;
+ALTER TABLE study ALTER COLUMN subtitle type text;
+ALTER TABLE study ALTER COLUMN protocol type text;
+ALTER TABLE study ALTER COLUMN authority type text;
+ALTER TABLE study ALTER COLUMN timeperiodcoveredend type text;
+ALTER TABLE study ALTER COLUMN dateofcollectionend type text;
+ALTER TABLE study ALTER COLUMN unitofanalysis type text;
+ALTER TABLE study ALTER COLUMN kindofdata type text;
+ALTER TABLE study ALTER COLUMN seriesname type text;
+ALTER TABLE study ALTER COLUMN numberoffiles type text;
+ALTER TABLE study ALTER COLUMN studyversion type text;
+
+
+

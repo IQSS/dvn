959df2d68d36e0c767dbeb9c83996b18a57590e4
Robert Treacy <rtreacy@hmdc.harvard.edu>
2007-06-12 00:58:24 +0000

    Change email address and log studyId when lock occurs

--- src/DVN-EJB/src/java/edu/harvard/hmdc/vdcnet/index/IndexMessage.java
+++ src/DVN-EJB/src/java/edu/harvard/hmdc/vdcnet/index/IndexMessage.java
@@ -25,13 +25,13 @@ import javax.jms.ObjectMessage;
 
 /**
  * Entity class IndexMessage
- * 
+ *
  * @author roberttreacy
  */
 @MessageDriven(mappedName = "jms/IndexMessage", activationConfig =  {
-        @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge"),
-        @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue")
-    })
+    @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge"),
+    @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue")
+})
 public class IndexMessage implements MessageListener {
     @EJB StudyServiceLocal studyService;
 //    @EJB VDCServiceLocal vdcService;
@@ -40,7 +40,7 @@ public class IndexMessage implements MessageListener {
     /** Creates a new instance of IndexMessage */
     public IndexMessage() {
     }
-
+    
     public void onMessage(Message message) {
         IndexEdit operation = null;
         
@@ -50,11 +50,11 @@ public class IndexMessage implements MessageListener {
             operation = (IndexEdit) om.getObject();
             switch (operation.getOperation()){
                 case ADD:
-                   indexStudy(operation.getStudyId());
-                   break;
+                    indexStudy(operation.getStudyId());
+                    break;
                 case UPDATE:
-                   updateStudy(operation.getStudyId());
-                   break;
+                    updateStudy(operation.getStudyId());
+                    break;
                 case DELETE:
                     deleteStudy(operation.getStudyId());
                     break;
@@ -62,10 +62,10 @@ public class IndexMessage implements MessageListener {
             }
         } catch (JMSException ex) {
             ex.printStackTrace(); // error in getting object from message; can't send e-mail
-
+            
         } catch (Exception ex) { // we catch any exception that may have happened during the process
             ex.printStackTrace();
-         }             
+        }
     }
     
     private void indexStudy(long studyId){
@@ -77,21 +77,25 @@ public class IndexMessage implements MessageListener {
         indexer.deleteDocument(studyId);
         addDocument(studyId);
     }
-
+    
     private void deleteStudy(long studyId){
         Indexer indexer = Indexer.getInstance();
         indexer.deleteDocument(studyId);
-     }
-
+    }
+    
     private void addDocument(final long studyId) {
-        Study study = studyService.getStudy(studyId);    
+        Study study = studyService.getStudy(studyId);
         Indexer indexer = Indexer.getInstance();
+        String indexAdminMail = System.getProperty("dvn.indexadmin");
+        if (indexAdminMail == null){
+            indexAdminMail = "dataverse@lists.hmdc.harvard.edu";
+        }
         try {
             indexer.addDocument(study);
         } catch (IOException ex) {
             ex.printStackTrace();
             try {
-                mailService.sendDoNotReplyMail("vdc-dev@lists.hmdc.harvard.edu","IO problem", "Check index write lock "+InetAddress.getLocalHost().getHostAddress());
+                mailService.sendDoNotReplyMail(indexAdminMail,"IO problem", "Check index write lock "+InetAddress.getLocalHost().getHostAddress() + " , studyId " + studyId);
             } catch (UnknownHostException u) {
                 u.printStackTrace();
             }

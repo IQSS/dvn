23f037db21e647b483fa86513d064cfc78f7c2d3
Gustavo Durand <gdurand@hmdc.harvard.edu>
2007-07-02 14:58:28 +0000

    added mif support

--- src/DVN-EJB/src/java/edu/harvard/hmdc/vdcnet/study/StudyServiceLocal.java
+++ src/DVN-EJB/src/java/edu/harvard/hmdc/vdcnet/study/StudyServiceLocal.java
@@ -74,9 +74,9 @@ public interface StudyServiceLocal {
 
     edu.harvard.hmdc.vdcnet.study.Study importLegacyStudy(File xmlFile, Long vdcId, Long userId);
     edu.harvard.hmdc.vdcnet.study.Study importHarvestStudy(File xmlFile, Long vdcId, Long userId);
-    edu.harvard.hmdc.vdcnet.study.Study importStudy(File xmlFile, Long vdcId, Long userId, boolean registerHandle, boolean genegenerateHandle, boolean allowUpdates, boolean checkRestrictions, boolean retrieveFiles);
+    edu.harvard.hmdc.vdcnet.study.Study importStudy(File xmlFile,  int xmFileFormatId, Long vdcId, Long userId, boolean registerHandle, boolean genegenerateHandle, boolean allowUpdates, boolean checkRestrictions, boolean retrieveFiles);
 
-    edu.harvard.hmdc.vdcnet.study.Study doImportStudy(File xmlFile, Long vdcId, Long userId, boolean registerHandle, boolean genegenerateHandle, boolean allowUpdates, boolean checkRestrictions, boolean retrieveFiles);
+    edu.harvard.hmdc.vdcnet.study.Study doImportStudy(File xmlFile, int xmFileFormatId, Long vdcId, Long userId, boolean registerHandle, boolean genegenerateHandle, boolean allowUpdates, boolean checkRestrictions, boolean retrieveFiles);
 
 
     

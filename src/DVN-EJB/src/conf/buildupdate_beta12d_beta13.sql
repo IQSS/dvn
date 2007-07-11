c5391c935f414f47616bad052b8d50755aa8bd4f
Gustavo Durand <gdurand@hmdc.harvard.edu>
2007-07-11 23:23:16 +0000

    added harvestHoldings column

--- src/DVN-EJB/src/conf/buildupdate_beta12d_beta13.sql
+++ src/DVN-EJB/src/conf/buildupdate_beta12d_beta13.sql
@@ -3,3 +3,6 @@
 
 ALTER TABLE filecategory ALTER COLUMN name type text;
 
+ALTER TABLE study ADD COLUMN harvestsource varchar(255);
+ALTER TABLE study ALTER COLUMN harvestsource SET STORAGE EXTENDED;
+

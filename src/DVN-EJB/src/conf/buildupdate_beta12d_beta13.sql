607e7e279919d40c071f456df9d035fb5befe709
Gustavo Durand <gdurand@hmdc.harvard.edu>
2007-07-11 23:23:50 +0000

    added harvestHoldings column (wrong name first time around)

--- src/DVN-EJB/src/conf/buildupdate_beta12d_beta13.sql
+++ src/DVN-EJB/src/conf/buildupdate_beta12d_beta13.sql
@@ -3,6 +3,6 @@
 
 ALTER TABLE filecategory ALTER COLUMN name type text;
 
-ALTER TABLE study ADD COLUMN harvestsource varchar(255);
-ALTER TABLE study ALTER COLUMN harvestsource SET STORAGE EXTENDED;
+ALTER TABLE study ADD COLUMN harvestHoldings varchar(255);
+ALTER TABLE study ALTER COLUMN harvestHoldings SET STORAGE EXTENDED;
 

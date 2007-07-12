63733453981ff4e88fc1e44996141d029a298b87
Gustavo Durand <gdurand@hmdc.harvard.edu>
2007-07-12 20:34:56 +0000

    added harvestIdentifier

--- src/DVN-EJB/src/conf/buildupdate_beta12d_beta13.sql
+++ src/DVN-EJB/src/conf/buildupdate_beta12d_beta13.sql
@@ -6,3 +6,5 @@ ALTER TABLE filecategory ALTER COLUMN name type text;
 ALTER TABLE study ADD COLUMN harvestHoldings varchar(255);
 ALTER TABLE study ALTER COLUMN harvestHoldings SET STORAGE EXTENDED;
 
+ALTER TABLE study ADD COLUMN harvestIdentifier varchar(255);
+ALTER TABLE study ALTER COLUMN harvestIdentifier SET STORAGE EXTENDED;

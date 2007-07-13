a248065c252c59f316af2194964b0aeaa705a31c
Robert Treacy <rtreacy@hmdc.harvard.edu>
2007-07-13 15:06:35 +0000

    RT 49075 - changes to lucene query broke variable search

--- src/DVN-EJB/src/java/edu/harvard/hmdc/vdcnet/index/IndexServiceBean.java
+++ src/DVN-EJB/src/java/edu/harvard/hmdc/vdcnet/index/IndexServiceBean.java
@@ -246,7 +246,7 @@ public class IndexServiceBean implements edu.harvard.hmdc.vdcnet.index.IndexServ
     }
     
     public List searchVariables(VDC vdc,SearchTerm searchTerm){
-        List studyIds = listVdcStudyIds(vdc);
+        List studyIds = vdc != null ? listVdcStudyIds(vdc) : null;
 
         Indexer indexer = Indexer.getInstance();
         List matchingStudyIds = null;

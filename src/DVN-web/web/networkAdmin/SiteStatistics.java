799f9690b3785f73406c6b08d5d4db723c625b55
Wendy Bossons <dvn_support@help.hmdc.harvard.edu>
2007-07-30 16:19:36 +0000

    separate report writing from the site statistics page backing bean

---
+++ src/DVN-web/web/networkAdmin/SiteStatistics.java
@@ -0,0 +1,140 @@
+/*
+ * SiteStatistics.java
+ *
+ * Created on Jul 20, 2007, 2:26:01 PM
+ *
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+/**
+ *
+ * @author wbossons
+ */
+
+public class SiteStatistics extends ReportConstants {
+
+	private int startMonth = 7; // the default start month is July
+	private int endMonth   = 6; // the default end month is June.
+
+    /** Creates a new instance of SiteStatistics */
+    public SiteStatistics() {
+    }
+
+    /**
+     * Use this to kick off reports
+     * from outside the UI, e.g.
+     *  the command line
+     *
+     *
+     */
+    public static void main(String[] args)
+			throws java.io.IOException {
+		int months = Integer.parseInt(args[0]);
+		ReportWriter reportwriter = new ReportWriter(months, args[1]);
+		reportwriter.writeMitReport();
+        //writeMitReport(args[0]);
+    }
+
+    public void writeReport(String dataverse) {
+        // this is blank for now
+    }
+
+
+    private String category;
+
+    private String getCategory() {
+        return this.category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    private int count;
+
+    public int getCount() {
+        return this.count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    private int visits;
+
+    public int getVisits() {
+        return this.visits;
+    }
+
+    public void setVisits(int totalvisits) {
+        this.visits = totalvisits;
+    }
+
+    private int uniqueVisits;
+
+    public int getUniqueVisits() {
+        return this.uniqueVisits;
+    }
+
+    public void setUniqueVisits(int totalUniqueVisits) {
+        this.uniqueVisits = totalUniqueVisits;
+    }
+
+    private int downloads;
+
+    public int getDownloads() {
+        return this.downloads;
+    }
+
+    public void setDownloads(int downloads) {
+        this.downloads = downloads;
+    }
+
+    /**
+     * parsed from the access log
+     *
+     */
+    private int uniqueDownloadIps;
+
+    public int getUniqueDownloadIps() {
+        //parsed from the access log
+        return this.uniqueDownloadIps;
+    }
+
+    public void setUniqueDownloadIps(int unique) {
+        this.uniqueDownloadIps = unique;
+    }
+
+    private int subsetAnalysis;
+
+    private int getSubsetAnalysis() {
+        return this.subsetAnalysis;
+    }
+
+    public void setSubsetAnalysis(int numsubsets) {
+        this.subsetAnalysis = numsubsets;
+    }
+
+    /**
+     *  parsed from the access log
+     * this is probably a list or array, not an int
+     */
+    private int subsetAnalysisIps;
+
+    public int getSubsetAnalysisIps() {
+        return this.subsetAnalysisIps;
+    }
+
+    public void setSubsetAnalysisIps(int numsubsets) {
+        this.subsetAnalysisIps = numsubsets;
+    }
+
+
+}

7b5d1839978e3f51604e16bc8f755356e5931323
Leonid Andreev <leonid@hmdc.harvard.edu>
2007-08-09 15:46:29 +0000

    a little cleanup for the R libraries installation framework, to produce better logging output when the rpm is installed.

--- src/DSB/installR.sh
+++ src/DSB/installR.sh
@@ -2,26 +2,18 @@
 
 installRmodules()
 {
-    export DISPLAY="";
+    export DISPLAY=""
+
+    LOG="/tmp/RINSTALL.$$.LOG";
     for RPACK in `cat ../etc/Rmodules.txt`
     do
-        LOG="/tmp/${RPACK}.$$";
-        echo 'Sys.putenv("DISPLAY"=NULL); install.packages("'${RPACK}'", repos="http://up2date.hmdc.harvard.edu/CRAN",dependencies=T, lib=.libPaths()[length(.libPaths())] )' | R --vanilla --slave >${LOG} 2>&1
-        ERRORS=`grep -ci -e '^ERROR' -e "No package \"${RPACK}\" on CRAN" ${LOG}`;
-        if [ $ERRORS -ne 0 ]; then
-            echo -e "\a*** We were unable to install one of the required R packages (${RPACK}).";
-            echo -e "*** This will likely cause problems later.\n\n";
-            echo -e "Install output:\n";
-            cat ${LOG};
-            echo "-----------------------------------------------------------------------";
-        fi
-#        rm -f ${LOG};
+        echo 'install.packages("'${RPACK}'", repos="http://up2date.hmdc.harvard.edu/CRAN",dependencies=T, lib=.libPaths()[length(.libPaths())] )' | R --vanilla --slave 2>&1 | tee -a ${LOG}
     done
 
     # post-Zelig installs
+
     echo '
 library(VDCutil)
-Sys.putenv("DISPLAY"=NULL)
 deps= zeligModelDependencies(inZeligOnly=T, schemaVersion="1.1", uninstalledOnly=T, repos="http://up2date.hmdc.harvard.edu/CRAN")
 if (!is.null(deps)){
         for (i in 1:dim(deps)[1]) {
@@ -29,11 +21,10 @@ if (!is.null(deps)){
                 install.packages(deps[i,1],repos=tmpCran,
                         dependencies=T, lib=.libPaths()[length(.libPaths())] )
         }
-}' | R --slave --vanilla 2>&1 | egrep "(Error)|(Warning)"
+}' | R --slave --vanilla 2>&1 | tee -a ${LOG}
+
 
 }
 
 
 installRmodules;
-
-

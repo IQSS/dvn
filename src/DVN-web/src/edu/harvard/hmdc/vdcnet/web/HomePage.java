bee21ad4c87e372fa28f8304f6d221636ca62c50
Gustavo Durand <gdurand@hmdc.harvard.edu>
2007-08-29 17:05:42 +0000

    home page no longer expands all collections for tree

--- src/DVN-web/src/edu/harvard/hmdc/vdcnet/web/HomePage.java
+++ src/DVN-web/src/edu/harvard/hmdc/vdcnet/web/HomePage.java
@@ -100,7 +100,7 @@ public class HomePage extends VDCBaseBean{
     
     /**
      * Getter for property collectionTree.
-     * @return Value of property collectionTree.
+     * @return Value of property collectionTree.m
      */
     public Tree getCollectionTree() {
         if (collectionTree == null) {

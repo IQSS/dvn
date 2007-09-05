600233fc2b48e96a786e2e6c2e06cfbe01ef4c26
Wendy Bossons <dvn_support@help.hmdc.harvard.edu>
2007-09-05 21:02:51 +0000

    52239 -- Email field now accepts more than one email. The email addresses must be comma separated, no space following the comma.

--- src/DVN-web/src/edu/harvard/hmdc/vdcnet/util/EmailValidator.java
+++ src/DVN-web/src/edu/harvard/hmdc/vdcnet/util/EmailValidator.java
@@ -36,85 +36,90 @@ public class EmailValidator implements Validator {
     public void validate(FacesContext context, UIComponent component, 
                         Object value) {
         if (value == null) return;
-        String email = (String) value;
-            if (!validateEmail(email) ) {
-                logger.info("Validation Error:  An invalid email was found.");
-                FacesMessage message = new FacesMessage(msg);
-                message.setSeverity(FacesMessage.SEVERITY_ERROR);
-                ((UIInput)component).setValid(false);
-                context.addMessage(component.getClientId(context), message);
-                throw new ValidatorException(message);
-            }
+        String email        = (String)value;
+        if (!validateEmail(email) ) {
+            logger.info("Validation Error:  An invalid email was found.");
+            FacesMessage message = new FacesMessage(msg);
+            message.setSeverity(FacesMessage.SEVERITY_ERROR);
+            ((UIInput)component).setValid(false);
+            context.addMessage(component.getClientId(context), message);
+            throw new ValidatorException(message);
+        }
     }
     
     
 
     public boolean validateEmail (String email) {
         boolean isValid = true;
-        String input = email;
-      if (email.indexOf('@') == -1) {
-          isValid = false;
-          msg += "  A valid email address must contain \"@\".";
-          return isValid;
-      } 
-      //Checks for email addresses starting with
-      //inappropriate symbols like dots or @ signs.
-      Pattern p = Pattern.compile("^\\.|^\\@");
-      Matcher m = p.matcher(input);
-      if (m.find()) {
-          isValid = false;
-          msg += "  An email address cannot start" +
-                            " with \".\" or \"@\".";
-          return isValid;
-      }
-      //check to see if there is something after the @
-      p = Pattern.compile("\\@$");
-      m = p.matcher(input);
-      if (m.find()) {
-          isValid = false;
-          msg += " An email address cannot end with @.";
-          return isValid;
-      }
-      
-      //check to see if the domain part of the email address matches the rfc for email addresses
-      p = Pattern.compile("\\@[A-Za-z0-9\\-]+\\.[A-Za-z0-9\\-]+");
-      m = p.matcher(input);
-      if (!m.find()) {
-          isValid = false;
-          msg += "Please check the email address domain. It appears to be incorrect.";
-          return isValid;
-      }
-      //Checks for email addresses that start with
-      //www. and prints a message if it does.
-      p = Pattern.compile("^www\\.");
-      m = p.matcher(input);
-      if (m.find()) {
-          isValid = false;
-          msg += "  An email address cannot start with " +
-                " with \"www.\".";
-          return isValid;
-      }
-      //now check for invalid chars in the local part of the email address
-      p = Pattern.compile("[^A-Za-z0-9\\.\\@_\\-~#]+");
-      m = p.matcher(input);
-      StringBuffer sb = new StringBuffer();
-      boolean result = m.find();
-      boolean deletedIllegalChars = false;
+        email = email.replace(" ", "");
+        String[] input = email.split(",");
+        int i = 0;
+        while (i < input.length) {
+          if (input[i].indexOf('@') == -1) {
+              isValid = false;
+              msg += "  A valid email address must contain \"@\".";
+              return isValid;
+          } 
+          //Checks for email addresses starting with
+          //inappropriate symbols like dots or @ signs.
+          Pattern p = Pattern.compile("^\\.|^\\@");
+          Matcher m = p.matcher(input[i]);
+          if (m.find()) {
+              isValid = false;
+              msg += "  An email address cannot start" +
+                                " with \".\" or \"@\".";
+              return isValid;
+          }
+          //check to see if there is something after the @
+          p = Pattern.compile("\\@$");
+          m = p.matcher(input[i]);
+          if (m.find()) {
+              isValid = false;
+              msg += " An email address cannot end with @.";
+              return isValid;
+          }
 
-      while(result) {
-         deletedIllegalChars = true;
-         m.appendReplacement(sb, "");
-         result = m.find();
-      }
-      // Add the last segment of input to the new String
-      m.appendTail(sb);
-      input = sb.toString();
+          //check to see if the domain part of the email address matches the rfc for email addresses
+          p = Pattern.compile("\\@[A-Za-z0-9\\-]+\\.[A-Za-z0-9\\-]+");
+          m = p.matcher(input[i]);
+          if (!m.find()) {
+              isValid = false;
+              msg += "Please check the email address domain. It appears to be incorrect.";
+              return isValid;
+          }
+          //Checks for email addresses that start with
+          //www. and prints a message if it does.
+          p = Pattern.compile("^www\\.");
+          m = p.matcher(input[i]);
+          if (m.find()) {
+              isValid = false;
+              msg += "  An email address cannot start with " +
+                    " with \"www.\".";
+              return isValid;
+          }
+          //now check for invalid chars in the local part of the email address
+          p = Pattern.compile("[^A-Za-z0-9\\.\\@_\\-~#]+");
+          m = p.matcher(input[i]);
+          StringBuffer sb = new StringBuffer();
+          boolean result = m.find();
+          boolean deletedIllegalChars = false;
 
-      if (deletedIllegalChars) {
-          isValid = false;
-          msg += "  Invalid characters were found" +
-                           ".  Check for invalid characters or spaces at the end of the email address.";
-      }
+          while(result) {
+             deletedIllegalChars = true;
+             m.appendReplacement(sb, "");
+             result = m.find();
+          }
+          // Add the last segment of input to the new String
+          m.appendTail(sb);
+          String appendInput = sb.toString();
+
+          if (deletedIllegalChars) {
+              isValid = false;
+              msg += "  Invalid characters were found" +
+                               ".  Check for invalid characters or spaces at the end of the email address.";
+          }
+          i++;
+        }
       return isValid;
     }
 }

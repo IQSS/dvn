c59f8bb38a0b27ab2f37cf5b1a7d1f1ceb309a66
Merce Crosas <mcrosas@iq.harvard.edu>
2007-09-17 14:56:13 +0000

    Add GNU Affero license statement

--- src/DVN-web/src/edu/harvard/hmdc/vdcnet/web/subsetting/AdvancedStatGUIdata.java
+++ src/DVN-web/src/edu/harvard/hmdc/vdcnet/web/subsetting/AdvancedStatGUIdata.java
@@ -1,7 +1,27 @@
+/*
+ * Dataverse Network - A web application to distribute, share and analyze quantitative data.
+ * Copyright (C) 2007
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses
+ * or write to the Free Software Foundation,Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
 /**
-
-AdvancedStatGUIdata.java
-
+*
+* AdvancedStatGUIdata.java
+*
 */
 
 package edu.harvard.hmdc.vdcnet.web.subsetting;
@@ -13,8 +33,8 @@ import edu.harvard.hmdc.vdcnet.dsb.zelig.*;
 import java.util.logging.Logger;
 
 /*
-  AdvancedStatGUIdata
-
+*  AdvancedStatGUIdata
+*
 */
 public class AdvancedStatGUIdata {
     // static fields

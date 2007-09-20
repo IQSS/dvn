6710765ecd725e133377d10fa525829834abbf43
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2007-09-20 16:24:16 +0000

    add table for ejb_timer

--- src/DVN-EJB/src/conf/referenceData.sql
+++ src/DVN-EJB/src/conf/referenceData.sql
@@ -916,6 +916,25 @@ CREATE SEQUENCE filesystemname_seq
 ALTER TABLE filesystemname_seq OWNER TO "vdcApp";
 
 
+CREATE TABLE ejb__timer__tbl
+(
+  creationtimeraw numeric(19) NOT NULL,
+  blob bytea,
+  timerid varchar(256) NOT NULL,
+  containerid numeric(19) NOT NULL,
+  ownerid varchar(256),
+  state int4 NOT NULL,
+  pkhashcode int4 NOT NULL,
+  intervalduration numeric(19) NOT NULL,
+  initialexpirationraw numeric(19) NOT NULL,
+  lastexpirationraw numeric(19) NOT NULL,
+  CONSTRAINT pk_ejb__timer__tbl PRIMARY KEY (timerid)
+) 
+WITHOUT OIDS;
+ALTER TABLE ejb__timer__tbl OWNER TO "vdcApp";
+
+
+
 create index datavariable_id_index on  datavariable (id);
 create index summarystatistic_id_index on  summarystatistic (id);
 create index summarystatistic_datavariable_id_index on  summarystatistic (datavariable_id);

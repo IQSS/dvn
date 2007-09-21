db77b02154fb52473367d73bb0d11e2fdc32d170
Robert Treacy <rtreacy@hmdc.harvard.edu>
2007-09-21 03:06:37 +0000

    start on version page

--- src/DVN-web/src/edu/harvard/hmdc/vdcnet/web/VersionPage.java
+++ src/DVN-web/src/edu/harvard/hmdc/vdcnet/web/VersionPage.java
@@ -19,7 +19,7 @@ import javax.ejb.EJB;
  * @author roberttreacy
  */
 public class VersionPage extends VDCBaseBean {
-    @EJB DVNVersionServiceLocal dvnVersionS dvnVersionService;
+    @EJB DVNVersionServiceLocal dvnVersionService;
     
     public void init() {
         super.init();

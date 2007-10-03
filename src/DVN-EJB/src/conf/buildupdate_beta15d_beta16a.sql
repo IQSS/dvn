1d49514e770d577a0cb08d61c5b0a5e1b3fa274b
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2007-10-03 16:53:12 +0000

    new columns for export configuration

---
+++ src/DVN-EJB/src/conf/buildupdate_beta15d_beta16a.sql
@@ -0,0 +1,12 @@
+ALTER TABLE vdcnetwork ADD COLUMN exporthourofday int4;
+ALTER TABLE vdcnetwork ALTER COLUMN exporthourofday SET STORAGE PLAIN;
+
+ALTER TABLE vdcnetwork ADD COLUMN exportdayofweek int4;
+ALTER TABLE vdcnetwork ALTER COLUMN exportdayofweek SET STORAGE PLAIN;
+
+ALTER TABLE vdcnetwork ADD COLUMN exportperiod varchar(255);
+ALTER TABLE vdcnetwork ALTER COLUMN exportperiod SET STORAGE EXTENDED;
+
+INSERT INTO pagedef ( name, path, role_id, networkrole_id ) VALUES ( 'EditExportSchedulePage', '/networkAdmin/EditExportSchedulePage.jsp', null,2 );
+
+

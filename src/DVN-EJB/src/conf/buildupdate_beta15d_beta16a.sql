f8ede4f4d7b662fb6046427f673a4e2bbf823b43
Wendy Bossons <dvn_support@help.hmdc.harvard.edu>
2007-10-09 13:43:25 +0000

    SQL updates required for my changes to the network home page.

--- src/DVN-EJB/src/conf/buildupdate_beta15d_beta16a.sql
+++ src/DVN-EJB/src/conf/buildupdate_beta15d_beta16a.sql
@@ -12,5 +12,24 @@ INSERT INTO pagedef ( name, path, role_id, networkrole_id ) VALUES ( 'EditExport
 ALTER TABLE study ADD COLUMN lastexporttime timestamp;
 ALTER TABLE study ALTER COLUMN lastexporttime SET STORAGE PLAIN;
 
+-- Column: dtype
+-- ALTER TABLE vdc DROP COLUMN dtype;
+ALTER TABLE vdc ADD COLUMN dtype varchar(31);
+ALTER TABLE vdc ALTER COLUMN dtype SET STORAGE EXTENDED;
 
+-- Column: lastname
+-- ALTER TABLE vdc DROP COLUMN lastname;
+ALTER TABLE vdc ADD COLUMN lastname varchar(255);
+ALTER TABLE vdc ALTER COLUMN lastname SET STORAGE EXTENDED;
+
+-- Column: firstname
+-- ALTER TABLE vdc DROP COLUMN firstname;
+ALTER TABLE vdc ADD COLUMN firstname varchar(255);
+ALTER TABLE vdc ALTER COLUMN firstname SET STORAGE EXTENDED;
+
+-- Column: affiliation
+-- ALTER TABLE vdc DROP COLUMN affiliation;
+
+ALTER TABLE vdc ADD COLUMN affiliation varchar(255);
+ALTER TABLE vdc ALTER COLUMN affiliation SET STORAGE EXTENDED;
 

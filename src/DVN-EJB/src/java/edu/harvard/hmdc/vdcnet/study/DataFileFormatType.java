21c803288c9d08088c6c720f6ac326ce4214630a
Gustavo Durand <gdurand@hmdc.harvard.edu>
2007-10-15 19:41:57 +0000

    added originalFileType

--- src/DVN-EJB/src/java/edu/harvard/hmdc/vdcnet/study/DataFileFormatType.java
+++ src/DVN-EJB/src/java/edu/harvard/hmdc/vdcnet/study/DataFileFormatType.java
@@ -31,8 +31,6 @@ package edu.harvard.hmdc.vdcnet.study;
 
 import java.io.Serializable;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 /**
@@ -41,6 +39,9 @@ import javax.persistence.Id;
  */
 @Entity
 public class DataFileFormatType implements Serializable {
+    
+    public static final String ORIGINAL_FILE_DATA_FILE_FORMAT = "_original";
+    
     private static final long serialVersionUID = 1L;
     private Long id;
 

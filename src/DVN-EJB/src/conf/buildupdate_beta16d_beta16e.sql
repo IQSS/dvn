b6d1956540c6afbf8aa92782dd586ce0a1801387
Gustavo Durand <gdurand@hmdc.harvard.edu>
2007-10-24 17:04:54 +0000

    added updates for old deata (and moved order around)

--- src/DVN-EJB/src/conf/buildupdate_beta16d_beta16e.sql
+++ src/DVN-EJB/src/conf/buildupdate_beta16d_beta16e.sql
@@ -1,6 +1,10 @@
-ALTER TABLE deletedstudy DROP COLUMN authority;
-ALTER TABLE deletedstudy DROP COLUMN studyid;
 ALTER TABLE deletedstudy ADD COLUMN globalid varchar(255);
 
 ALTER TABLE deletedstudy ADD COLUMN deletedtime timestamp;
 ALTER TABLE deletedstudy ALTER COLUMN deletedtime SET STORAGE PLAIN;
+
+update deletedstudy set globalid = 'hdl:' || authority  || '/' || studyId;
+update deletedstudy set deletedtime = current_timestamp(3);
+
+ALTER TABLE deletedstudy DROP COLUMN authority;
+ALTER TABLE deletedstudy DROP COLUMN studyid;

e5180f81e69b0dfb1ffa72c63d9a00ab5ebcff88
Wendy Bossons <dvn_support@help.hmdc.harvard.edu>
2007-12-08 00:09:07 +0000

    Changes to support tabs in DataList component.

--- src/DVN-web/src/edu/harvard/hmdc/vdcnet/web/component/DataList.java
+++ src/DVN-web/src/edu/harvard/hmdc/vdcnet/web/component/DataList.java
@@ -29,7 +29,9 @@
 
 package edu.harvard.hmdc.vdcnet.web.component;
 
+import com.sun.rave.web.ui.component.Hyperlink;
 import java.io.IOException;
+import java.lang.String;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
@@ -40,12 +42,19 @@ import javax.el.ExpressionFactory;
 import javax.el.MethodExpression;
 import javax.faces.FacesException;
 import javax.faces.application.Application;
+import javax.faces.component.UIColumn;
 import javax.faces.component.UIComponentBase;
+import javax.faces.component.UIOutput;
+import javax.faces.component.html.HtmlGraphicImage;
 import javax.faces.component.html.HtmlOutputText;
+import javax.faces.component.html.HtmlPanelGrid;
+import javax.faces.component.html.HtmlPanelGroup;
 import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseWriter;
+import javax.faces.el.ValueBinding;
 import javax.faces.event.ActionEvent;
 import javax.faces.event.MethodExpressionActionListener;
+import javax.servlet.http.HttpServletRequest;
 import org.ajax4jsf.ajax.html.HtmlAjaxCommandLink;
 
 /**
@@ -62,41 +71,146 @@ public class DataList extends UIComponentBase {
         return null;
     }
 
+    @Override
+    public boolean getRendersChildren() {
+        return true;
+    }
+
     public void encodeBegin(FacesContext facescontext)
                             throws IOException {
         ResponseWriter writer = facescontext.getResponseWriter();
+        writer.startElement("div", this);
+        writer.writeAttribute("class", "TabGrp", null);
+        writer.startElement("a", this);
+        writer.writeAttribute("id", "tabSet1skipLink_skipHyperlinkId", null);
+        writer.writeAttribute("href", "#tabSet1skipLink_skipHyperlinkId", null);
+        writer.startElement("img", this);
+        writer.writeAttribute("id", "tabSet1skipLink_skipHyperlinkId_image", null);
+        writer.writeAttribute("src", "/dvn/theme/com/sun/rave/web/ui/defaulttheme/images/other/dot.gif", null);
+        writer.writeAttribute("alt", "Jump Over Tab Navigation Area. Current selection is ", null);
+        writer.writeAttribute("border", "0", null);
+        writer.endElement("img");
+        writer.endElement("a");
+    }
+
+    public void encodeChildren(FacesContext facescontext)
+        throws IOException {
+        String tab = (String)getAttributes().get("tab");
+        Map tabsMap = (Map)getAttributes().get("tabs");
+        Set tabsKeys = tabsMap.keySet();
+        Iterator tabsIterator = tabsKeys.iterator();
+        String[] tabNames = new String[tabsMap.size()];
+        for (int i = 0; i < tabsMap.size(); i++){
+          String key = tabsIterator.next().toString();
+          Object object = tabsMap.get(key);
+          tabNames[i] = (String)tabsMap.get(key);
+        }
+        
+        formatTabs(tabNames, tab);
+        ResponseWriter writer = FacesContext.getCurrentInstance().getResponseWriter();
+        writer.startElement("div", this);
+        writer.writeAttribute("class", "TabGrpBox", null);
         Map map = (Map)getAttributes().get("contents");
         Set keys = map.keySet();
+        
         Iterator iterator = keys.iterator();
         for (int i = 0; i < map.size(); i++){
           String key = iterator.next().toString();
           Object object = map.get(key);
           List<DataListing> datalistings = (List<DataListing>)map.get(key);
-          formatHeading(key);
-          formatChildTable(datalistings, key);
+          if (datalistings.isEmpty()) {
+              formatMessage("There are no dataverses to display.");
+              break;
+          } else {
+            formatHeading(key);
+            formatChildTable(datalistings, key);
+          }
         }
-    }
-
-    public void encodeChildren(FacesContext facescontext)
-        throws IOException {
-        //Nothing to do
-
+        
+        writer.endElement("div");
     }
 
     public void encodeEnd(FacesContext facescontext)
         throws IOException {
-        // nothing to do
+        ResponseWriter writer = FacesContext.getCurrentInstance().getResponseWriter();
+        writer.endElement("div");
+    }
+    
+    private void formatTabs(String[] tabNames, String tab) {
+        /** write this out
+         *  because there is some kind of bug where this
+         *  is not formatting the table correctly when written
+         *  similarly to the formatChild method
+         */
+        ResponseWriter writer = FacesContext.getCurrentInstance().getResponseWriter();
+        try {
+        writer.startElement("div", this);
+        writer.writeAttribute("class", "MniTabDiv", null);
+        writer.startElement("table", this);
+        writer.writeAttribute("class", "MniTabTbl", null);
+        //title="" border="0" cellpadding="0" cellspacing="0"
+        writer.writeAttribute("title", "", null);
+        writer.writeAttribute("border", "0", null);
+        writer.writeAttribute("cellpadding", "0", null);
+        writer.writeAttribute("cellspacing", "0", null);
+        writer.startElement("tr", this);
+        for (int i = 0; i < tabNames.length; i++) {
+            writer.startElement("td", this);
+            if (tabNames[i].replaceAll(" ", "").toLowerCase().equals(tab)) {
+                writer.writeAttribute("class", "MniTabTblSelTd", null);//TODO parameterize this from the url or jsp
+                writer.startElement("div", this);
+                // title="Current Selection: Cataloging Information"
+                writer.writeAttribute("title", "Current Selection: " + tabNames[i], null);//TODO: set this according to url param
+                writer.writeAttribute("class", "MniTabSelTxt", null);
+                writer.startElement("span", this);
+                writer.writeAttribute("id", tabNames[i].replaceAll(" ", "").toLowerCase(), null);//TODO, set this to the key
+                writer.writeAttribute("class", "disabled", null);
+                writer.writeText(tabNames[i], null);
+                writer.endElement("span");
+                writer.endElement("div");
+            } else {
+                writer.startElement("a", this);
+                writer.writeAttribute("id", tabNames[i].replaceAll(" ", "").toLowerCase(), null);
+                writer.writeAttribute("class", "MniTabLnk", null);
+                writer.writeAttribute("title", tabNames[i], null);
+                writer.writeAttribute("href", "/dvn/?tab=" + tabNames[i].replaceAll(" ", "").toLowerCase(), null);//TODO parameterize the link to the home page.
+                writer.writeText(tabNames[i], null);
+                writer.endElement("a");
+            }
+            writer.endElement("td");
+        }
+        writer.endElement("tr");
+        writer.endElement("table");
+        writer.endElement("div");
+        writer.startElement("div", this);
+        writer.startElement("a", this);
+        writer.writeAttribute("id", "tabSet1skipLink_skipHyperlinkId", null);
+        writer.writeAttribute("name", "tabSet1skipLink_skipHyperlinkId", null);
+        writer.endElement("a");
+        writer.endElement("div");
+    } catch (Exception ioe) {
+            throw new FacesException();
+        }
+        
     }
 
     // helper methods
       //utils
     private void formatHeading(String heading) {
-        ResponseWriter writer = FacesContext.getCurrentInstance().getResponseWriter();
+      
+        HtmlPanelGroup panelGroup = new HtmlPanelGroup();
+        panelGroup.setStyleClass("dvnMainPanel");
+        UIOutput headingText = new UIOutput();
+        ValueBinding outervaluebinding = FacesContext.getCurrentInstance().getApplication().createValueBinding(heading);
+        headingText.setValueBinding("value", outervaluebinding);
         try {
-          writer.startElement("span", this);
-          writer.writeAttribute("class", "dvnMainPanel", null);
-          writer.writeText(heading, null);
-          writer.endElement("span");
+            panelGroup.getChildren().add(headingText);
+            this.getChildren().add(panelGroup);
+            panelGroup.encodeBegin(FacesContext.getCurrentInstance());
+            if (panelGroup.getRendersChildren()) {
+                panelGroup.encodeChildren(FacesContext.getCurrentInstance());
+            }
+            panelGroup.encodeEnd(FacesContext.getCurrentInstance());
         } catch (IOException ioe) {
             throw new FacesException();
         }
@@ -110,10 +224,7 @@ public class DataList extends UIComponentBase {
      * @author wbossons
      */
     private void formatChildTable(List ndvs, String heading) {
-        //Set no. of records per column
         int totalColumns = 3;
-        //TODO: use and/or calculate the 1st arg of setColumnLength from the current rows property
-        //DEBUG
         int columns = 0;
         int startNew = 0;
         if (totalColumns <= ndvs.size())
@@ -121,98 +232,107 @@ public class DataList extends UIComponentBase {
         else
             startNew = setColumnLength(3);//was (ndvs.size(), totalColumns);
         int startPos = 0;
-         
-        ResponseWriter writer = FacesContext.getCurrentInstance().getResponseWriter();
-        try {
-            //Begin writing the child table associated with this heading
-            writer.startElement("table", this);
-            writer.writeAttribute("id", heading, null);
-            writer.writeAttribute("class", "dvnChildTable", null);
-            writer.startElement("tr", this);
-            //writer.startElement("td", this);
-            //writer.writeAttribute("class", "dvnChildColumn", null);
-            //writer.writeAttribute("width", "33%", null);
-            ListIterator iterator = ndvs.listIterator();
-            while (iterator.hasNext()) {
-                DataListing ndv  = (DataListing)iterator.next();
-                if (startPos == 0) {
-                    columns++;
-                    writer.startElement("td", this);
-                    writer.writeAttribute("class", "dvnChildColumn", null);
-                }
-                writer.startElement("ul", this);
-                writer.startElement("li", this);
-                writer.writeAttribute("class", "activeBullet", null);
-                writer.startElement("a", this);
-                writer.writeAttribute("href", "/dvn/dv/" + ndv.getAlias(), null);
-                if ( ndv.getRestricted().equals("yes") ) {
-                    writer.writeAttribute("title", "This dataverse is not released.", null);
-                    writer.writeText(ndv.getName(), null);
-                    writer.endElement("a");
-                    writer.startElement("span", this);
-                    writer.writeAttribute("class", "dvn_dvNotReleased", null);
-                    writer.writeText("Not Released", null);
-                    writer.endElement("span");
-                } else {
-                    writer.writeAttribute("title", ndv.getTooltip() + " dataverse", null);
-                    writer.writeText(ndv.getName(), null);
-                    writer.endElement("a");
-                }
-                if (ndv.getAffiliation() != null && !ndv.getAffiliation().equals("")) {
-                    //affiliation bullets
-                    writer.startElement("ul", this);
-                    writer.writeAttribute("class", "affiliationBullet", null);
-                    writer.startElement("li", this);
-                    writer.writeAttribute("class", "affiliationBullet", null);
-                    writer.writeText(ndv.getAffiliation(), null);
-                    writer.endElement("li");
-                    writer.endElement("ul");
-                } else {
-                    writer.startElement("ul", this);
-                    writer.startElement("li", this);
-                    writer.writeAttribute("class", "dvnLineSpacer", null);
-                    writer.writeText("&nbsp;&nbsp;", null);
-                    writer.endElement("li");
-                    writer.endElement("ul");
-                }
-                // end outside bullet (represents dv name bullets)
-                writer.endElement("li");
-                writer.endElement("ul");
+        ListIterator iterator = ndvs.listIterator();
+        HtmlPanelGrid childTable = new HtmlPanelGrid();
+        childTable = new HtmlPanelGrid(); // start the child table which eventually must be added to the view
+        childTable.setId(heading.replaceAll(" ", "").toLowerCase());
+        childTable.setStyleClass("dvnChildTable");
+        childTable.setColumns(3);
+        childTable.setColumnClasses("dvnChildColumn");
+        UIColumn column              = null;
+        HtmlPanelGroup linkPanel     = null;
+        HtmlOutputText startLinkTag  = null;
+        HtmlOutputText endLinkTag    = null;
+        HtmlOutputText affiliationTag = null;
+        Hyperlink nodelink           = null;
+        HtmlGraphicImage image       = null;
+        HtmlOutputText textTag       = null;
+        
+        while (iterator.hasNext()) {
+            DataListing ndv  = (DataListing)iterator.next();
+            if (startPos == 0) {
+            column = new UIColumn();
+            }
+            startLinkTag = new HtmlOutputText();
+            startLinkTag.setEscape(false);
+            startLinkTag.setValue("<ul><li class='activeBullet'>");
+            nodelink = new Hyperlink();
+            nodelink.setText(ndv.getName());
+            nodelink.setToolTip(ndv.getTooltip() + " dataverse");
+            nodelink.setUrl("/dv/" + ndv.getAlias());
+            endLinkTag = new HtmlOutputText();
+            endLinkTag.setEscape(false);
+            endLinkTag.setValue("</li></ul>");
+            linkPanel = new HtmlPanelGroup();
+            linkPanel.getChildren().add(startLinkTag);
+            linkPanel.getChildren().add(nodelink);
+            if ( ndv.getRestricted().equals("yes") ) {
+                nodelink.setToolTip("This dataverse is not released.");
+            }
+            if (ndv.getAffiliation() != null && !ndv.getAffiliation().equals("")) {
+            affiliationTag = new HtmlOutputText();
+            affiliationTag.setEscape(false);
+            affiliationTag.setValue("<ul class='affiliationBullet'><li class='affiliationBullet'>" + ndv.getAffiliation() + "</li></ul>");
+            linkPanel.getChildren().add(affiliationTag);
+            }
+            
+            linkPanel.getChildren().add(endLinkTag);
+            column.getChildren().add(linkPanel);
                 startPos++;
-                // TODO: make columns var
-                if (startPos == startNew || !iterator.hasNext()) {
-                    writer.endElement("td");
+            if (startPos == startNew || iterator.hasNext() == false) {
+                childTable.getChildren().add(column);
                     startPos = 0;
                 }
-                if (startPos == startNew && !iterator.hasNext()) {
-                    break;
-
-                }
-
-
             }
             //manage the condition where there were only enough records
             // to build two columns -- to achieve balance in the presentation
-            if (columns < totalColumns) {
-                int remainder = totalColumns - columns;
+        if (childTable.getChildCount() < totalColumns) {
+            int remainder = totalColumns - childTable.getChildCount();
                 int i = 0;
                 HtmlOutputText placeholder = null;
                 while (i < remainder) {
-                    writer.startElement("td", this);
-                    writer.startElement("br", this);
-                    writer.endElement("br");
-                    writer.endElement("td");
+                column = new UIColumn();
+                placeholder = new HtmlOutputText();
+                placeholder.setEscape(false);
+                placeholder.setValue("<br />");
+                column.getChildren().add(placeholder);
+                childTable.getChildren().add(column);
                     i++;
                 }
             }
-            writer.endElement("tr");
-            writer.endElement("table");
+        
+        try {
+            this.getChildren().add(childTable);
+            childTable.encodeBegin(FacesContext.getCurrentInstance());
+            if (childTable.getRendersChildren()) {
+                childTable.encodeChildren(FacesContext.getCurrentInstance());
+            }
+            childTable.encodeEnd(FacesContext.getCurrentInstance());
+            FacesContext context = FacesContext.getCurrentInstance();
+        Application application = context.getApplication();
+        ELContext elContext = FacesContext.getCurrentInstance().getELContext();
+        ExpressionFactory expressionFactory = application.getExpressionFactory();
+       
             } catch (IOException ioe) {
             throw new FacesException();
         } //TODO: implment ajax command links 
         //finally {
             //createNavigationLinks(heading);
         //}
+    }
+    
+    private void formatMessage(String message) {
+        HtmlOutputText output = new HtmlOutputText();
+        output.setValue(message);
+        this.getChildren().add(output);
+        try {
+            output.encodeBegin(FacesContext.getCurrentInstance());
+            if (output.getRendersChildren())
+                output.encodeChildren(FacesContext.getCurrentInstance());
+            output.encodeEnd(FacesContext.getCurrentInstance());
+        } catch (IOException ioe) {
+            throw new FacesException();
+        }
     }
     
     private void createNavigationLinks(String heading) {
@@ -234,7 +354,7 @@ public class DataList extends UIComponentBase {
         nextLink.setValue("Next >>");
         MethodExpression nextActionListener = expressionFactory.createMethodExpression(elContext, "#{list.page_action}", null, new Class[] {ActionEvent.class});
         nextLink.addActionListener(new MethodExpressionActionListener(nextActionListener));
-        
+
         this.getChildren().add(nextLink);
         } catch (Exception e) {
             System.out.println("there was an issue in the create links area " + e.toString());

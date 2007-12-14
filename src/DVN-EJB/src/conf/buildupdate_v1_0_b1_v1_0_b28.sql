5bdf08a8a7d31ee3d53888ad89e75e91342ef288
Merce Crosas <mcrosas@iq.harvard.edu>
2007-12-14 21:19:00 +0000

    New update for release v1_0

---
+++ src/DVN-EJB/src/conf/buildupdate_v1_0_b1_v1_0_b28.sql
@@ -0,0 +1,3 @@
+update vdcnetwork set defaultvdcabouttext='This About page is not used anymore in the DVN application.', defaultvdcannouncements='A description of your Dataverse or announcements may be added here. Use My Options to edit or remove this text.';
+
+

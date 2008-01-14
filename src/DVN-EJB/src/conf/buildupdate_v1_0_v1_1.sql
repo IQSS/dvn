db85825c6f1d831bcc1f5c4e05e4e7023c8f0335
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2008-01-14 20:20:37 +0000

    database changes for new terms of use data

---
+++ src/DVN-EJB/src/conf/buildupdate_v1_0_v1_1.sql
@@ -0,0 +1,29 @@
+ALTER TABLE vdcnetwork ADD COLUMN termsOfUse text;
+ALTER TABLE vdcnetwork ALTER COLUMN termsOfUse SET STORAGE EXTENDED;
+
+ALTER TABLE vdcnetwork ADD COLUMN depositTermsOfUse text;
+ALTER TABLE vdcnetwork ALTER COLUMN depositTermsOfUse SET STORAGE EXTENDED;
+
+ALTER TABLE vdcnetwork ADD COLUMN deposittermsofuseenabled bool;
+ALTER TABLE vdcnetwork ALTER COLUMN deposittermsofuseenabled SET STORAGE PLAIN;
+
+ALTER TABLE vdcnetwork ADD COLUMN downloadTermsOfUse text;
+ALTER TABLE vdcnetwork ALTER COLUMN downloadTermsOfUse SET STORAGE EXTENDED;
+
+ALTER TABLE vdcnetwork ADD COLUMN downloadtermsofuseenabled bool;
+ALTER TABLE vdcnetwork ALTER COLUMN downloadtermsofuseenabled SET STORAGE PLAIN;
+
+
+ALTER TABLE vdc ADD COLUMN depositTermsOfUse text;
+ALTER TABLE vdc ALTER COLUMN depositTermsOfUse SET STORAGE EXTENDED;
+
+ALTER TABLE vdc ADD COLUMN deposittermsofuseenabled bool;
+ALTER TABLE vdc ALTER COLUMN deposittermsofuseenabled SET STORAGE PLAIN;
+
+ALTER TABLE vdcuser ADD COLUMN agreedtermsofuse bool;
+ALTER TABLE vdcuser ALTER COLUMN agreedtermsofuse SET STORAGE PLAIN;
+
+update vdc set deposittermsofuseenabled=false;
+update vdcnetwork set deposittermsofuseenabled=false;
+update vdcnetwork set downloadtermsofuseenabled=false;
+update vdcuser set agreedtermsofuse=false;

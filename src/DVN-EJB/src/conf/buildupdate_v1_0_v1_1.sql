f5ebbd0913c2bcaffedce59687355d0b7e263cf7
Wendy Bossons <dvn_support@help.hmdc.harvard.edu>
2008-01-24 21:54:00 +0000

    Set display number default and per group for network home page.

--- src/DVN-EJB/src/conf/buildupdate_v1_0_v1_1.sql
+++ src/DVN-EJB/src/conf/buildupdate_v1_0_v1_1.sql
@@ -16,16 +16,22 @@ ALTER TABLE vdcnetwork ALTER COLUMN downloadtermsofuseenabled SET STORAGE PLAIN;
 ALTER TABLE vdcnetwork ADD COLUMN termsofuseupdated timestamp;
 ALTER TABLE vdcnetwork ALTER COLUMN termsofuseupdated SET STORAGE PLAIN;
 
-
+ALTER TABLE vdcnetwork ADD COLUMN defaultdisplaynumber int4;
+ALTER TABLE vdcnetwork ALTER COLUMN defaultdisplaynumber SET STORAGE PLAIN;
 
 ALTER TABLE vdc ADD COLUMN deposittermsofuseenabled bool;
 ALTER TABLE vdc ALTER COLUMN deposittermsofuseenabled SET STORAGE PLAIN;
+
+ALTER TABLE vdcgroup ADD COLUMN defaultdisplaynumber int4;
+ALTER TABLE vdcgroup ALTER COLUMN defaultdisplaynumber SET STORAGE PLAIN;
 
 ALTER TABLE vdcuser ADD COLUMN agreedtermsofuse bool;
 ALTER TABLE vdcuser ALTER COLUMN agreedtermsofuse SET STORAGE PLAIN;
 
 update vdc set deposittermsofuseenabled=false;
+update vdcgroup set defaultDisplayNumber=15;
 update vdcnetwork set deposittermsofuseenabled=false;
 update vdcnetwork set downloadtermsofuseenabled=false;
+update vdcnetwork set defaultdisplaynumber=15;
 update vdcuser set agreedtermsofuse=false;
 

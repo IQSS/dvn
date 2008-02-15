6750b3fd27dad7a278c8f916efc9d18fe638620e
Gustavo Durand <gdurand@hmdc.harvard.edu>
2008-02-15 22:02:58 +0000

    added methods to replace old DDI20ServiceBean

--- src/DVN-EJB/src/java/edu/harvard/hmdc/vdcnet/ddi/DDIServiceLocal.java
+++ src/DVN-EJB/src/java/edu/harvard/hmdc/vdcnet/ddi/DDIServiceLocal.java
@@ -11,7 +11,9 @@ package edu.harvard.hmdc.vdcnet.ddi;
 
 import edu.harvard.hmdc.vdcnet.study.Study;
 import edu.harvard.hmdc.vdcnet.study.StudyExporter;
+import edu.harvard.hmdc.vdcnet.study.StudyFile;
 import java.io.File;
+import java.io.OutputStream;
 import java.util.Map;
 import javax.ejb.Local;
 
@@ -21,7 +23,8 @@ import javax.ejb.Local;
  */
 @Local
 public interface DDIServiceLocal extends StudyExporter, java.io.Serializable {
+    void mapDDI(String xmlToParse, Study study);
     void mapDDI(File ddiFile, Study study);
-
     Map determineId(File ddiFile);
+    void exportDataFile(StudyFile sf, OutputStream out);
 }

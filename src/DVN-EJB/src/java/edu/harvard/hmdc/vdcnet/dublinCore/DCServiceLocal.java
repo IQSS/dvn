93bb97bef7e9bb9a677e485727b7b8568fc66afa
Gustavo Durand <gdurand@hmdc.harvard.edu>
2008-02-28 21:42:25 +0000

    cleaned up import/export to close files

--- src/DVN-EJB/src/java/edu/harvard/hmdc/vdcnet/dublinCore/DCServiceLocal.java
+++ src/DVN-EJB/src/java/edu/harvard/hmdc/vdcnet/dublinCore/DCServiceLocal.java
@@ -38,7 +38,7 @@ import javax.xml.bind.JAXBException;
 public interface DCServiceLocal extends StudyExporter, java.io.Serializable  {
   
  
-    void exportStudy(Study study, OutputStream out) throws IOException, JAXBException;
+    void exportStudy(Study study, OutputStream out) throws IOException;
     
  
     

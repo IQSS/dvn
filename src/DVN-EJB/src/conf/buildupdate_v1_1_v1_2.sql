98535053689c2cb1a1da322a19b607ef1fc2efe0
Gustavo Durand <gdurand@hmdc.harvard.edu>
2008-03-25 22:17:00 +0000

    added UtiltiesPage to pagedef table

--- src/DVN-EJB/src/conf/buildupdate_v1_1_v1_2.sql
+++ src/DVN-EJB/src/conf/buildupdate_v1_1_v1_2.sql
@@ -2,3 +2,4 @@ ALTER TABLE studygrant ALTER COLUMN number type text;
 ALTER TABLE studygrant ALTER COLUMN number SET STORAGE EXTENDED;
 
 INSERT INTO pagedef ( name, path, role_id, networkrole_id ) VALUES ( 'ForgotPasswordPage', '/login/ForgotPasswordPage.jsp', null,null );
+INSERT INTO pagedef ( name, path, role_id, networkrole_id ) VALUES ( 'UtilitiesPage', '/networkAdmin/UtilitiesPage.jsp', null, 2 );

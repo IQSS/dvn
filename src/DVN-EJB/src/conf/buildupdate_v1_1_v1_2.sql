59b23cde6be48e40f9c2baef2ba23280c8709184
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2008-04-08 16:11:30 +0000

    added constraints to pagedef table (to match new constraint annotations in PageDef class)

--- src/DVN-EJB/src/conf/buildupdate_v1_1_v1_2.sql
+++ src/DVN-EJB/src/conf/buildupdate_v1_1_v1_2.sql
@@ -6,3 +6,9 @@ INSERT INTO pagedef ( name, path, role_id, networkrole_id ) VALUES ( 'UtilitiesP
 INSERT INTO pagedef (name, path, role_id, networkrole_id ) VALUES  ( 'SubsettingPage', '/subsetting/SubsettingPage.jsp', null,null );
 
 update dvnversion set versionnumber=1, buildnumber=2;
+
+ALTER TABLE pagedef
+  ADD CONSTRAINT unq_pagedef_0 UNIQUE(path);
+
+ALTER TABLE pagedef
+  ADD CONSTRAINT unq_pagedef_1 UNIQUE(name);

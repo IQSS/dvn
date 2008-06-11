453dda342de93deea1a49e46556d5f0ea5f171f1
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2008-06-11 16:22:28 +0000

    moved metadata seq setval

--- src/DVN-EJB/src/conf/buildupdate_v1_2_v2_0.sql
+++ src/DVN-EJB/src/conf/buildupdate_v1_2_v2_0.sql
@@ -80,7 +80,7 @@ CREATE TABLE metadata
 WITHOUT OIDS;
 ALTER TABLE metadata OWNER TO "dvnApp";
 
-setval('metadata_id_seq',100000);
+
 
 CREATE INDEX metadata_id_index
   ON metadata
@@ -510,7 +510,7 @@ alter table template add CONSTRAINT fk_template_metadata_id FOREIGN KEY (metadat
  alter table metadata add  CONSTRAINT fk_metadata_template_id FOREIGN KEY (template_id)
       REFERENCES "template" (id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION;
-
+setval('metadata_id_seq',100000);
 INSERT INTO metadata(id, version ) VALUES (nextval('metadata_id_seq'), 1);
 update template set metadata_id=currval('metadata_id_seq');
 

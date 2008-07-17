2e6d1372e4c579a18868be9bf7ac719a4eb0221c
Gustavo Durand <gdurand@hmdc.harvard.edu>
2008-07-17 22:06:08 +0000

    added studyFileActivity (to track downloads per file, rather than study)

--- src/DVN-EJB/src/java/edu/harvard/hmdc/vdcnet/study/StudyFile.java
+++ src/DVN-EJB/src/java/edu/harvard/hmdc/vdcnet/study/StudyFile.java
@@ -24,7 +24,6 @@
  * Created on July 28, 2006, 2:59 PM
  *
  */
-
 package edu.harvard.hmdc.vdcnet.study;
 
 import edu.harvard.hmdc.vdcnet.admin.NetworkRoleServiceLocal;
@@ -52,107 +51,102 @@ import javax.persistence.*;
  * @author Ellen Kraffmiller
  */
 @Entity
-public class StudyFile implements Serializable{
+public class StudyFile implements Serializable {
+
     private String globalId;
     private String fileName;
     private String fileType;
     private boolean subsettable;
     private String fileSystemLocation;
     private String label;
-    
     private String originalFileType;
-
-    
-    @Column(columnDefinition="TEXT")
+    @Column(columnDefinition = "TEXT")
     private String description;
-    
+
     /**
      * Creates a new instance of StudyFile
      */
     public StudyFile() {
     }
-    
+
     public String getGlobalId() {
         return globalId;
     }
-    
+
     public void setGlobalId(String globalId) {
         this.globalId = globalId;
     }
-    
+
     public String getFileName() {
         return fileName;
     }
-    
+
     public void setFileName(String fileName) {
         this.fileName = fileName;
     }
-    
+
     public String getFileType() {
         return fileType;
     }
-    
+
     public void setFileType(String fileType) {
         this.fileType = fileType;
     }
-    
+
     public boolean isSubsettable() {
         return subsettable;
     }
-    
+
     public void setSubsettable(boolean subsettable) {
         this.subsettable = subsettable;
     }
-    
+
     public String getFileSystemLocation() {
         return fileSystemLocation;
     }
-    
+
     public void setFileSystemLocation(String fileSystemLocation) {
         this.fileSystemLocation = fileSystemLocation;
     }
-    
+
     public String getLabel() {
         return label;
     }
-    
+
     public void setLabel(String label) {
         this.label = label;
     }
-    
+
     public String getOriginalFileType() {
         return originalFileType;
     }
 
     public void setOriginalFileType(String originalFileType) {
         this.originalFileType = originalFileType;
-    }    
-   
+    }
+
     public String getDescription() {
         return description;
     }
-    
+
     public void setDescription(String description) {
         this.description = description;
     }
-    
-  
-    @ManyToOne (cascade={CascadeType.MERGE, CascadeType.PERSIST})
+    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.PERSIST})
     private FileCategory fileCategory;
-    
+
     public FileCategory getFileCategory() {
         return fileCategory;
     }
-    
+
     public void setFileCategory(FileCategory fileCategory) {
         this.fileCategory = fileCategory;
     }
-    
     /**
      * Holds value of property displayOrder.
      */
     private int displayOrder;
-    
+
     /**
      * Getter for property displayOrder.
      * @return Value of property displayOrder.
@@ -160,7 +154,7 @@ public class StudyFile implements Serializable{
     public int getDisplayOrder() {
         return this.displayOrder;
     }
-    
+
     /**
      * Setter for property displayOrder.
      * @param displayOrder New value of property displayOrder.
@@ -168,14 +162,14 @@ public class StudyFile implements Serializable{
     public void setDisplayOrder(int displayOrder) {
         this.displayOrder = displayOrder;
     }
-    
     /**
      * Holds value of property id.
      */
-    @SequenceGenerator(name="studyfile_gen", sequenceName="studyfile_id_seq")
-    @Id @GeneratedValue(strategy=GenerationType.SEQUENCE,generator="studyfile_gen")
+    @SequenceGenerator(name = "studyfile_gen", sequenceName = "studyfile_id_seq")
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "studyfile_gen")
     private Long id;
-    
+
     /**
      * Getter for property id.
      * @return Value of property id.
@@ -183,7 +177,7 @@ public class StudyFile implements Serializable{
     public Long getId() {
         return this.id;
     }
-    
+
     /**
      * Setter for property id.
      * @param id New value of property id.
@@ -191,14 +185,12 @@ public class StudyFile implements Serializable{
     public void setId(Long id) {
         this.id = id;
     }
-    
-    
     /**
      * Holds value of property version.
      */
     @Version
     private Long version;
-    
+
     /**
      * Getter for property version.
      * @return Value of property version.
@@ -206,7 +198,7 @@ public class StudyFile implements Serializable{
     public Long getVersion() {
         return this.version;
     }
-    
+
     /**
      * Setter for property version.
      * @param version New value of property version.
@@ -214,12 +206,11 @@ public class StudyFile implements Serializable{
     public void setVersion(Long version) {
         this.version = version;
     }
-   
     /**
      * Holds value of property restricted.
      */
     private boolean restricted;
-    
+
     /**
      * Getter for property restricted.
      * @return Value of property restricted.
@@ -227,7 +218,7 @@ public class StudyFile implements Serializable{
     public boolean isRestricted() {
         return this.restricted;
     }
-    
+
     /**
      * Setter for property restricted.
      * @param restricted New value of property restricted.
@@ -235,13 +226,12 @@ public class StudyFile implements Serializable{
     public void setRestricted(boolean restricted) {
         this.restricted = restricted;
     }
-    
     /**
      * Holds value of property allowedGroups.
      */
-    @ManyToMany(cascade={CascadeType.REMOVE, CascadeType.MERGE, CascadeType.PERSIST })
+    @ManyToMany(cascade = {CascadeType.REMOVE, CascadeType.MERGE, CascadeType.PERSIST})
     private Collection<UserGroup> allowedGroups;
-    
+
     /**
      * Getter for property allowedGroups.
      * @return Value of property allowedGroups.
@@ -249,7 +239,7 @@ public class StudyFile implements Serializable{
     public Collection<UserGroup> getAllowedGroups() {
         return this.allowedGroups;
     }
-    
+
     /**
      * Setter for property allowedGroups.
      * @param allowedGroups New value of property allowedGroups.
@@ -257,13 +247,12 @@ public class StudyFile implements Serializable{
     public void setAllowedGroups(Collection<UserGroup> allowedGroups) {
         this.allowedGroups = allowedGroups;
     }
-    
     /**
      * Holds value of property allowedUsers.
      */
-    @ManyToMany(cascade={CascadeType.REMOVE, CascadeType.MERGE, CascadeType.PERSIST })
+    @ManyToMany(cascade = {CascadeType.REMOVE, CascadeType.MERGE, CascadeType.PERSIST})
     private Collection<VDCUser> allowedUsers;
-    
+
     /**
      * Getter for property allowedUsers.
      * @return Value of property allowedUsers.
@@ -271,7 +260,7 @@ public class StudyFile implements Serializable{
     public Collection<VDCUser> getAllowedUsers() {
         return this.allowedUsers;
     }
-    
+
     /**
      * Setter for property allowedUsers.
      * @param allowedUsers New value of property allowedUsers.
@@ -279,14 +268,13 @@ public class StudyFile implements Serializable{
     public void setAllowedUsers(Collection<VDCUser> allowedUsers) {
         this.allowedUsers = allowedUsers;
     }
-    
     /**
      * Holds value of property requestAccess.
      * If this is true, then the user can send a request to the vdc administrator
      * to be allowed to view the file.
      */
     private boolean requestAccess;
-    
+
     /**
      * Getter for property requestAccess.
      * @return Value of property requestAccess.
@@ -294,7 +282,7 @@ public class StudyFile implements Serializable{
     public boolean isRequestAccess() {
         return this.requestAccess;
     }
-    
+
     /**
      * Setter for property requestAccess.
      * @param requestAccess New value of property requestAccess.
@@ -302,12 +290,11 @@ public class StudyFile implements Serializable{
     public void setRequestAccess(boolean requestAccess) {
         this.requestAccess = requestAccess;
     }
-    
     /**
      * Holds value of property allowAccessRequest.
      */
     private boolean allowAccessRequest;
-    
+
     /**
      * Getter for property allowAccessRequest.
      * @return Value of property allowAccessRequest.
@@ -315,7 +302,7 @@ public class StudyFile implements Serializable{
     public boolean isAllowAccessRequest() {
         return this.allowAccessRequest;
     }
-    
+
     /**
      * Setter for property allowAccessRequest.
      * @param allowAccessRequest New value of property allowAccessRequest.
@@ -323,13 +310,12 @@ public class StudyFile implements Serializable{
     public void setAllowAccessRequest(boolean allowAccessRequest) {
         this.allowAccessRequest = allowAccessRequest;
     }
-    
     /**
      * Holds value of property dataTable.
      */
-    @OneToOne(mappedBy="studyFile", cascade={CascadeType.REMOVE, CascadeType.MERGE, CascadeType.PERSIST})
+    @OneToOne(mappedBy = "studyFile", cascade = {CascadeType.REMOVE, CascadeType.MERGE, CascadeType.PERSIST})
     private DataTable dataTable;
-    
+
     /**
      * Getter for property studyFile.
      * @return Value of property studyFile.
@@ -337,7 +323,7 @@ public class StudyFile implements Serializable{
     public DataTable getDataTable() {
         return this.dataTable;
     }
-    
+
     /**
      * Setter for property studyFile.
      * @param studyFile New value of property studyFile.
@@ -348,36 +334,36 @@ public class StudyFile implements Serializable{
 
     public boolean isSubsetRestrictedForUser(VDCUser user, VDC vdc, UserGroup ipUserGroup) {
         // the restrictions should be checked on the owner of the study, not the currentVDC (needs cleanup)
-        VDC owner = this.getFileCategory().getStudy().getOwner();        
+        VDC owner = this.getFileCategory().getStudy().getOwner();
         if (owner.isHarvestingDataverse()) {
             HarvestingDataverse hd = owner.getHarvestingDataverse();
             return hd.isSubsetRestrictedForUser(user, ipUserGroup);
         } else {
             return isFileRestrictedForUser(user, owner, ipUserGroup);
         }
-    }    
-    
-    public boolean isFileRestrictedForUser( VDCUser user, VDC vdc, UserGroup ipUserGroup ) {
+    }
+
+    public boolean isFileRestrictedForUser(VDCUser user, VDC vdc, UserGroup ipUserGroup) {
 
         // the restrictions should be checked on the owner of the study, not the currentVDC (needs cleanup)
         vdc = this.getFileCategory().getStudy().getOwner();
 
-        
+
         // first check if study is restricted, regardless of file permissions
         Study study = getFileCategory().getStudy();
-        if (study.isStudyRestrictedForUser(vdc,user)){
+        if (study.isStudyRestrictedForUser(vdc, user)) {
             return true;
         }
 
         //  then check dataverse level file permisssions
-         if (vdc.areFilesRestrictedForUser(user, ipUserGroup)) {
-             return true;
-         }
-        
+        if (vdc.areFilesRestrictedForUser(user, ipUserGroup)) {
+            return true;
+        }
+
         // otherwise check for restriction on file itself
-        if ( isRestricted() ) {
+        if (isRestricted()) {
             if (user == null) {
-                if (ipUserGroup==null) {
+                if (ipUserGroup == null) {
                     return true;
                 } else {
                     Iterator iter = this.getAllowedGroups().iterator();
@@ -385,44 +371,44 @@ public class StudyFile implements Serializable{
                         UserGroup allowedGroup = (UserGroup) iter.next();
                         if (allowedGroup.equals(ipUserGroup)) {
                             return false;
-                        }    
-                    } 
+                        }
+                    }
                     return true;
                 }
             }
-          
+
             // 1. check if study is restricted (this part was moved a little higher; still need more cleanup)    
             // 2. check network role
-            if (user.getNetworkRole()!=null && user.getNetworkRole().getName().equals(NetworkRoleServiceLocal.ADMIN) ) {
+            if (user.getNetworkRole() != null && user.getNetworkRole().getName().equals(NetworkRoleServiceLocal.ADMIN)) {
                 // If you are network admin, you can do anything!
                 return false;
             }
-            
+
             // 3. check vdc role
             VDCRole userRole = user.getVDCRole(vdc);
             if (userRole != null) {
                 String userRoleName = userRole.getRole().getName();
-                if ( userRoleName.equals(RoleServiceLocal.ADMIN) || userRoleName.equals(RoleServiceLocal.CURATOR) ) {
+                if (userRoleName.equals(RoleServiceLocal.ADMIN) || userRoleName.equals(RoleServiceLocal.CURATOR)) {
                     return false;
                 }
             }
-            
+
             //4. check if creator
             if (user.getId().equals(this.getFileCategory().getStudy().getCreator().getId())) {
                 return false;
             }
-            
+
             // 5. check user
             Iterator iter = this.getAllowedUsers().iterator();
             while (iter.hasNext()) {
                 VDCUser allowedUser = (VDCUser) iter.next();
-                if ( allowedUser.getId().equals(user.getId()) ) {
+                if (allowedUser.getId().equals(user.getId())) {
                     return false;
                 }
             }
-            
+
             // 6. check groups
-          
+
             iter = this.getAllowedGroups().iterator();
             while (iter.hasNext()) {
                 UserGroup allowedGroup = (UserGroup) iter.next();
@@ -431,34 +417,32 @@ public class StudyFile implements Serializable{
                 }
             }
             return true;
-        }      
-        else {
+        } else {
             return false;
         }
-     }
-    
-     public boolean isUserInAllowedUsers(Long userId) {
-         for (Iterator it = allowedUsers.iterator(); it.hasNext();) {
-             VDCUser elem = (VDCUser) it.next();
-             if (elem.getId().equals(userId)) {
-                 return true;
-             }
-             
-         }
-         return false;
-     }
-     
-   public boolean isGroupInAllowedGroups(Long groupId) {
-         for (Iterator it = allowedGroups.iterator(); it.hasNext();) {
-             UserGroup elem = (UserGroup) it.next();
-             if (elem.getId().equals(groupId)) {
-                 return true;
-             }
-             
-         }
-         return false;
-     }     
+    }
 
+    public boolean isUserInAllowedUsers(Long userId) {
+        for (Iterator it = allowedUsers.iterator(); it.hasNext();) {
+            VDCUser elem = (VDCUser) it.next();
+            if (elem.getId().equals(userId)) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    public boolean isGroupInAllowedGroups(Long groupId) {
+        for (Iterator it = allowedGroups.iterator(); it.hasNext();) {
+            UserGroup elem = (UserGroup) it.next();
+            if (elem.getId().equals(groupId)) {
+                return true;
+            }
+
+        }
+        return false;
+    }
     /**
      * Holds value of property fileSystemName.
      */
@@ -479,14 +463,13 @@ public class StudyFile implements Serializable{
     public void setFileSystemName(String fileSystemName) {
         this.fileSystemName = fileSystemName;
     }
-    
+
     public boolean isRemote() {
-        return 
-            fileSystemLocation != null && 
-            (fileSystemLocation.startsWith("http:") || fileSystemLocation.startsWith("https:") || fileSystemLocation.startsWith("ftp:") );
+        return fileSystemLocation != null &&
+                (fileSystemLocation.startsWith("http:") || fileSystemLocation.startsWith("https:") || fileSystemLocation.startsWith("ftp:"));
     }
-    
- public int hashCode() {
+
+    public int hashCode() {
         int hash = 0;
         hash += (this.id != null ? this.id.hashCode() : 0);
         return hash;
@@ -497,9 +480,21 @@ public class StudyFile implements Serializable{
         if (!(object instanceof StudyFile)) {
             return false;
         }
-        StudyFile other = (StudyFile)object;
-        if (this.id != other.id && (this.id == null || !this.id.equals(other.id))) return false;
+        StudyFile other = (StudyFile) object;
+        if (this.id != other.id && (this.id == null || !this.id.equals(other.id))) {
+            return false;
+        }
         return true;
-    }       
+    }
     
+    @OneToOne(mappedBy = "studyFile", cascade = {CascadeType.REMOVE, CascadeType.MERGE, CascadeType.PERSIST})
+    private StudyFileActivity studyFileActivity;
+
+    public StudyFileActivity getStudyFileActivity() {
+        return studyFileActivity;
+    }
+
+    public void setStudyFileActivity(StudyFileActivity studyFileActivity) {
+        this.studyFileActivity = studyFileActivity;
+    }
 }

c8e4df2ba493d5dc90c6b4a819945f65bce3e296
Wendy Bossons <dvn_support@help.hmdc.harvard.edu>
2008-08-18 17:38:36 +0000

    Add properties needed by the Dvn Home Page changes.

--- src/DVN-EJB/src/conf/buildupdate_v1_3_v1_4.sql
+++ src/DVN-EJB/src/conf/buildupdate_v1_3_v1_4.sql
@@ -1,2 +1,23 @@
-UPDATE pagedef SET path=regexp_replace(pagedef.path, '.jsp', '.xhtml', 'g')
+UPDATE pagedef SET path=regexp_replace(pagedef.path, '.jsp', '.xhtml', 'g');
 
+-- Column: releasedate
+
+-- ALTER TABLE vdc DROP COLUMN releasedate;
+
+ALTER TABLE vdc ADD COLUMN releasedate timestamp;
+ALTER TABLE vdc ALTER COLUMN releasedate SET STORAGE PLAIN;
+
+-- Column: createddate
+
+-- ALTER TABLE vdc DROP COLUMN createddate;
+
+ALTER TABLE vdc ADD COLUMN createddate timestamp;
+ALTER TABLE vdc ALTER COLUMN createddate SET STORAGE PLAIN;
+
+
+-- Column: dvndescription
+
+-- ALTER TABLE vdc DROP COLUMN dvndescription;
+
+ALTER TABLE vdc ADD COLUMN dvndescription varchar(255);
+ALTER TABLE vdc ALTER COLUMN dvndescription SET STORAGE EXTENDED;

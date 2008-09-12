751b46907ce90063bf649aa8fc31af09aa30fcc8
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2008-09-12 19:56:02 +0000

    remove unused init() method

--- src/DVN-web/src/edu/harvard/hmdc/vdcnet/web/networkAdmin/EditNetworkDepositUseTermsPage.java
+++ src/DVN-web/src/edu/harvard/hmdc/vdcnet/web/networkAdmin/EditNetworkDepositUseTermsPage.java
@@ -131,74 +131,9 @@ public class EditNetworkDepositUseTermsPage extends VDCBaseBean implements java.
         return isUseTerms;
     }
 
-    /**
-     * <p>Callback method that is called whenever a page is navigated to,
-     * either directly via a URL, or indirectly via page navigation.
-     * Customize this method to acquire resources that will be needed
-     * for event handlers and lifecycle methods, whether or not this
-     * page is performing post back processing.</p>
-     *
-     * <p>Note that, if the current request is a postback, the property
-     * values of the components do <strong>not</strong> represent any
-     * values submitted with this request.  Instead, they represent the
-     * property values that were saved for this view when it was rendered.</p>
-     */
-    public void init() {
-        // Perform initializations inherited from our superclass
-        super.init();
-// Perform application initialization that must complete
-        // *before* managed components are initialized
-        // TODO - add your own initialiation code here
+ 
 
-// <editor-fold defaultstate="collapsed" desc="Visual-Web-managed Component Initialization">
-// Initialize automatically managed components
-        // *Note* - this logic should NOT be modified
-        try {
-            _init();
-        } catch (Exception e) {
-            log("Page1 Initialization Failure", e);
-            throw e instanceof FacesException ? (FacesException) e : new FacesException(e);
-        }
-    }
 
-    /**
-     * <p>Callback method that is called after the component tree has been
-     * restored, but before any event processing takes place.  This method
-     * will <strong>only</strong> be called on a postback request that
-     * is processing a form submit.  Customize this method to allocate
-     * resources that will be required in your event handlers.</p>
-     */
-    public void preprocess() {
-    }
-
-    /**
-     * <p>Callback method that is called just before rendering takes place.
-     * This method will <strong>only</strong> be called for the page that
-     * will actually be rendered (and not, for example, on a page that
-     * handled a postback and then navigated to a different page).  Customize
-     * this method to allocate resources that will be required for rendering
-     * this page.</p>
-     */
-    public void prerender() {
-    }
-
-    /**
-     * <p>Callback method that is called after rendering is completed for
-     * this request, if <code>init()</code> was called (regardless of whether
-     * or not this was the page that was actually rendered).  Customize this
-     * method to release resources acquired in the <code>init()</code>,
-     * <code>preprocess()</code>, or <code>prerender()</code> methods (or
-     * acquired during execution of an event handler).</p>
-     */
-    public void destroy() {
-    }
-
-    /**
-     * <p>Automatically managed component initialization.  <strong>WARNING:</strong>
-     * This method is automatically generated, so any user-specified code inserted
-     * here is subject to being replaced.</p>
-     */
-    private void _init() {
-    }
+   
 }
 

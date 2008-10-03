01dd7804fa2d2120796ce7648bc9e312987a02af
Wendy Bossons <dvn_support@help.hmdc.harvard.edu>
2008-10-03 21:02:24 +0000

    This is work in progress. You can look . . . and it will add groups to your dataverse network, but there's still some wiring to be done to make it work as designed ;-)

---
+++ src/DVN-web/src/edu/harvard/hmdc/vdcnet/web/networkAdmin/AddClassificationsPage.java
@@ -0,0 +1,121 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.harvard.hmdc.vdcnet.web.networkAdmin;
+
+import com.icesoft.faces.component.ext.HtmlInputText;
+import com.icesoft.faces.component.ext.HtmlInputTextarea;
+import com.icesoft.faces.component.ext.HtmlSelectOneMenu;
+import edu.harvard.hmdc.vdcnet.admin.LoginAffiliate;
+import edu.harvard.hmdc.vdcnet.vdc.VDCGroup;
+import edu.harvard.hmdc.vdcnet.vdc.VDCGroupServiceLocal;
+import edu.harvard.hmdc.vdcnet.web.common.VDCBaseBean;
+import java.io.Serializable;
+import java.util.Iterator;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.faces.model.SelectItem;
+
+/**
+ * @author wbossons
+ */
+public class AddClassificationsPage extends VDCBaseBean implements Serializable {
+    @EJB VDCGroupServiceLocal vdcGroupService;
+
+    public static final String PROP_SAMPLE_PROPERTY = "sampleProperty";
+    
+    private String            nameProperty;
+    private String            descriptionProperty;
+    private HtmlInputText     nameInput;
+    private HtmlInputTextarea descriptionInput;
+    private HtmlInputText     parentInput;
+    private HtmlSelectOneMenu parentSelect;
+    private SelectItem[]      parentSelectItems;
+    
+    
+    public void init() {
+        super.init();
+        initParentSelectItems();
+    }
+
+    private void initParentSelectItems() {
+        List list = (List) vdcGroupService.findAll();
+        Iterator iterator = list.iterator();
+        parentSelectItems = new SelectItem[list.size()];
+        int i = 0;
+        while (iterator.hasNext()) {
+            VDCGroup vdcgroup = (VDCGroup)iterator.next();
+            SelectItem selectitem = new SelectItem();
+            selectitem.setLabel((String) vdcgroup.getName());
+            selectitem.setValue(vdcgroup.getId());
+            parentSelectItems[i] = selectitem;
+            i++;
+        }
+    }
+
+    //getters
+    public String getNameProperty() {
+        return nameProperty;
+    }
+
+    public String getDescriptionProperty() {
+        return descriptionProperty;
+    }
+      
+    public HtmlInputText getNameInput() {
+        return this.nameInput;
+    }
+    
+    public HtmlInputTextarea getDescriptionInput() {
+        return this.descriptionInput;
+    }
+
+    public HtmlInputText getParentInput() {
+        return this.parentInput;
+    }
+
+    public HtmlSelectOneMenu getParentSelect() {
+        return this.parentSelect;
+    }
+
+    public SelectItem[] getParentSelectItems() {
+        return parentSelectItems;
+    }
+
+    //setters
+    public void setNameInput(HtmlInputText nameinput) {
+        this.nameInput = nameinput;
+    }
+
+    public void setDescriptionInput(HtmlInputTextarea descriptioninput) {
+        this.descriptionInput = descriptioninput;
+    }
+
+    public void setParentInput(HtmlInputText parentinput) {
+        this.parentInput = parentinput;
+    }
+
+    public void setParentSelect(HtmlSelectOneMenu parentselect) {
+        this.parentSelect = parentselect;
+    }
+
+    public void setParentSelectItems(SelectItem[] parentselectitems) {
+        this.parentSelectItems = parentselectitems;
+    }
+
+    public String add_action() {
+        VDCGroup vdcgroup = new VDCGroup();
+        vdcgroup.setName((String)nameInput.getValue());
+        vdcgroup.setDescription((String)descriptionInput.getValue());
+        vdcgroup.setParent((new Long(9)));
+        System.out.println("the selected value is " + parentSelect.getValue());
+        vdcGroupService.create(vdcgroup);
+        System.out.println("the property is " + nameInput.getValue() + descriptionInput.getValue());
+        return "Success";
+    }
+
+
+    
+}

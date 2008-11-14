83ec1a518a1ddc237d4ce258a2a0111b27197449
Gustavo Durand <gdurand@hmdc.harvard.edu>
2008-11-14 19:50:05 +0000

    revamped alpha numeric comparator logic to fix bug when multiple sets of numbers and overall handle more cases

--- src/DVN-EJB/src/java/edu/harvard/hmdc/vdcnet/util/AlphaNumericComparator.java
+++ src/DVN-EJB/src/java/edu/harvard/hmdc/vdcnet/util/AlphaNumericComparator.java
@@ -20,7 +20,10 @@
 package edu.harvard.hmdc.vdcnet.util;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
 
 /**
  *
@@ -32,61 +35,116 @@ import java.util.Comparator;
  * @author Ellen Kraffmiller
  */
 public class AlphaNumericComparator implements Comparator, java.io.Serializable {
-    public int compare(Object o1,Object o2) {
-        String str1 = (String)o1;
-        String str2 = (String)o2;
-        BigDecimal num1 = getNumber(str1);
-        BigDecimal num2 = getNumber(str2);
-        if (num1==null) {
-            if (num2==null) {
-                // str1 and str2 both begin with alpha
-                return str1.compareTo(str2);
-            } else {
-                // str1 begins with alpha; str2 begins with number
-                return 1;
-            }
-        } else {
-            if (num2==null) {
-                // str1 begins with number; str2 begins with alpha
-                return -1;
-            } else {
-                // str1 and str2 both begin with number
-                return num1.compareTo(num2);
-            }
-        }
-    }
-    
- 
-    
-    public boolean equals(Object obj) {
-        return (obj instanceof AlphaNumericComparator);
-    }
 
     public AlphaNumericComparator() {
     }
 
-     public BigDecimal getNumber(String s) {
-        char period = '.';
-        char comma = ',';
-        char minus='-';
-        BigDecimal bigDecimal=null;
-        StringBuffer strBuff = new StringBuffer("");
-        char[] charArray = s.trim().toCharArray();
-        for (int i=0;i<charArray.length;i++) {
-            char c =charArray[i];
-            boolean isNextChar = i+1<charArray.length;
-            if(Character.isDigit(c) || 
-                (c==period && isNextChar && Character.isDigit(charArray[i+1])) ||
-                (c==comma  && isNextChar && Character.isDigit(charArray[i+1])) ||
-                (c==minus  && i==0 && isNextChar && Character.isDigit(charArray[i+1]))){
-                strBuff.append(c);
+    public int compare(Object o1, Object o2) {
+        List tokenizedList1 = getTokenizedList((String) o1);
+        List tokenizedList2 = getTokenizedList((String) o2);
+
+        for (int i = 0; i < Math.min(tokenizedList1.size(), tokenizedList2.size()); i++) {
+            Object token1 = tokenizedList1.get(i);
+            Object token2 = tokenizedList2.get(i);
+
+            if (token1 instanceof BigDecimal) {
+                if (token2 instanceof BigDecimal) {
+                    int compareVal = ((BigDecimal) token1).compareTo((BigDecimal) token2);
+                    if (compareVal != 0) {
+                        return compareVal;
+                    }
+
+                } else {
+                    return -1; // token1 is a number, token2 is not
+                }
+
+            } else if (token2 instanceof BigDecimal) {
+                return 1; // token2 is a number, token1 is  not
+
+            } else {
+                int compareVal = ((String) token1).compareTo((String) token2);
+                if (compareVal != 0) {
+                    return compareVal;
+                }
             }
         }
-        String numString = strBuff.toString();
-        if (!numString.equals("")) {
-            bigDecimal = new BigDecimal(numString);
-       }
-       return bigDecimal;
+
+        // they match up, so compare based on who stll has tokens
+        return new Integer(tokenizedList1.size()).compareTo(new Integer(tokenizedList2.size()));
+    }
+
+    /* this method returns a list of the String as tokens of BigDecimals and Strings
+    // a slight challenge is to determine the intent of the user, e.g. a '-' could be for a negative number
+    // or just be for demarcation (e.g A-1, A-2)
+    //
+    // some of the logic incorporated below:
+    // a '.' is part of a number if if it is followed by a digit and there is only one instance
+    // if there are multiple instances in a number (e.g. 1.5.2 the whole thing is treated as a String)
+    // a ',' is part of a number if it is both preceded and followed by a digit (we do not validate that
+    // it is in a proper grouping (i.e. 1,00 is treated as 100, even if that is not a standard way of writing it)
+    // a '-' is only used to denote negative if it is the first character of the String
+    */
+     private List getTokenizedList(String value) {
+        List tokenizedList = new ArrayList();
+        char[] charArray = value.trim().toCharArray();
+
+        StringBuffer currentToken = new StringBuffer("");
+        boolean isCurrentTokenNumeric = false;
+
+        for (int i = 0; i < charArray.length; i++) {
+            char c = charArray[i];
+            boolean hasPrevChar = i > 0;
+            boolean hasNextChar = i < charArray.length - 1;
+
+            if (Character.isDigit(c) ||
+                    (c == '.' && hasNextChar && Character.isDigit(charArray[i + 1])) ||
+                    (c == ',' && hasNextChar && hasPrevChar && Character.isDigit(charArray[i - 1]) && Character.isDigit(charArray[i + 1])) ||
+                    (c == '-' && !hasPrevChar && hasNextChar && Character.isDigit(charArray[i + 1]))) {
+
+                if (!isCurrentTokenNumeric) { // reset
+                    tokenizedList.add(currentToken.toString());
+                    currentToken = new StringBuffer("");
+                }
+
+                if (c != ',') { // if comma, don't append as it's just a visual separator
+                    currentToken.append(c);
+                }
+                isCurrentTokenNumeric = true;
+                
+            } else {
+
+                if (isCurrentTokenNumeric) { // reset
+                    try {
+                        tokenizedList.add(new BigDecimal(currentToken.toString()));
+                    } catch (NumberFormatException nfe) {
+                        tokenizedList.add(currentToken.toString()); // something went wrong, but go ahead and add a a String
+                    }
+                    currentToken = new StringBuffer("");
+                }
+
+                currentToken.append(c);
+                isCurrentTokenNumeric = false;
+            }
+        }
+        
+        // add the last token to the list
+        if (isCurrentTokenNumeric) {
+            try {
+                tokenizedList.add(new BigDecimal(currentToken.toString()));
+            } catch (NumberFormatException nfe) {
+                tokenizedList.add(currentToken.toString()); // something went wrong, but go ahead and add a a String
+            }
+        } else {
+            tokenizedList.add(currentToken.toString());   
+        }
+
+        return tokenizedList;
+
+    }
+
+
+    public boolean equals(Object obj) {
+        return (obj instanceof AlphaNumericComparator);
     }
 
 }

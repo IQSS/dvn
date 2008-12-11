eb940df6970386f26fbeb24f8e975ca4c5a827b1
Wendy Bossons <dvn_support@help.hmdc.harvard.edu>
2008-12-11 22:24:25 +0000

    PagedDataModel class - extends DataModel for HomePage performance improvements.

---
+++ src/DVN-web/src/edu/harvard/hmdc/vdcnet/util/PagedDataModel.java
@@ -0,0 +1,77 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.harvard.hmdc.vdcnet.util;
+
+import java.util.List;
+import javax.faces.model.DataModel;
+
+/**
+ *
+ * @author wbossons
+ */
+public class PagedDataModel extends DataModel {
+    private int rowIndex = -1;
+  
+  private int rowCount;
+  
+  private int pageSize;
+  
+  private List list;
+  
+  public PagedDataModel() {
+    super();
+  }
+  
+  public PagedDataModel(List list, int totalNumRows, int pageSize) {
+    super();
+    setWrappedData(list);
+    this.rowCount = totalNumRows;
+    this.pageSize = pageSize;
+  }
+  
+  public boolean isRowAvailable() {
+    if(list == null)
+      return false;
+    
+    int rowIndex = getRowIndex();
+    if(rowIndex >=0 && rowIndex < list.size())
+      return true;
+    else
+      return false;
+  }
+
+  public int getRowCount() {
+    return rowCount;
+  }
+
+  public Object getRowData() {
+    if(list == null)
+      return null;
+    else if(!isRowAvailable())
+      throw new IllegalArgumentException();
+    else {
+      int dataIndex = getRowIndex();
+      return list.get(dataIndex);
+    }
+  }
+
+  public int getRowIndex() {
+    return (rowIndex % pageSize);
+  }
+
+  public void setRowIndex(int rowIndex) {
+    this.rowIndex = rowIndex;
+  }
+
+  public Object getWrappedData() {
+    return list;
+  }
+  
+  public void setWrappedData(Object list) {
+    this.list = (List) list;
+  }
+ 
+}

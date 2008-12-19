c1b6961d5ce0acf9b6190cd131cd4d7cae43a0b4
Gustavo Durand <gdurand@hmdc.harvard.edu>
2008-12-19 17:20:48 +0000

    reordered build update to have all DDL related changes first, then DML

--- src/DVN-EJB/src/conf/buildupdate_v1_3_v1_4.sql
+++ src/DVN-EJB/src/conf/buildupdate_v1_3_v1_4.sql
@@ -1,26 +1,22 @@
 begin;
 
-UPDATE pagedef SET path=regexp_replace(pagedef.path, '.jsp', '.xhtml', 'g');
+--
+-- first do DDL
+--
 
 -- Column: releasedate
-
 -- ALTER TABLE vdc DROP COLUMN releasedate;
-
 ALTER TABLE vdc ADD COLUMN releasedate timestamp;
 ALTER TABLE vdc ALTER COLUMN releasedate SET STORAGE PLAIN;
 
 -- Column: createddate
-
 -- ALTER TABLE vdc DROP COLUMN createddate;
-
 ALTER TABLE vdc ADD COLUMN createddate timestamp;
 ALTER TABLE vdc ALTER COLUMN createddate SET STORAGE PLAIN;
 
 
 -- Column: dvndescription
-
 -- ALTER TABLE vdc DROP COLUMN dvndescription;
-
 ALTER TABLE vdc ADD COLUMN dvndescription varchar(255);
 ALTER TABLE vdc ALTER COLUMN dvndescription SET STORAGE EXTENDED;
 
@@ -36,9 +32,7 @@ alter table oaiset alter column spec type text;
 alter table oaiset alter column description type text;
 
 -- Column: parent
-
 -- ALTER TABLE vdcgroup DROP COLUMN parent;
-
 ALTER TABLE vdcgroup ADD COLUMN parent int4;
 ALTER TABLE vdcgroup ALTER COLUMN parent SET STORAGE PLAIN;
 
@@ -58,12 +52,16 @@ ALTER TABLE harvestingdataverse ALTER COLUMN failedstudycount SET STORAGE PLAIN;
 ALTER TABLE harvestingdataverse ADD COLUMN harvestresult varchar(255);
 ALTER TABLE harvestingdataverse ALTER COLUMN harvestresult SET STORAGE EXTENDED;
 
-update harvestingdataverse set lastsuccessfulharvesttime=lastharvesttime;
+-- new studyId column (and fk constraint) to studyFileActivity
+ALTER TABLE studyfileactivity ADD COLUMN study_id bigint;
+ALTER TABLE studyfileactivity ALTER COLUMN study_id SET STORAGE PLAIN;
 
-
+ALTER TABLE studyfileactivity
+  ADD CONSTRAINT fk_studyfileactivity_study_id FOREIGN KEY (study_id)
+      REFERENCES study (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 -- Changes to vdccollection table
-
 alter table vdccollection drop column visible;
 
 alter table vdccollection drop column reviewstate_id;
@@ -80,11 +78,27 @@ ALTER TABLE vdccollection ALTER COLUMN localscope SET STORAGE PLAIN;
 ALTER TABLE vdccollection ADD COLUMN type varchar(255);
 ALTER TABLE vdccollection ALTER COLUMN type SET STORAGE EXTENDED;
 
+
+-- new indices
+create index study_owner_id_index on study(owner_id);
+create index filecategory_id_index on filecategory(id);
+create index studyfile_id_index on studyfile(id);
+create index studyfileactivity_id_index on studyfileactivity(id);
+create index studyfileactivity_studyfile_id_index on studyfileactivity(studyfile_id);
+create index studyfileactivity_study_id_index on studyfileactivity(study_id);
+
+--
+-- and now DML
+--
+
+update dvnversion set buildnumber=4;
+
+update harvestingdataverse set lastsuccessfulharvesttime=lastharvesttime;
+
+-- vdccollection chabges
 update vdccollection set localscope = false;
 update vdccollection set type='static' where query is null;
 update vdccollection set type='dynamic' where query is not null;
-
-update dvnversion set buildnumber=4;
 
 
 -- remove owned studies from root collection
@@ -97,6 +111,8 @@ where study_id || '_' || vdc_collection_id in
 );
 
 
+-- pagedef changes
+UPDATE pagedef SET path=regexp_replace(pagedef.path, '.jsp', '.xhtml', 'g');
 delete from pagedef where name = 'AddCollectionsPage';
 update pagedef set name = 'ManageCollectionsPage' where name = 'ManageCollectionPage';
 update pagedef set path = '/collection/ManageCollectionsPage.xhtml' where path = '/collection/ManageCollectionPage.xhtml';
@@ -112,30 +128,12 @@ delete from search_result_fields
 where study_field_id = (select id from studyfield where name = 'productionDate');
 
 
--- new studyId column (and fk constraint) to studyFileActivity
-ALTER TABLE studyfileactivity ADD COLUMN study_id bigint;
-ALTER TABLE studyfileactivity ALTER COLUMN study_id SET STORAGE PLAIN;
-
-ALTER TABLE studyfileactivity
-  ADD CONSTRAINT fk_studyfileactivity_study_id FOREIGN KEY (study_id)
-      REFERENCES study (id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION;
-
 
 -- populate the new column
 update studyfileactivity set study_id = fc.study_id
 from studyfile sf, filecategory fc
 where studyfileactivity.studyfile_id = sf.id
 and sf.filecategory_id = fc.id;
-
-
--- new indices
-create index study_owner_id_index on study(owner_id);
-create index filecategory_id_index on filecategory(id);
-create index studyfile_id_index on studyfile(id);
-create index studyfileactivity_id_index on studyfileactivity(id);
-create index studyfileactivity_studyfile_id_index on studyfileactivity(studyfile_id);
-create index studyfileactivity_study_id_index on studyfileactivity(study_id);
 
 
 -- fix the templatefield data

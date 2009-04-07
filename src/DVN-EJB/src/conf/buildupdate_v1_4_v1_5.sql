f8bfdc4309340d686b9fb2403874a816071a1f32
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2009-04-07 20:38:08 +0000

    Add Creator Request Info and Contributor Request Info to list of pages that are skipped when saving LoginFilter OriginalUrl

--- src/DVN-EJB/src/conf/buildupdate_v1_4_v1_5.sql
+++ src/DVN-EJB/src/conf/buildupdate_v1_4_v1_5.sql
@@ -13,6 +13,10 @@ begin;
 
 update dvnversion set buildnumber=5;
 
+
+INSERT INTO pagedef ( name, path, role_id, networkrole_id ) VALUES ( 'ContributorRequestInfoPage', '/login/ContributorRequestInfoPage.xhtml', null,null );
+INSERT INTO pagedef ( name, path, role_id, networkrole_id ) VALUES ( 'CreatorRequestInfoPage','/login/CreatorRequestInfoPage.xhtml', null,null );
+
 -- remove date of deposit from templates
 update metadata set dateofdeposit = '' where id in (select metadata_id from template);
 

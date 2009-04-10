4e66ab437e8cc913b8eea58b6137b1aa35b9ff29
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2009-04-10 13:58:49 +0000

    fixed logic in timerServer if statement

--- src/DVN-web/src/edu/harvard/hmdc/vdcnet/web/login/LoginPage.java
+++ src/DVN-web/src/edu/harvard/hmdc/vdcnet/web/login/LoginPage.java
@@ -106,7 +106,7 @@ public class LoginPage extends VDCBaseBean implements java.io.Serializable  {
             loginFailed = true;
             errMessage = "Login failed. Please check your username and password and try again.";
             return null;
-        } else if (PropertyUtil.isTimerServer() && user.getNetworkRole()!=null && user.getNetworkRole().getName().equals(NetworkRoleServiceLocal.ADMIN)) {
+        } else if (!PropertyUtil.isTimerServer() && user.getNetworkRole()!=null && user.getNetworkRole().getName().equals(NetworkRoleServiceLocal.ADMIN)) {
            loginFailed = true;
            errMessage = "Login failed.  Users with Network Admin privileges must log into the timer server.";
            return null;

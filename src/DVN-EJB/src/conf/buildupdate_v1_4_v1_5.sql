498848c20dc91906606c8af60c3a20782c52dc82
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2009-04-10 15:24:10 +0000

    updated pagedef data

--- src/DVN-EJB/src/conf/buildupdate_v1_4_v1_5.sql
+++ src/DVN-EJB/src/conf/buildupdate_v1_4_v1_5.sql
@@ -16,6 +16,9 @@ update dvnversion set buildnumber=5;
 
 INSERT INTO pagedef ( name, path, role_id, networkrole_id ) VALUES ( 'ContributorRequestInfoPage', '/login/ContributorRequestInfoPage.xhtml', null,null );
 INSERT INTO pagedef ( name, path, role_id, networkrole_id ) VALUES ( 'CreatorRequestInfoPage','/login/CreatorRequestInfoPage.xhtml', null,null );
+update pagedef set networkrole_id = 2 where name = 'EditHarvestSitePage';
+update pagedef set networkrole_id = 2 where name =  'NetworkOptionsPage';
+update pagedef set networkrole_id = 2 where name =  'HarvestSitesPage';
 
 -- remove date of deposit from templates
 update metadata set dateofdeposit = '' where id in (select metadata_id from template);

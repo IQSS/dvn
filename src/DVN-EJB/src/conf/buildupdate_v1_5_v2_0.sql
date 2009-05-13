e238e1096354042ac2c1e3d4b89b90e30b386f42
Robert Treacy <rtreacy@hmdc.harvard.edu>
2009-05-13 16:53:28 +0000

    add 3 columns -- commentnotificationemail, allowstudycomments, generatenotifications -- to vdc for new study comments function

--- src/DVN-EJB/src/conf/buildupdate_v1_5_v2_0.sql
+++ src/DVN-EJB/src/conf/buildupdate_v1_5_v2_0.sql
@@ -3,7 +3,16 @@
 -- first do DDL (this cannot be rollbacked)
 --
 
---- NONE SO FAR
+-- changes to vdc for study comments
+ALTER TABLE vdc ADD COLUMN commentnotificationemail varchar(255);
+ALTER TABLE vdc ALTER COLUMN commentnotificationemail SET STORAGE EXTENDED;
+
+ALTER TABLE vdc ADD COLUMN allowstudycomments bool;
+ALTER TABLE vdc ALTER COLUMN allowstudycomments SET STORAGE PLAIN;
+
+ALTER TABLE vdc ADD COLUMN generatenotifications bool;
+ALTER TABLE vdc ALTER COLUMN generatenotifications SET STORAGE PLAIN;
+
 
 --
 -- and now DML

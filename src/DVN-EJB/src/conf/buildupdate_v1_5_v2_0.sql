e8f88b59aa84540cae0345d1181a12ada400bd1c
Wendy Bossons <dvn_support@help.hmdc.harvard.edu>
2009-05-15 16:45:14 +0000

    Edit Study Comments page is restricted to the dataverse admin and the network admin.

--- src/DVN-EJB/src/conf/buildupdate_v1_5_v2_0.sql
+++ src/DVN-EJB/src/conf/buildupdate_v1_5_v2_0.sql
@@ -7,6 +7,8 @@
 ALTER TABLE vdc ADD COLUMN allowstudycomments bool default true;
 ALTER TABLE vdc ALTER COLUMN allowstudycomments SET STORAGE PLAIN;
 
+INSERT INTO pagedef ( name, path, role_id, networkrole_id ) VALUES ( 'EditStudyCommentsPage', '/admin/EditStudyCommentsPage.xhtml', 3,null );
+
 --
 -- and now DML
 --

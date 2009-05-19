1e651c9739652165a132d199331a051e584904c7
Alexander D'Amour <dvn_support@help.hmdc.harvard.edu>
2009-05-19 21:55:30 +0000

    Fixed a bug in measures; attached measures to vertices, no the whole graph.

--- src/DSB/R/NetworkUtils/R/measure.R
+++ src/DSB/R/NetworkUtils/R/measure.R
@@ -1,9 +1,9 @@
 add_pagerank <- function(g, d=0.85){
-    g$pagerank <- page.rank(g, damping=d)
+    V(g)$pagerank <- page.rank(g, damping=d)$vector
     return(g)
 }
 
 add_degree <- function(g){
-    g$degree <- degree(g, V(g))
+    V(g)$degree <- degree(g, V(g))
     return(g)
 }

62e916cffee83a4272e1bfb70aa5113f92a42887
Robert Treacy <rtreacy@hmdc.harvard.edu>
2009-05-26 19:16:11 +0000

    getStudyComments get comments by study primary key (id) rather than studyId

--- src/DVN-EJB/src/java/edu/harvard/iq/dvn/core/study/StudyCommentServiceBean.java
+++ src/DVN-EJB/src/java/edu/harvard/iq/dvn/core/study/StudyCommentServiceBean.java
@@ -58,7 +58,7 @@ public class StudyCommentServiceBean implements StudyCommentService {
 
     @Override
     public List <StudyComment> getStudyComments(Long studyId){
-        String studyCommentsByStudyIdQuery = "Select c from StudyComment c where c.study.studyId = :commentStudyId";
+        String studyCommentsByStudyIdQuery = "Select c from StudyComment c where c.study.id = :commentStudyId";
         List<StudyComment> studyComments = em.createQuery(studyCommentsByStudyIdQuery).setParameter("commentStudyId", studyId.toString()).getResultList();
         return studyComments;
     }

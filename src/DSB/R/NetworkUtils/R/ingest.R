4e8a0e5384c33edf7c0b1402828f366361a34938
Alexander D'Amour <dvn_support@help.hmdc.harvard.edu>
2009-05-27 20:36:27 +0000

    Functions for parsing and dumping graphML and loading RData

---
+++ src/DSB/R/NetworkUtils/R/ingest.R
@@ -0,0 +1,18 @@
+ingest_graphml <- function(filename){
+    g <- read.graph(filename, format="graphml")
+    split_file <- unlist(strsplit(filename, '.', fixed=T))
+    binfile <- paste( paste(split_file[-length(split_file)], collapse='.'),
+                      ".RData", sep='')
+    save(g, file=binfile)
+    return(binfile)
+}
+
+load_and_clear <- function(filename){
+    rm(list=ls(envir=parent.frame()),envir=parent.frame())
+    load(filename, envir=parent.frame())
+}
+
+dump_graphml <- function(g, filename){
+    write.graph(filename, file=filename, format="graphml")
+    return(filename)
+}

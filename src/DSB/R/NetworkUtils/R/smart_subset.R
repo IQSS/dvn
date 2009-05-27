404bb3f2cad37ec6d097bc739e7ee5d49d431e08
Alexander D'Amour <dvn_support@help.hmdc.harvard.edu>
2009-05-27 20:44:17 +0000

    Modified to modify a global graph variable 'in place'.

--- src/DSB/R/NetworkUtils/R/smart_subset.R
+++ src/DSB/R/NetworkUtils/R/smart_subset.R
@@ -1,7 +1,8 @@
 #Returns the largest component of a graph as a graph object. Faster than decompose.graph
-component <- function(g,num=1){
-	k <- clusters(g)
+component <- function(tmp_g,num=1){
+	k <- clusters(tmp_g)
 	comp_num <- as.numeric(names(sort(tapply(k$membership, k$membership, length), decreasing=T)[num]))
-	g1 <- subgraph(g, V(g)[(which(k$membership==comp_num)-1)])
-	return(g1)
+	tmp_g <- subgraph(tmp_g, V(tmp_g)[(which(k$membership==comp_num)-1)])
+    g <<- tmp_g 
+	return(c(vcount(tmp_g), ecount(tmp_g)))
 }

4c15bfa14f02b1058ba4be8ef0342af38299f90e
Alexander D'Amour <dvn_support@help.hmdc.harvard.edu>
2009-05-27 20:44:02 +0000

    Modified to modify a global graph variable 'in place'.

--- src/DSB/R/NetworkUtils/R/measure.R
+++ src/DSB/R/NetworkUtils/R/measure.R
@@ -1,9 +1,17 @@
-add_pagerank <- function(g, d=0.85){
-    V(g)$pagerank <- page.rank(g, damping=d)$vector
-    return(g)
+add_pagerank <- function(tmp_g, damping=0.85){
+    tryCatch(pagerank_num <<- pagerank_num+1,
+             error=function(e){pagerank_num <<- 1})
+    meas_name <- paste("pagerank",pagerank_num, sep="_")
+    tmp_g <- set.vertex.attribute(tmp_g, meas_name, V(tmp_g), page.rank(tmp_g, damping=damping)$vector)
+    g <<- tmp_g
+    return(meas_name)
 }
 
-add_degree <- function(g){
-    V(g)$degree <- degree(g, V(g))
-    return(g)
+add_degree <- function(tmp_g){
+    tryCatch(degree_num <<- degree_num+1,
+             error=function(e){degree_num <<- 1})
+    meas_name <- paste("degree",degree_num, sep="_")
+    tmp_g <- set.vertex.attribute(tmp_g, meas_name, V(tmp_g), degree(tmp_g, V(tmp_g)))
+    g <<- tmp_g
+    return(meas_name)
 }

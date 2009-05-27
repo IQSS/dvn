68913d7d1f27d0a19cb8c45cfe9b103bf6be6a8d
Alexander D'Amour <dvn_support@help.hmdc.harvard.edu>
2009-05-27 18:24:27 +0000

    Added query sanitizer which generates errors on dangerous queries.

--- src/DSB/R/NetworkUtils/R/subset.R
+++ src/DSB/R/NetworkUtils/R/subset.R
@@ -1,4 +1,5 @@
 vertex_subset <- function(g, subset_str){
+    subset_str <- clean_subset(subset_str)
     vs <- V(g)[eval(parse(text=subset_str))]
     return(subgraph(g, vs))
 }
@@ -7,6 +8,7 @@ vertex_subset <- function(g, subset_str){
 #Like subgraph, but takes an edgeset and returns a graph that only contains those edges
 #and vertices incident to those edges.
 edge_subset <- function(g, subset_str, drop_disconnected=FALSE){
+    subset_str <- clean_subset(subset_str)
     if(drop_disconnected){
         es <- E(g)[eval(parse(text=subset_str))]
         e <- get.edges(g, es)
@@ -18,4 +20,17 @@ edge_subset <- function(g, subset_str, drop_disconnected=FALSE){
         es <- E(g)[eval(parse(text=paste("!(",subset_str,")")))]
         return(delete.edges(g, es))
     }
+}
+
+clean_subset <- function(subset_str){
+    subset_str <- gsub("||","|",subset_str, fixed=T)
+
+    evil_keywords <- c("system(", "eval(", ";", "]")
+    e <- simpleError("Bad query error.")
+    sapply(evil_keywords, function(k){
+                            if(length(grep(k, subset_str, fixed=T)))
+                                stop(e,sprintf("Query contained blacklisted string \"%s\".",k))
+                          }
+          );
+    return(subset_str)
 }

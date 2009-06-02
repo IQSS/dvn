2e74c0288fb0307e8f2dff4d5823a795af92b165
Wendy Bossons <dvn_support@help.hmdc.harvard.edu>
2009-06-02 15:42:26 +0000

    Added support to  show reported link when the user is a one of its (comment) reporters...

--- src/DVN-web/src/edu/harvard/iq/dvn/core/web/study/StudyCommentUI.java
+++ src/DVN-web/src/edu/harvard/iq/dvn/core/web/study/StudyCommentUI.java
@@ -16,11 +16,19 @@ import java.util.List;
  */
 public class StudyCommentUI {
 
+    protected boolean InFlaggedByUsers;
     protected StudyComment studyComment;
     protected String flaggedByUserNames = new String("");
+    protected VDCUser user = null;
+
 
     public StudyCommentUI(StudyComment studyComment) {
         this.studyComment = studyComment;
+    }
+
+    public StudyCommentUI(StudyComment studyComment, VDCUser currentUser) {
+        this.studyComment = studyComment;
+        this.user         = currentUser;
     }
 
     public String getFlaggedByUserNames() {
@@ -37,6 +45,57 @@ public class StudyCommentUI {
 
     public StudyComment getStudyComment() {
         return this.studyComment;
+    }
+
+
+
+    /**
+     * Get the value of InFlaggedByUsers
+     *
+     * @return the value of InFlaggedByUsers
+     */
+    public boolean isInFlaggedByUsers() {
+        if (getFlaggedByUserNames().indexOf(user.getUserName()) != -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Set the value of InFlaggedByUsers
+     *
+     * @param InFlaggedByUsers new value of InFlaggedByUsers
+     */
+    public void setInFlaggedByUsers(boolean InFlaggedByUsers) {
+        this.InFlaggedByUsers = InFlaggedByUsers;
+    }
+
+    protected boolean reportAbuseEnabled;
+
+    /**
+     * Get the value of reportAbuseEnabled
+     *
+     * @return the value of reportAbuseEnabled
+     */
+    public boolean isReportAbuseEnabled() {
+        if (studyComment.getStatus().toString().equals("FLAGGED") && isInFlaggedByUsers()) {
+            reportAbuseEnabled = false;
+        } else {
+            reportAbuseEnabled = true;
+        }
+        return reportAbuseEnabled;
+    }
+
+
+
+    /**
+     * Set the value of reportAbuseEnabled
+     *
+     * @param reportAbuseEnabled new value of reportAbuseEnabled
+     */
+    public void setReportAbuseEnabled(boolean reportAbuseEnabled) {
+        this.reportAbuseEnabled = reportAbuseEnabled;
     }
 
 

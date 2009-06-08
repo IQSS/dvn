0099eaec9ef5bedeff24fbaade20268dc4a2711b
Gustavo Durand <gdurand@hmdc.harvard.edu>
2009-06-08 21:10:56 +0000

    added paramters for network measures

--- src/DVN-EJB/src/java/edu/harvard/iq/dvn/core/analysis/NetworkDataServiceLocal.java
+++ src/DVN-EJB/src/java/edu/harvard/iq/dvn/core/analysis/NetworkDataServiceLocal.java
@@ -7,7 +7,7 @@ package edu.harvard.iq.dvn.core.analysis;
 
 import edu.harvard.iq.dvn.core.study.StudyFileEditBean;
 import java.io.File;
-import java.util.Map;
+import java.util.List;
 import javax.ejb.Local;
 
 /**
@@ -21,7 +21,7 @@ public interface NetworkDataServiceLocal extends java.io.Serializable {
     
     public NetworkDataSubsetResult runManualQuery(String rWorkspace, String attributeSet, String query, boolean eliminateDisconnectedVertices);
     public NetworkDataSubsetResult runAutomaticQuery(String rWorkspace, String automaticQuery, String nValue);
-    public String runNetworkMeasure(String rWorkspace, String networkMeasure, Map<String,String> parameters);
+    public String runNetworkMeasure(String rWorkspace, String networkMeasure, List<NetworkMeasureParameter> parameters);
 
     public File getSubsetExport(String rWorkspace);
 

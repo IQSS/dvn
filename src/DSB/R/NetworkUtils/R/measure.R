17755b11ac3dd1522df1162bdfd6f29cb58eaf8f
Alexander D'Amour <dvn_support@help.hmdc.harvard.edu>
2009-06-15 16:57:14 +0000

    Added unique_degree function and support for the undo function.

--- src/DSB/R/NetworkUtils/R/measure.R
+++ src/DSB/R/NetworkUtils/R/measure.R
@@ -14,6 +14,7 @@ next_measure_name <- function(g, measure){
 }
 
 add_pagerank <- function(tmp_g, damping=0.85){
+    g_last <<- tmp_g
     meas_name <- next_measure_name(tmp_g, "pagerank") 
     tmp_g <- set.vertex.attribute(tmp_g, meas_name, V(tmp_g), page.rank(tmp_g, damping=damping)$vector)
     g <<- tmp_g
@@ -21,13 +22,23 @@ add_pagerank <- function(tmp_g, damping=0.85){
 }
 
 add_degree <- function(tmp_g){
+    g_last <<- tmp_g
     meas_name <- next_measure_name(tmp_g, "degree")
     tmp_g <- set.vertex.attribute(tmp_g, meas_name, V(tmp_g), degree(tmp_g, V(tmp_g)))
     g <<- tmp_g
     return(meas_name)
 }
 
+add_unique_degree <- function(tmp_g){
+    g_last <<- tmp_g
+    meas_name <- next_measure_name(tmp_g, "unique_degree")
+    tmp_g <- set.vertex.attribute(tmp_g, meas_name, V(tmp_g), degree(simplify(tmp_g), V(tmp_g)))
+    g <<- tmp_g
+    return(meas_name)
+}
+
 add_in_largest_component <- function(tmp_g){
+    g_last <<- tmp_g
     meas_name <- next_measure_name(tmp_g, "in_largest_component") 
 
     k <- clusters(tmp_g)

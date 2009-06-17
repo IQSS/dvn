011f053a2282a1413c66199845f38ae5102be8b9
Alexander D'Amour <dvn_support@help.hmdc.harvard.edu>
2009-06-17 23:48:59 +0000

    Added bonacich centrality.

--- src/DSB/R/NetworkUtils/R/measure.R
+++ src/DSB/R/NetworkUtils/R/measure.R
@@ -50,3 +50,50 @@ add_in_largest_component <- function(tmp_g){
     g <<- tmp_g
     return(meas_name)
 }
+
+add_bonacich_centrality <- function(tmp_g, alpha=0, exo=1){
+    g_last <<- tmp_g
+    meas_name <- next_measure_name(tmp_g, "bonacich_centrality")
+
+    tmp_g <- set.vertex.attribute(tmp_g, meas_name, V(tmp_g),
+                alpha.centrality.sparse(tmp_g, alpha=alpha, exo=exo))
+
+    g <<- tmp_g
+    return(meas_name)
+}
+
+#Basically just bonpow.sparse recipe from http://igraph.wikidot.com without 
+#node selection.
+alpha.centrality.sparse <-
+    function(graph, alpha=1, exo=1, loops=F, rescale=T, tol=.Machine$double.eps){
+
+    ev <- evcent(g)$value
+    if(alpha >= 1)
+        stop("alpha must be greater than or equal to 0 and strictly less than 1.")
+    if(alpha > 1/ev)
+        warning(sprintf("alpha values larger than the inverse of the dominant eigenvalue cause unpredictable behavior. Consider trying a value between 0 and %f.", 1/ev)) 
+    else if(alpha < 0)
+        stop("alpha must be between 0 and 1.")
+
+    require(Matrix)
+    
+    if(!loops) {
+        graph <- simplify(graph, remove.multiple=F, remove.loops=T)
+    }
+
+    exo <- rep(exo, vcount(g))
+    exo <- as.matrix(exo, nc=1)
+
+    d <- get.adjacency(graph, sparse=T)
+
+    id <- spMatrix(vcount(graph), vcount(graph),
+                    i=1:vcount(graph), j=1:vcount(graph), rep(1, vcount(graph)))
+    id <- as(id, "dgCMatrix")
+
+    ev <- solve(id - alpha * d, tol=tol) %*% exo 
+
+    if(rescale)
+        ev <- ev/sum(ev)
+
+    return(as.numeric(ev))
+}

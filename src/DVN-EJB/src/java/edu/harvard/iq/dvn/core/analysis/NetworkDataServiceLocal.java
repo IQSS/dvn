e38d0962a6548fc6002c2862cdfab57f51069a32
Gustavo Durand <gdurand@hmdc.harvard.edu>
2009-06-29 19:01:48 +0000

    new error handling (part 1)

--- src/DVN-EJB/src/java/edu/harvard/iq/dvn/core/analysis/NetworkDataServiceLocal.java
+++ src/DVN-EJB/src/java/edu/harvard/iq/dvn/core/analysis/NetworkDataServiceLocal.java
@@ -6,6 +6,7 @@
 package edu.harvard.iq.dvn.core.analysis;
 
 import edu.harvard.iq.dvn.core.study.StudyFileEditBean;
+import edu.harvard.iq.dvn.ingest.dsb.impl.DvnRGraphServiceImpl.DvnRGraphException;
 import java.io.File;
 import java.util.List;
 import javax.ejb.Local;
@@ -17,13 +18,13 @@ import javax.ejb.Local;
 @Local
 public interface NetworkDataServiceLocal extends java.io.Serializable {
 
-    public String initAnalysis(String fileLocation) throws Exception;
+    public String initAnalysis(String fileLocation) throws DvnRGraphException;
     
-    public NetworkDataSubsetResult runManualQuery(String rWorkspace, String attributeSet, String query, boolean eliminateDisconnectedVertices) throws Exception;
-    public NetworkDataSubsetResult runAutomaticQuery(String rWorkspace, String automaticQuery, String nValue) throws Exception;
-    public String runNetworkMeasure(String rWorkspace, String networkMeasure, List<NetworkMeasureParameter> parameters) throws Exception;
+    public NetworkDataSubsetResult runManualQuery(String rWorkspace, String attributeSet, String query, boolean eliminateDisconnectedVertices) throws DvnRGraphException;
+    public NetworkDataSubsetResult runAutomaticQuery(String rWorkspace, String automaticQuery, String nValue) throws DvnRGraphException;
+    public String runNetworkMeasure(String rWorkspace, String networkMeasure, List<NetworkMeasureParameter> parameters) throws DvnRGraphException;
 
-    public void undoLastEvent(String rWorkspace) throws Exception;
+    public void undoLastEvent(String rWorkspace) throws DvnRGraphException;
             
     public File getSubsetExport(String rWorkspace) throws Exception;
 

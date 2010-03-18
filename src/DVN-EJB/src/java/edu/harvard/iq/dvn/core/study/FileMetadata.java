63af0545bebe0dc8a62aa5b8b25ca9455bd0cd3d
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2010-03-18 15:28:08 +0000

    add null contraints for required relationships

--- src/DVN-EJB/src/java/edu/harvard/iq/dvn/core/study/FileMetadata.java
+++ src/DVN-EJB/src/java/edu/harvard/iq/dvn/core/study/FileMetadata.java
@@ -6,12 +6,12 @@
 package edu.harvard.iq.dvn.core.study;
 
 import java.io.Serializable;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Version;
 
@@ -30,8 +30,10 @@ public class FileMetadata implements Serializable {
     private String category = "";
 
     @ManyToOne
+    @JoinColumn(nullable=false)
     private StudyVersion studyVersion;
     @ManyToOne
+    @JoinColumn(nullable=false)
     private StudyFile studyFile;
 
     public String getLabel() {

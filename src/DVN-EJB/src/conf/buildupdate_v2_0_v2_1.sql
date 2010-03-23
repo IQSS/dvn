b1dbadd18d6993f9b48e34b2605f133caaa7c5af
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2010-03-23 16:05:07 +0000

    add null contraints for required relationships

--- src/DVN-EJB/src/conf/buildupdate_v2_0_v2_1.sql
+++ src/DVN-EJB/src/conf/buildupdate_v2_0_v2_1.sql
@@ -41,9 +41,70 @@ commit;
 --
 -- lastly, remove some unneeded colums and add some more constraints
 --
-
+ALTER TABLE studycomment ALTER COLUMN studyversion_id SET NOT NULL;
 ALTER TABLE studycomment DROP COLUMN study_id;
 ALTER TABLE studycomment ALTER COLUMN studyversion_id SET NOT NULL;
 
 ALTER TABLE studyfile DROP COLUMN filecategory_id;
 alter table study drop column metadata_id;
+
+--
+-- End version related changees
+--
+
+--
+-- New columns needed for contributor settings
+--
+ALTER TABLE vdc ADD COLUMN allowcontributorseditall boolean;
+ALTER TABLE vdc ALTER COLUMN allowcontributorseditall SET STORAGE PLAIN;
+
+ALTER TABLE vdc ADD COLUMN allowregistereduserstocontribute boolean;
+ALTER TABLE vdc ALTER COLUMN allowregistereduserstocontribute SET STORAGE PLAIN;
+
+update vdc set allowcontributorseditall = false;
+update vdc set allowregistereduserstocontribute = false;
+
+--
+-- Add null constraints to existing columns
+--
+ALTER TABLE datatable ALTER COLUMN studyfile_id SET NOT NULL;
+ALTER TABLE datavariable ALTER COLUMN datatable_id SET NOT NULL;
+ALTER TABLE datavariable ALTER COLUMN variableformattype_id SET NOT NULL;
+ALTER TABLE harvestingdataverse ALTER COLUMN harvestformattype_id SET NOT NULL;
+ALTER TABLE loginaffiliate ALTER COLUMN usergroup_id SET NOT NULL;
+ALTER TABLE logindomain ALTER COLUMN usergroup_id SET NOT NULL;
+ALTER TABLE networkrolerequest ALTER COLUMN vdcuser_id SET NOT NULL;
+ALTER TABLE networkrolerequest ALTER COLUMN networkrole_id SET NOT NULL;
+ALTER TABLE rolerequest ALTER COLUMN vdcuser_id SET NOT NULL;
+ALTER TABLE rolerequest ALTER COLUMN vdc_id SET NOT NULL;
+ALTER TABLE rolerequest ALTER COLUMN role_id SET NOT NULL;
+ALTER TABLE studyaccessrequest ALTER COLUMN vdcuser_id SET NOT NULL;
+ALTER TABLE studyaccessrequest ALTER COLUMN study_id SET NOT NULL;
+ALTER TABLE studyrequest ALTER COLUMN vdcuser_id SET NOT NULL;
+ALTER TABLE studyrequest ALTER COLUMN study_id SET NOT NULL;
+ALTER TABLE studycomment ALTER COLUMN commentcreator_id SET NOT NULL;
+ALTER TABLE studydistributor ALTER COLUMN metadata_id SET NOT NULL;
+ALTER TABLE studyfileactivity ALTER COLUMN studyfile_id SET NOT NULL;
+ALTER TABLE studyfileactivity ALTER COLUMN study_id SET NOT NULL;
+ALTER TABLE studylock ALTER COLUMN user_id SET NOT NULL;
+ALTER TABLE studylock ALTER COLUMN study_id SET NOT NULL;
+ALTER TABLE summarystatistic ALTER COLUMN type_id SET NOT NULL;
+ALTER TABLE summarystatistic ALTER COLUMN datavariable_id SET NOT NULL;
+ALTER TABLE study ALTER COLUMN creator_id SET NOT NULL;
+ALTER TABLE study ALTER COLUMN template_id SET NOT NULL;
+ALTER TABLE study ALTER COLUMN owner_id SET NOT NULL;
+ALTER TABLE vdc ALTER COLUMN creator_id SET NOT NULL;
+ALTER TABLE vdc ALTER COLUMN defaulttemplate_id SET NOT NULL;
+ALTER TABLE templatefield ALTER COLUMN fieldinputlevel_id SET NOT NULL;
+ALTER TABLE templatefield ALTER COLUMN template_id SET NOT NULL;
+ALTER TABLE templatefield ALTER COLUMN studyfield_id SET NOT NULL;
+ALTER TABLE templatefieldselectvalue ALTER COLUMN templatefield_id SET NOT NULL;
+ALTER TABLE termsofuse ALTER COLUMN vdc_id SET NOT NULL;
+ALTER TABLE variablecategory ALTER COLUMN datavariable_id SET NOT NULL;
+ALTER TABLE variablerange ALTER COLUMN datavariable_id SET NOT NULL;
+ALTER TABLE variablerangeitem ALTER COLUMN datavariable_id SET NOT NULL;
+ALTER TABLE vdcgrouprelationship ALTER COLUMN subgroup_id SET NOT NULL;
+ALTER TABLE vdcgrouprelationship ALTER COLUMN group_id SET NOT NULL;
+ALTER TABLE vdcrole ALTER COLUMN vdcuser_id SET NOT NULL;
+ALTER TABLE vdcrole ALTER COLUMN vdc_id SET NOT NULL;
+ALTER TABLE vdcrole ALTER COLUMN role_id SET NOT NULL;

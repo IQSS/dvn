1340cf29f65e0221d43ac2f395ebfc15f7669a60
Michael Heppler <mheppler@hmdc.harvard.edu>
2010-04-13 15:23:50 +0000

    Update for accordion plugin for new jquery library.

--- src/DVN-web/web/resources/javascript/jquery.accordion.js
+++ src/DVN-web/web/resources/javascript/jquery.accordion.js
@@ -1,272 +1,311 @@
 /*
- * Accordion 1.3 - jQuery menu widget
+ * jQuery UI Accordion 1.6
+ * 
+ * Copyright (c) 2007 JÃ¶rn Zaefferer
  *
- * Copyright (c) 2006 Jörn Zaefferer, Frank Marcia
+ * http://docs.jquery.com/UI/Accordion
  *
  * Dual licensed under the MIT and GPL licenses:
  *   http://www.opensource.org/licenses/mit-license.php
  *   http://www.gnu.org/licenses/gpl.html
  *
- * Revision: $Id: jquery.accordion.js 1524 2007-03-13 20:09:19Z joern $
+ * Revision: $Id: jquery.accordion.js 4876 2008-03-08 11:49:04Z joern.zaefferer $
  *
  */
 
-/**
- * Make the selected elements Accordion widgets.
- *
- * Semantic requirements:
- *
- * If the structure of your container is flat with unique
- * tags for header and content elements, eg. a definition list
- * (dl > dt + dd), you don't have to specify any options at
- * all.
- *
- * If your structure uses the same elements for header and
- * content or uses some kind of nested structure, you have to
- * specify the header elements, eg. via class, see the second example.
- *
- * Use activate(Number) to change the active content programmatically.
- *
- * A change event is triggered everytime the accordion changes. Apart from
- * the event object, all arguments are jQuery objects.
- * Arguments: event, newHeader, oldHeader, newContent, oldContent
- *
- * @example jQuery('#nav').Accordion();
- * @before <dl id="nav">
- *   <dt>Header 1</dt>
- *   <dd>Content 1</dd>
- *   <dt>Header 2</dt>
- *   <dd>Content 2</dd>
- * </dl>
- * @desc Creates an Accordion from the given definition list
- *
- * @example jQuery('#nav').Accordion({
- *   header: 'div.title'
- * });
- * @before <div id="nav">
- *  <div>
- *    <div class="title">Header 1</div>
- *    <div>Content 1</div>
- *  </div>
- *  <div>
- *    <div class="title">Header 2</div>
- *    <div>Content 2</div>
- *  </div>
- * </div>
- * @desc Creates an Accordion from the given div structure
- *
- * @example jQuery('#nav').Accordion({
- *   header: 'a.head'
- * });
- * @before <ul id="nav">
- *   <li>
- *     <a class="head">Header 1</a>
- *     <ul>
- *       <li><a href="#">Link 1</a></li>
- *       <li><a href="#">Link 2></a></li>
- *     </ul>
- *   </li>
- *   <li>
- *     <a class="head">Header 2</a>
- *     <ul>
- *       <li><a href="#">Link 3</a></li>
- *       <li><a href="#">Link 4></a></li>
- *     </ul>
- *   </li>
- * </ul>
- * @desc Creates an Accordion from the given navigation list
- *
- * @example jQuery('#accordion').Accordion().change(function(event, newHeader, oldHeader, newContent, oldContent) {
- *   jQuery('#status').html(newHeader.text());
- * });
- * @desc Updates the element with id status with the text of the selected header every time the accordion changes
- *
- * @param Map options key/value pairs of optional settings.
- * @option String|Element|jQuery|Boolean active Selector for the active element, default is the first child, set to false to display none at start
- * @option String|Element|jQuery header Selector for the header element, eg. div.title, a.head, default is the first child's tagname
- * @option String|Number showSpeed Speed for the slideIn, default is 'slow' (for numbers: smaller = faster)
- * @option String|Number hideSpeed Speed for the slideOut, default is 'fast' (for numbers: smaller = faster)
- * @option String selectedClass Class for active header elements, default is 'selected'
- * @option Boolean alwaysOpen Whether there must be one content element open, default is true.
- * @option Boolean animated Set to false to disable animations. Default: true
- * @option String event The event on which to trigger the accordion, eg. "mouseover". Default: "click"
- *
- * @type jQuery
- * @see activate(Number)
- * @name Accordion
- * @cat Plugins/Accordion
- */
+;(function($) {
+	
+// If the UI scope is not available, add it
+$.ui = $.ui || {};
 
-/**
- * Activate a content part of the Accordion programmatically at the given zero-based index.
- *
- * If the index is not specified, it defaults to zero, if it is an invalid index, eg. a string,
- * nothing happens.
- *
- * @example jQuery('#accordion').activate(1);
- * @desc Activate the second content of the Accordion contained in <div id="accordion">.
- *
- * @example jQuery('#nav').activate();
- * @desc Activate the first content of the Accordion contained in <ul id="nav">.
- *
- * @param Number index (optional) An Integer specifying the zero-based index of the content to be
- *				 activated. Default: 0
- *
- * @type jQuery
- * @name activate
- * @cat Plugins/Accordion
- */
- 
-/**
- * Override the default settings of the Accordion. Affects only following plugin calls.
- *
- * @example jQuery.Accordion.setDefaults({
- * 	showSpeed: 1000,
- * 	hideSpeed: 150
- * });
- *
- * @param Map options key/value pairs of optional settings, see Accordion() for details
- *
- * @type jQuery
- * @name setDefaults
- * @cat Plugins/Accordion
- */
+$.fn.extend({
+	accordion: function(options, data) {
+		var args = Array.prototype.slice.call(arguments, 1);
 
-jQuery.fn.extend({
-	// nextUntil is necessary, would be nice to have this in jQuery core
-	nextUntil: function(expr) {
-	    var match = [];
-	
-	    // We need to figure out which elements to push onto the array
-	    this.each(function(){
-	        // Traverse through the sibling nodes
-	        for( var i = this.nextSibling; i; i = i.nextSibling ) {
-	            // Make sure that we're only dealing with elements
-	            if ( i.nodeType != 1 ) continue;
-	
-	            // If we find a match then we need to stop
-	            if ( jQuery.filter( expr, [i] ).r.length ) break;
-	
-	            // Otherwise, add it on to the stack
-	            match.push( i );
-	        }
-	    });
-	
-	    return this.pushStack( match );
+		return this.each(function() {
+			if (typeof options == "string") {
+				var accordion = $.data(this, "ui-accordion");
+				accordion[options].apply(accordion, args);
+			// INIT with optional options
+			} else if (!$(this).is(".ui-accordion"))
+				$.data(this, "ui-accordion", new $.ui.accordion(this, options));
+		});
 	},
-	// the plugin method itself
-	Accordion: function(settings) {
-		// setup configuration
-		settings = jQuery.extend({}, jQuery.Accordion.defaults, {
-			// define context defaults
-			header: jQuery(':first-child', this)[0].tagName // take first childs tagName as header
-		}, settings);
-
-		// calculate active if not specified, using the first header
-		var container = this,
-			active = settings.active
-				? jQuery(settings.active, this)
-				: settings.active === false
-					? jQuery("<div>")
-					: jQuery(settings.header, this).eq(0),
-			running = 0;
-
-		container.find(settings.header)
-			.not(active || "")
-			.nextUntil(settings.header)
-			.hide();
-		active.addClass(settings.selectedClass);
-
-		function clickHandler(event) {
-			// get the click target
-			var clicked = jQuery(event.target);
-			
-			// due to the event delegation model, we have to check if one
-			// of the parent elements is our actual header, and find that
-			if ( clicked.parents(settings.header).length ) {
-				while ( !clicked.is(settings.header) ) {
-					clicked = clicked.parent();
-				}
-			}
-			
-			var clickedActive = clicked[0] == active[0];
-			
-			// if animations are still active, or the active header is the target, ignore click
-			if(running || (settings.alwaysOpen && clickedActive) || !clicked.is(settings.header))
-				return;
-
-			// switch classes
-			active.toggleClass(settings.selectedClass);
-			if ( !clickedActive ) {
-				clicked.addClass(settings.selectedClass);
-			}
-
-			// find elements to show and hide
-			var toShow = clicked.nextUntil(settings.header),
-				toHide = active.nextUntil(settings.header),
-				data = [clicked, active, toShow, toHide];
-			active = clickedActive ? jQuery([]) : clicked;
-			// count elements to animate
-			running = toHide.size() + toShow.size();
-			var finished = function(cancel) {
-				running = cancel ? 0 : --running;
-				if ( running )
-					return;
-
-				// trigger custom change event
-				container.trigger("change", data);
-			};
-			// TODO if hideSpeed is set to zero, animations are crappy
-			// workaround: use hide instead
-			// solution: animate should check for speed of 0 and do something about it
-			if ( settings.animated ) {
-				if ( !settings.alwaysOpen && clickedActive ) {
-					toShow.slideToggle(settings.showSpeed);
-					finished(true);
-				} else {
-					toHide.filter(":hidden").each(finished).end().filter(":visible").slideUp(settings.hideSpeed, finished);
-					toShow.slideDown(settings.showSpeed, finished);
-				}
-			} else {
-				if ( !settings.alwaysOpen && clickedActive ) {
-					toShow.toggle();
-				} else {
-					toHide.hide();
-					toShow.show();
-				}
-				finished(true);
-			}
-
-			return false;
-		};
-		function activateHandlder(event, index) {
-			// call clickHandler with custom event
-			clickHandler({
-				target: jQuery(settings.header, this)[index]
-			});
-		};
-
-		return container
-			.bind(settings.event, clickHandler)
-			.bind("activate", activateHandlder);
-	},
-	// programmatic triggering
+	// deprecated, use accordion("activate", index) instead
 	activate: function(index) {
-		return this.trigger('activate', [index || 0]);
+		return this.accordion("activate", index);
 	}
 });
 
-jQuery.Accordion = {};
-jQuery.extend(jQuery.Accordion, {
+$.ui.accordion = function(container, options) {
+	
+	// setup configuration
+	this.options = options = $.extend({}, $.ui.accordion.defaults, options);
+	this.element = container;
+	
+	$(container).addClass("ui-accordion");
+	
+	if ( options.navigation ) {
+		var current = $(container).find("a").filter(options.navigationFilter);
+		if ( current.length ) {
+			if ( current.filter(options.header).length ) {
+				options.active = current;
+			} else {
+				options.active = current.parent().parent().prev();
+				current.addClass("current");
+			}
+		}
+	}
+	
+	// calculate active if not specified, using the first header
+	options.headers = $(container).find(options.header);
+	options.active = findActive(options.headers, options.active);
+
+	if ( options.fillSpace ) {
+		var maxHeight = $(container).parent().height();
+		options.headers.each(function() {
+			maxHeight -= $(this).outerHeight();
+		});
+		var maxPadding = 0;
+		options.headers.next().each(function() {
+			maxPadding = Math.max(maxPadding, $(this).innerHeight() - $(this).height());
+		}).height(maxHeight - maxPadding);
+	} else if ( options.autoheight ) {
+		var maxHeight = 0;
+		options.headers.next().each(function() {
+			maxHeight = Math.max(maxHeight, $(this).outerHeight());
+		}).height(maxHeight);
+	}
+
+	options.headers
+		.not(options.active || "")
+		.next()
+		.hide();
+	options.active.parent().andSelf().addClass(options.selectedClass);
+	
+	if (options.event)
+		$(container).bind((options.event) + ".ui-accordion", clickHandler);
+};
+
+$.ui.accordion.prototype = {
+	activate: function(index) {
+		// call clickHandler with custom event
+		clickHandler.call(this.element, {
+			target: findActive( this.options.headers, index )[0]
+		});
+	},
+	
+	enable: function() {
+		this.options.disabled = false;
+	},
+	disable: function() {
+		this.options.disabled = true;
+	},
+	destroy: function() {
+		this.options.headers.next().css("display", "");
+		if ( this.options.fillSpace || this.options.autoheight ) {
+			this.options.headers.next().css("height", "");
+		}
+		$.removeData(this.element, "ui-accordion");
+		$(this.element).removeClass("ui-accordion").unbind(".ui-accordion");
+	}
+}
+
+function scopeCallback(callback, scope) {
+	return function() {
+		return callback.apply(scope, arguments);
+	};
+}
+
+function completed(cancel) {
+	// if removed while animated data can be empty
+	if (!$.data(this, "ui-accordion"))
+		return;
+	var instance = $.data(this, "ui-accordion");
+	var options = instance.options;
+	options.running = cancel ? 0 : --options.running;
+	if ( options.running )
+		return;
+	if ( options.clearStyle ) {
+		options.toShow.add(options.toHide).css({
+			height: "",
+			overflow: ""
+		});
+	}
+	$(this).triggerHandler("change.ui-accordion", [options.data], options.change);
+}
+
+function toggle(toShow, toHide, data, clickedActive, down) {
+	var options = $.data(this, "ui-accordion").options;
+	options.toShow = toShow;
+	options.toHide = toHide;
+	options.data = data;
+	var complete = scopeCallback(completed, this);
+	
+	// count elements to animate
+	options.running = toHide.size() == 0 ? toShow.size() : toHide.size();
+	
+	if ( options.animated ) {
+		if ( !options.alwaysOpen && clickedActive ) {
+			$.ui.accordion.animations[options.animated]({
+				toShow: jQuery([]),
+				toHide: toHide,
+				complete: complete,
+				down: down,
+				autoheight: options.autoheight
+			});
+		} else {
+			$.ui.accordion.animations[options.animated]({
+				toShow: toShow,
+				toHide: toHide,
+				complete: complete,
+				down: down,
+				autoheight: options.autoheight
+			});
+		}
+	} else {
+		if ( !options.alwaysOpen && clickedActive ) {
+			toShow.toggle();
+		} else {
+			toHide.hide();
+			toShow.show();
+		}
+		complete(true);
+	}
+}
+
+function clickHandler(event) {
+	var options = $.data(this, "ui-accordion").options;
+	if (options.disabled)
+		return false;
+	
+	// called only when using activate(false) to close all parts programmatically
+	if ( !event.target && !options.alwaysOpen ) {
+		options.active.parent().andSelf().toggleClass(options.selectedClass);
+		var toHide = options.active.next(),
+			data = {
+				instance: this,
+				options: options,
+				newHeader: jQuery([]),
+				oldHeader: options.active,
+				newContent: jQuery([]),
+				oldContent: toHide
+			},
+			toShow = options.active = $([]);
+		toggle.call(this, toShow, toHide, data );
+		return false;
+	}
+	// get the click target
+	var clicked = $(event.target);
+	
+	// due to the event delegation model, we have to check if one
+	// of the parent elements is our actual header, and find that
+	if ( clicked.parents(options.header).length )
+		while ( !clicked.is(options.header) )
+			clicked = clicked.parent();
+	
+	var clickedActive = clicked[0] == options.active[0];
+	
+	// if animations are still active, or the active header is the target, ignore click
+	if (options.running || (options.alwaysOpen && clickedActive))
+		return false;
+	if (!clicked.is(options.header))
+		return;
+
+	// switch classes
+	options.active.parent().andSelf().toggleClass(options.selectedClass);
+	if ( !clickedActive ) {
+		clicked.parent().andSelf().addClass(options.selectedClass);
+	}
+
+	// find elements to show and hide
+	var toShow = clicked.next(),
+		toHide = options.active.next(),
+		//data = [clicked, options.active, toShow, toHide],
+		data = {
+			instance: this,
+			options: options,
+			newHeader: clicked,
+			oldHeader: options.active,
+			newContent: toShow,
+			oldContent: toHide
+		},
+		down = options.headers.index( options.active[0] ) > options.headers.index( clicked[0] );
+	
+	options.active = clickedActive ? $([]) : clicked;
+	toggle.call(this, toShow, toHide, data, clickedActive, down );
+
+	return false;
+};
+
+function findActive(headers, selector) {
+	return selector != undefined
+		? typeof selector == "number"
+			? headers.filter(":eq(" + selector + ")")
+			: headers.not(headers.not(selector))
+		: selector === false
+			? $([])
+			: headers.filter(":eq(0)");
+}
+
+$.extend($.ui.accordion, {
 	defaults: {
 		selectedClass: "selected",
-		showSpeed: 'slow',
-		hideSpeed: 'fast',
 		alwaysOpen: true,
-		animated: true,
-		event: "click"
+		animated: 'slide',
+		event: "click",
+		header: "a",
+		autoheight: true,
+		running: 0,
+		navigationFilter: function() {
+			return this.href.toLowerCase() == location.href.toLowerCase();
+		}
 	},
-	setDefaults: function(settings) {
-		jQuery.extend(jQuery.Accordion.defaults, settings);
+	animations: {
+		slide: function(options, additions) {
+			options = $.extend({
+				easing: "swing",
+				duration: 300
+			}, options, additions);
+			if ( !options.toHide.size() ) {
+				options.toShow.animate({height: "show"}, options);
+				return;
+			}
+			var hideHeight = options.toHide.height(),
+				showHeight = options.toShow.height(),
+				difference = showHeight / hideHeight;
+			options.toShow.css({ height: 0, overflow: 'hidden' }).show();
+			options.toHide.filter(":hidden").each(options.complete).end().filter(":visible").animate({height:"hide"},{
+				step: function(now) {
+					var current = (hideHeight - now) * difference;
+					if ($.browser.msie || $.browser.opera) {
+						current = Math.ceil(current);
+					}
+					options.toShow.height( current );
+				},
+				duration: options.duration,
+				easing: options.easing,
+				complete: function() {
+					if ( !options.autoheight ) {
+						options.toShow.css("height", "auto");
+					}
+					options.complete();
+				}
+			});
+		},
+		bounceslide: function(options) {
+			this.slide(options, {
+				easing: options.down ? "bounceout" : "swing",
+				duration: options.down ? 1000 : 200
+			});
+		},
+		easeslide: function(options) {
+			this.slide(options, {
+				easing: "easeinout",
+				duration: 700
+			})
+		}
 	}
 });
+
+})(jQuery);

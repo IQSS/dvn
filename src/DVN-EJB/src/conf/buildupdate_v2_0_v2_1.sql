c18a41dc2bcd34da5b659b1059379c066a793a39
Gustavo Durand <gdurand@hmdc.harvard.edu>
2010-04-26 21:44:40 +0000

    reordered statements to be DDL, then DML (and lastly some cleanup; added dates for studyversion; added statements for versioncontributors

--- src/DVN-EJB/src/conf/buildupdate_v2_0_v2_1.sql
+++ src/DVN-EJB/src/conf/buildupdate_v2_0_v2_1.sql
@@ -12,54 +12,6 @@ ALTER TABLE studycomment
       REFERENCES studyversion (id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION;
 
-
---
--- and now DML
---
-begin;
-
-insert into studyversion (versionnumber, versionnote, versionstate, study_id, metadata_id, version)
-select 1, 'Initial version', rs.name, s.id, metadata_id, 1 from study s, reviewstate rs where s.reviewstate_id = rs.id;
-
-insert into filemetadata (label, description, category, studyfile_id, studyversion_id, version)
-select filename, sf.description, fc.name, sf.id,  sv.id, 1
-from studyfile sf, filecategory fc, study s, studyversion sv
-where sf.filecategory_id = fc.id
-and sf.study_id = s.id
-and s.id = sv.study_id;
-
-update studycomment set studyversion_id = sv.id
-from studyversion sv where studycomment.study_id = sv.study_id;
-
-update studyversion set versionstate='DRAFT' where versionstate='New';
-update studyversion set versionstate='IN_REVIEW' where versionstate='In Review';
-update studyversion set versionstate='RELEASED' where versionstate='Released';
-
-ALTER TABLE study DROP COLUMN reviewstate_id;
-ALTER TABLE vdc DROP COLUMN reviewstate_id;
-
-drop table reviewstate;
-
-commit;
-
-
---
--- lastly, remove some unneeded colums and add some more constraints
---
-ALTER TABLE studycomment ALTER COLUMN studyversion_id SET NOT NULL;
-ALTER TABLE studycomment DROP COLUMN study_id;
-ALTER TABLE studycomment ALTER COLUMN studyversion_id SET NOT NULL;
-
-ALTER TABLE studyfile DROP COLUMN filecategory_id;
-alter table study drop column metadata_id;
-
-
-
-
---
--- End version related changees
---
-
 --
 -- New columns needed for contributor settings
 --
@@ -69,18 +21,16 @@ ALTER TABLE vdc ALTER COLUMN allowcontributorseditall SET STORAGE PLAIN;
 ALTER TABLE vdc ADD COLUMN allowregistereduserstocontribute boolean;
 ALTER TABLE vdc ALTER COLUMN allowregistereduserstocontribute SET STORAGE PLAIN;
 
-update vdc set allowcontributorseditall = false;
-update vdc set allowregistereduserstocontribute = false;
-
 -- change data type for filesystemlocation
 alter table studyfile alter column filesystemlocation type text;
 
 --
--- Page authorization change for contributor settings
+-- Modify variablecategory to use a double precision float for frequency:
 --
-update pagedef set role_id = null where path = '/admin/OptionsPage.xhtml';
-update pagedef set role_id = null where path = '/study/ManageStudiesPage.xhtml';
-update pagedef set networkrole_id = null where path = '/networkAdmin/NetworkOptionsPage.xhtml';
+ALTER TABLE variablecategory RENAME COLUMN frequency TO temp;
+ALTER TABLE variablecategory ADD COLUMN frequency float;
+UPDATE variablecategory SET frequency=temp;
+ALTER TABLE variablecategory DROP COLUMN temp;
 
 --
 -- Add null constraints to existing columns
@@ -128,9 +78,69 @@ ALTER TABLE vdcrole ALTER COLUMN vdc_id SET NOT NULL;
 ALTER TABLE vdcrole ALTER COLUMN role_id SET NOT NULL;
 
 --
--- Modify variablecategory to use a double precision float for frequency:
+-- and now DML
 --
-ALTER TABLE variablecategory RENAME COLUMN frequency TO temp;
-ALTER TABLE variablecategory ADD COLUMN frequency float;
-UPDATE variablecategory SET frequency=temp;
-ALTER TABLE variablecategory DROP COLUMN temp;
+begin;
+
+-- for release time, we use the lastupdatetime
+insert into studyversion (versionnumber, versionnote, versionstate,  createtime, lastupdatetime, study_id, metadata_id, version)
+select 1, 'Initial version', rs.name, s.createtime, s.lastupdatetime, s.id, metadata_id, 1 from study s, reviewstate rs where s.reviewstate_id = rs.id;
+
+-- first add last updaters as contributors
+insert into versioncontributor (studyversion_id, contributor_id, lastupdatetime, version)
+select sv.id, s.lastupdater_id, s.lastupdatetime, 1 from studyversion sv, study s where sv.study_id = s.id;
+
+-- if creator is a different user, then add creator as a contributor (suing createtime for lastupdatetime as best guess)
+insert into versioncontributor (studyversion_id, contributor_id, lastupdatetime, version)
+select sv.id, s.creator_id, s.createtime, 1 from studyversion sv, study s where sv.study_id = s.id and s.creator_id != s.lastupdater_id;
+
+insert into filemetadata (label, description, category, studyfile_id, studyversion_id, version)
+select filename, sf.description, fc.name, sf.id,  sv.id, 1
+from studyfile sf, filecategory fc, study s, studyversion sv
+where sf.filecategory_id = fc.id
+and sf.study_id = s.id
+and s.id = sv.study_id;
+
+update studycomment set studyversion_id = sv.id
+from studyversion sv where studycomment.study_id = sv.study_id;
+
+update studyversion set versionstate='DRAFT' where versionstate='New';
+update studyversion set versionstate='IN_REVIEW' where versionstate='In Review';
+update studyversion set versionstate='RELEASED' where versionstate='Released';
+
+update studyversion set releasetime = lastupdatetime where versionstate='RELEASED';
+
+
+-- set contributor setting booleans
+update vdc set allowcontributorseditall = false;
+update vdc set allowregistereduserstocontribute = false;
+
+--
+-- Page authorization change for contributor settings
+--
+update pagedef set role_id = null where path = '/admin/OptionsPage.xhtml';
+update pagedef set role_id = null where path = '/study/ManageStudiesPage.xhtml';
+update pagedef set networkrole_id = null where path = '/networkAdmin/NetworkOptionsPage.xhtml';
+
+commit;
+
+
+--
+-- lastly, remove some unneeded colums and add some more constraints
+--
+
+ALTER TABLE study DROP COLUMN reviewstate_id;
+ALTER TABLE vdc DROP COLUMN reviewstate_id;
+
+drop table reviewstate;
+
+ALTER TABLE studycomment ALTER COLUMN studyversion_id SET NOT NULL;
+ALTER TABLE studycomment DROP COLUMN study_id;
+ALTER TABLE studycomment ALTER COLUMN studyversion_id SET NOT NULL;
+
+ALTER TABLE studyfile DROP COLUMN filecategory_id;
+alter table study drop column metadata_id;
+
+
+
+

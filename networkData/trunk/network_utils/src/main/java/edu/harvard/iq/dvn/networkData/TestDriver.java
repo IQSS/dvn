d1fc7145961c5130e99823ba897f72e19164af6e
Alexander D'Amour <dvn_support@help.hmdc.harvard.edu>
2010-08-02 15:43:29 +0000

    Consolidated file specifications, reorganized tests.

--- networkData/trunk/network_utils/src/main/java/edu/harvard/iq/dvn/networkData/TestDriver.java
+++ networkData/trunk/network_utils/src/main/java/edu/harvard/iq/dvn/networkData/TestDriver.java
@@ -4,55 +4,97 @@ import java.sql.SQLException;
 //import java.util.ArrayList;
 
 public class TestDriver {
+    public static final String NEO_DB = "RNAi_db1";
+    public static final String SQL_DB = "RNAi_props.db1";
+    public static final String INSERT_PROPS = "graphml.props";
+    public static final String NEO_PROPS = "neoDB.props";
+
+    public static final String SOURCE_XML = "RNAi_sample.xml";
+
     public static void main(String[] args) throws ClassNotFoundException, SQLException{
         //GraphBatchInserter gbi = new GraphBatchInserter("full_db", "full_props.db", "graphml.props");
-        GraphBatchInserter gbi = new GraphBatchInserter("RNAi_db1", "RNAi_props.db1", "graphml.props", "neodb.props");
+        GraphBatchInserter gbi = new GraphBatchInserter(NEO_DB, SQL_DB, INSERT_PROPS, NEO_PROPS);
         //gbi.ingest("big_one.xml");
-        gbi.ingest("RNAi_sample.xml");
+        gbi.ingest(SOURCE_XML);
         
         //DVNGraph lg = new DVNGraphImpl("full_db", "full_props.db");
-        DVNGraph lg = new DVNGraphImpl("RNAi_db", "RNAi_props.db", "neoDB.props");
+        DVNGraph lg = new DVNGraphImpl(NEO_DB, SQL_DB, NEO_PROPS);
+        
+        
         try{
-            lg.initialize();
+            //lg.initialize();
+            
+//            ((DVNGraphImpl)lg).tagComponents();
+//            ((DVNGraphImpl)lg).printComponents();
+            
+            System.out.println("\nComponent marking test.");
+            lg.markComponent(1);
+            lg.printUncommittedStatus();
+            lg.commit();
             lg.printStatus();
             
+            
+            System.out.println("\nVertex subset test.");
             lg.markNodesByProperty("\"loc\"=\"MA\"");
+            lg.printUncommittedStatus();
+            lg.commit();
             lg.printStatus();
             lg.initialize();
+            
 
-            lg.markRelationshipsByProperty("h_city = \"CAMBRIDGE\" or t_city = \"CAMBRIDGE\"",true);
+            System.out.println("\nRelationship subset test with dropped nodes.");
+            lg.markRelationshipsByProperty("h_city = \"CAMBRIDGE\" or t_city = \"CAMBRIDGE\"", true);
+            lg.printUncommittedStatus();
+            lg.commit();
+            lg.printStatus();
 
+            System.out.println("\nNeighborhood test.");
+            lg.markNeighborhood(3);
+            lg.printUncommittedStatus();
+            lg.commit();
+            lg.printStatus();
+
+            System.out.println("\nRelationship subset test without dropped nodes.");
+            lg.initialize();
+            lg.markRelationshipsByProperty("h_city = \"CAMBRIDGE\" or t_city = \"CAMBRIDGE\"", false);
+            lg.printUncommittedStatus();
+            lg.commit();
+            lg.printStatus();
+
+            System.out.println("\nNetwork measures test.");
             lg.calcPageRank(0.85);
             lg.calcDegree();
             lg.calcUniqueDegree();
             lg.calcInLargestComponent();
-
             lg.printUserProps();
-            
+
             lg.markComponent(1);
+            lg.commit();
+            lg.printUserProps();
 
             lg.calcPageRank(0.85);
-
             lg.printUserProps();
+            
 
+            System.out.println("\nGraphML dump test.");
             lg.dumpGraphML("RNAi_subset.xml");
+
+            System.out.println("\nTabular dump test.");
             lg.dumpTables("RNAi_verts.tab", "RNAi_edge.tab", ",");
 
+            System.out.println("\nProperty dump test:");
             System.out.println("Vertex Properties:");
             for(String p : lg.listNodeProperties())
                 System.out.println("\t" + p);
-            System.out.print("\n");
 
             System.out.println("Edge Properties:");
             for(String p : lg.listRelationshipProperties())
                 System.out.println("\t"+p);
             System.out.print("\n");
-
-
+            
         }
         finally{
             lg.finalize();
         }
-        
     }
 }

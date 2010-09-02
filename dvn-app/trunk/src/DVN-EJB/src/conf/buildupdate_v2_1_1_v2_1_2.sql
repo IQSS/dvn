54592fce76e62a13972771133228441f956640eb
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2010-09-02 14:55:12 +0000

    set new booleans in vdcnetwork to false

--- dvn-app/trunk/src/DVN-EJB/src/conf/buildupdate_v2_1_1_v2_1_2.sql
+++ dvn-app/trunk/src/DVN-EJB/src/conf/buildupdate_v2_1_1_v2_1_2.sql
@@ -3,4 +3,10 @@ Alter Table vdcnetwork
 add requireDVDescription boolean,
 add requireDVaffiliation boolean,
 add requireDVclassification boolean,
-add requireDVstudiesforrelease boolean
+add requireDVstudiesforrelease boolean;
+
+update vdcnetwork set  requireDVDescription = false,
+ requireDVaffiliation = false,
+ requireDVclassification = false,
+ requireDVstudiesforrelease = false;
+

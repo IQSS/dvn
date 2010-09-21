381380a7b9b01d7e28f354a4f4cacc9743c670ac
Stephen Kraffmiller <skraffmiller@hmdc.harvard.edu>
2010-09-21 14:53:21 +0000

    Undo a change made for diagnostics test.

--- dvn-app/trunk/src/DVN-EJB/src/java/edu/harvard/iq/dvn/core/vdc/VDCNetworkServiceBean.java
+++ dvn-app/trunk/src/DVN-EJB/src/java/edu/harvard/iq/dvn/core/vdc/VDCNetworkServiceBean.java
@@ -50,7 +50,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-import javax.ejb.TimerService;
 
 /**
  *

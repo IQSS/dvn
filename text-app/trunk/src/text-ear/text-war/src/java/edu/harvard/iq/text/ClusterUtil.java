4baa77fce7bc7a3b76646aca29393a83143d7201
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2010-10-25 12:15:48 +0000

    updates for milestone1

---
+++ text-app/trunk/src/text-ear/text-war/src/java/edu/harvard/iq/text/ClusterUtil.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.harvard.iq.text;
+
+import java.io.File;
+
+/**
+ *
+ * @author ekraffmiller
+ */
+public class ClusterUtil {
+        public static String getDocRoot() {
+            String docRoot = System.getProperty("text.documentRoot");
+            if (docRoot==null) {
+                throw new ClusterException("Error: JVM property 'text.documentRoot' is undefined.");
+            }
+            return docRoot;
+        }
+
+        public static File getSetDir(String setId) {
+            return new File(getDocRoot(),setId );
+        }
+
+}

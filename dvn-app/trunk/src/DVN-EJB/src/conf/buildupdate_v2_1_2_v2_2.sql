8c93cc68a9c1e2d397035bf3e66d1ee7ff1daec3
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2011-01-28 22:58:47 +0000

    Add permissions logic for SetUpDataExplorationPage

--- dvn-app/trunk/src/DVN-EJB/src/conf/buildupdate_v2_1_2_v2_2.sql
+++ dvn-app/trunk/src/DVN-EJB/src/conf/buildupdate_v2_1_2_v2_2.sql
@@ -5,3 +5,4 @@ update dataTable set  visualizationEnabled = false;
 
 
 INSERT INTO pagedef ( name, path, role_id, networkrole_id ) VALUES ( 'EditStudyFilesPage','/study/EditStudyFilesPage.xhtml',1,null );
+INSERT INTO pagedef ( name, path, role_id, networkrole_id ) VALUES ( 'SetUpDataExplorationPage','/study/SetUpDataExplorationPage.xhtml',1,null );

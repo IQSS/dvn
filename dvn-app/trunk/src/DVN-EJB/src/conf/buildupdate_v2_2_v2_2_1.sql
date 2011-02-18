c45f5a7ae2df459d07829c43ed4ada7e3bbb6bf6
Stephen Kraffmiller <skraffmiller@hmdc.harvard.edu>
2011-02-18 19:12:23 +0000

    Add Source URL to Set Up Exploration

--- dvn-app/trunk/src/DVN-EJB/src/conf/buildupdate_v2_2_v2_2_1.sql
+++ dvn-app/trunk/src/DVN-EJB/src/conf/buildupdate_v2_2_v2_2_1.sql
@@ -1,3 +1,14 @@
 
 -- this table is no longer needed - version is stored in a property file
 drop table dvnversion;
+
+-- Add fields to datatable to support visualization source url
+
+Alter Table dataTable
+add visualizationUrlIncludeVariableNames boolean;
+
+update dataTable
+set visualizationUrlIncludeVariableNames = false;
+
+Alter Table dataTable
+add visualizationUrl character varying(255);

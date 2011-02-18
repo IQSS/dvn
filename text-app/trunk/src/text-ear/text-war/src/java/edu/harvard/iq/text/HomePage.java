88986a07d3503ca7177456c1b97f895ef07ed8ef
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2011-02-18 20:17:51 +0000

    backing bean for dynamically generating set links

--- text-app/trunk/src/text-ear/text-war/src/java/edu/harvard/iq/text/HomePage.java
+++ text-app/trunk/src/text-ear/text-war/src/java/edu/harvard/iq/text/HomePage.java
@@ -5,6 +5,10 @@
 
 package edu.harvard.iq.text;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import javax.annotation.PostConstruct;
 
@@ -23,13 +27,57 @@ public class HomePage {
 
     @PostConstruct
     public void init() {
-        
+        File setDir = new File(ClusterUtil.getDocRoot());
+        File[] files = setDir.listFiles();
+        for (File f : files) {
+            if (f.isDirectory()) {
+                // get set id
+                SetInfo setInfo = new SetInfo();
+                setInfo.id = f.getName();
+                // get set description
+                File desc = new File(f, "description.txt");
+                if (desc.exists()) {
+                    byte[] buffer = new byte[(int) desc.length()];
+                    BufferedInputStream bi = null;
+                    try {
+                        bi = new BufferedInputStream(new FileInputStream(desc));
+                        bi.read(buffer);
+                    } catch  (IOException ignored) {
 
+                    }finally {
+                        if (bi != null) {
+                            try {
+                                bi.close();
+                            } catch (IOException ignored) {
+                            }
+                        }
+                    }
+                    setInfo.description = new String(buffer);
+                }
+                setList.add(setInfo);
+            }
+        }
     }
 
-    class SetInfo {
+    public ArrayList<SetInfo> getSetList() {
+        return setList;
+    }
+
+    public void setSetList(ArrayList<SetInfo> setList) {
+        this.setList = setList;
+    }
+
+    public class SetInfo {
         String id;
         String description;
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
 
         public String getDescription() {
             return description;

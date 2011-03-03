7f486ee719dc3a9a3cd349552e0e854be917de92
Gustavo Durand <gdurand@hmdc.harvard.edu>
2011-03-03 18:00:27 +0000

    removed unneeded data viz changes

--- dvn-app/trunk/src/DVN-EJB/src/conf/buildupdate_v2_2_v2_2_1.sql
+++ dvn-app/trunk/src/DVN-EJB/src/conf/buildupdate_v2_2_v2_2_1.sql
@@ -1,14 +1,3 @@
 
 -- this table is no longer needed - version is stored in a property file
 drop table dvnversion;
-
--- Add fields to datatable to support visualization source url
-
-Alter Table dataTable
-add visualizationUrlIncludeVariableNames boolean;
-
-update dataTable
-set visualizationUrlIncludeVariableNames = false;
-
-Alter Table dataTable
-add visualizationUrl character varying(255);

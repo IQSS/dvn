23248939c447de1e7234120b47de7f1767d05c24
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2011-03-09 18:16:01 +0000

    copying java classes to new project

---
+++ text-app/trunk/src/text6/text6-war/src/java/edu/harvard/iq/text/ClusterUtil.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.harvard.iq.text;
+import java.io.File;
+
+/**
+ *
+ * @author ekraffmiller
+ */
+public class ClusterUtil {
+        public static String getDocRoot() {
+            String docRoot = System.getProperty("text.documentRoot");
+            if (docRoot==null) {
+                throw new ClusterException("Error: JVM property 'text.documentRoot' is undefined.");
+            }
+            return docRoot;
+        }
+        public static String getMeadDir() {
+            String docRoot = System.getProperty("text.meadDir");
+            if (docRoot==null) {
+                throw new ClusterException("Error: JVM property 'text.meadDir' is undefined.");
+            }
+            return docRoot;
+        }
+
+
+        public static File getSetDir(String setId) {
+            return new File(getDocRoot(),setId );
+        }
+
+}

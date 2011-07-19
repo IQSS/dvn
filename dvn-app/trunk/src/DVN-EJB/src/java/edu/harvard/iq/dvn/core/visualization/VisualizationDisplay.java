83bc78e2503ffe7205d88d7b637fdc0dbc3583ea
Stephen Kraffmiller <skraffmiller@hmdc.harvard.edu>
2011-07-19 19:39:40 +0000

    Add Script to insert rows into display table for visualizations that have been released.

--- dvn-app/trunk/src/DVN-EJB/src/java/edu/harvard/iq/dvn/core/visualization/VisualizationDisplay.java
+++ dvn-app/trunk/src/DVN-EJB/src/java/edu/harvard/iq/dvn/core/visualization/VisualizationDisplay.java
@@ -21,7 +21,7 @@ import javax.persistence.OneToOne;
 public class VisualizationDisplay implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
     private boolean showImageGraph;

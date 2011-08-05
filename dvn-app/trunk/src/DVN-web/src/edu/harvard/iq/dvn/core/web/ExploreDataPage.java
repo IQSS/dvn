36fa6710f5de58b48e63fc12ffce1b42f581439b
Stephen Kraffmiller <skraffmiller@hmdc.harvard.edu>
2011-08-05 23:40:19 +0000

    Add 1 character blank string to pad image magick call

--- dvn-app/trunk/src/DVN-web/src/edu/harvard/iq/dvn/core/web/ExploreDataPage.java
+++ dvn-app/trunk/src/DVN-web/src/edu/harvard/iq/dvn/core/web/ExploreDataPage.java
@@ -2357,6 +2357,10 @@ public class ExploreDataPage extends VDCBaseBean  implements Serializable {
         BufferedImage yAxisImage = new BufferedImage(100, 500, BufferedImage.TYPE_INT_ARGB);
         BufferedImage combinedImage = new BufferedImage(776, 575 + heightAdjustment , BufferedImage.TYPE_INT_ARGB);
 
+                
+        if(graphTitle.trim().isEmpty()){
+            graphTitle = " ";
+        }
        
         File retFile = generateImageString("16", "676x", "South", "0", graphTitle);       
         BufferedImage titleImage =     ImageIO.read(retFile);
@@ -2367,8 +2371,15 @@ public class ExploreDataPage extends VDCBaseBean  implements Serializable {
              source = "Source: " + sources;
         }
         
+        if(source.trim().isEmpty()){
+            source = " ";
+        }
         retFile = generateImageString("14", "676x", "NorthWest", "0", source);        
         BufferedImage sourceImage =     ImageIO.read(retFile);
+        
+        if(yAxisLabel.trim().isEmpty()){
+            yAxisLabel = " ";
+        }        
         
         retFile = generateImageString("14", "200x", "South", "-90", yAxisLabel);        
         BufferedImage yAxisVertImage =     ImageIO.read(retFile);

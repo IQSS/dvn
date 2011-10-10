c2d1fdbf3b985abe46ac6128c4885264ff82acb9
Leonid Andreev <leonid@hmdc.harvard.edu>
2011-10-10 21:22:02 +0000

    Moved the EJB__TIMER table out of referenceData; created a separate sql script file for it. The timer now has to be created *before* the application is deployed for the first time.

--- dvn-app/trunk/src/DVN-EJB/src/conf/referenceData.sql
+++ dvn-app/trunk/src/DVN-EJB/src/conf/referenceData.sql
@@ -921,23 +921,6 @@ CREATE SEQUENCE filesystemname_seq
 ALTER TABLE filesystemname_seq OWNER TO "dvnApp";
 
 
-CREATE TABLE ejb__timer__tbl
-(
-  creationtimeraw numeric(19) NOT NULL,
-  blob bytea,
-  timerid varchar(256) NOT NULL,
-  containerid numeric(19) NOT NULL,
-  ownerid varchar(256),
-  state int4 NOT NULL,
-  pkhashcode int4 NOT NULL,
-  intervalduration numeric(19) NOT NULL,
-  initialexpirationraw numeric(19) NOT NULL,
-  lastexpirationraw numeric(19) NOT NULL,
-  CONSTRAINT pk_ejb__timer__tbl PRIMARY KEY (timerid)
-) 
-WITHOUT OIDS;
-ALTER TABLE ejb__timer__tbl OWNER TO "dvnApp";
-
 INSERT INTO DataFileFormatType VALUES (1, 'D02', 'Splus');
 INSERT INTO DataFileFormatType VALUES (2, 'D03', 'Stata');
 INSERT INTO DataFileFormatType VALUES (3, 'D04', 'R');

f7bc8e16d758f9540bb8d807df5f6f1da327804d
Gustavo Durand <gdurand@hmdc.harvard.edu>
2011-12-01 22:13:36 +0000

    upgrade changes for StudyPage and EditStudyPage

--- dvn-app/trunk/src/DVN-web/src/edu/harvard/iq/dvn/core/web/study/VersionNotesPopupBean.java
+++ dvn-app/trunk/src/DVN-web/src/edu/harvard/iq/dvn/core/web/study/VersionNotesPopupBean.java
@@ -5,17 +5,22 @@
 package edu.harvard.iq.dvn.core.web.study;
 
 import edu.harvard.iq.dvn.core.study.StudyVersion;
+import java.io.Serializable;
 import javax.faces.application.FacesMessage;
+import javax.faces.bean.ViewScoped;
 import javax.faces.component.UIComponent;
 import javax.faces.component.UIInput;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
+import javax.inject.Named;
 
 /**
  *
  * @author ekraffmiller
  */
-public class VersionNotesPopupBean {
+@Named("VersionNotesPopupBean")
+@ViewScoped
+public class VersionNotesPopupBean implements Serializable {
 
     /**
      * @return the notes

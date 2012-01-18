abaf021b17fe72568b60a0f6a3d0e806d2cf49fb
Gustavo Durand <gdurand@hmdc.harvard.edu>
2012-01-18 22:40:56 +0000

    fix for display of terms of use

--- dvn-app/trunk/src/DVN-web/src/edu/harvard/iq/dvn/core/web/networkAdmin/EditNetworkDownloadUseTermsPage.java
+++ dvn-app/trunk/src/DVN-web/src/edu/harvard/iq/dvn/core/web/networkAdmin/EditNetworkDownloadUseTermsPage.java
@@ -47,10 +47,15 @@ import javax.inject.Named;
 @Named("EditNetworkDownloadUseTermsPage")
 public class EditNetworkDownloadUseTermsPage extends VDCBaseBean implements java.io.Serializable  {
 
-    @EJB
-    VDCNetworkServiceLocal vdcNetworkService;
+    @EJB  VDCNetworkServiceLocal vdcNetworkService;
     private boolean termsOfUseEnabled;
     private String termsOfUse;
+    
+    public void init() {
+        super.init();
+        termsOfUse = getVDCRequestBean().getVdcNetwork().getDownloadTermsOfUse();
+        termsOfUseEnabled = getVDCRequestBean().getVdcNetwork().isDownloadTermsOfUseEnabled();
+    }    
 
     public boolean isTermsOfUseEnabled() {
         return termsOfUseEnabled;
@@ -69,7 +74,6 @@ public class EditNetworkDownloadUseTermsPage extends VDCBaseBean implements java
     }
 
     public String save_action() {      
-        success = true;
         if (validateTerms()) {
             // action code here
             VDCNetwork vdcNetwork = vdcNetworkService.find();
@@ -79,7 +83,6 @@ public class EditNetworkDownloadUseTermsPage extends VDCBaseBean implements java
             getExternalContext().getFlash().put("message", "Successfully updated terms for file download.");
             return "myNetworkOptions";
         } else {
-            success = false;
             return null;
         }
     }
@@ -87,36 +90,11 @@ public class EditNetworkDownloadUseTermsPage extends VDCBaseBean implements java
     public String cancel_action() {
         return "cancelNetwork";
     }
-    //UTILITY METHODS
-    /**
-     * Holds value of property success.
-     */
-    private boolean success;
+    
+ 
 
-    /**
-     * Getter for property success.
-     * @return Value of property success.
-     */
-    public boolean isSuccess() {
-        return this.success;
-    }
 
-    /**
-     * Setter for property success.
-     * @param success New value of property success.
-     */
-    public void setSuccess(boolean success) {
-        this.success = success;
-    }
-    /* validateTerms
-     *
-     **<p> Utility method to validate that the user entered terms of use.</p>
-     * 
-     *
-     * @author Wendy Bossons
-     */
-
-    public boolean validateTerms() {
+    private boolean validateTerms() {
         String elementValue = termsOfUse;
         boolean isUseTerms = true;
         if ((elementValue == null || elementValue.equals("")) && (termsOfUseEnabled)) {
@@ -127,46 +105,5 @@ public class EditNetworkDownloadUseTermsPage extends VDCBaseBean implements java
         return isUseTerms;
     }
 
-  
-
-    /**
-     * <p>Callback method that is called after the component tree has been
-     * restored, but before any event processing takes place.  This method
-     * will <strong>only</strong> be called on a postback request that
-     * is processing a form submit.  Customize this method to allocate
-     * resources that will be required in your event handlers.</p>
-     */
-    public void preprocess() {
-    }
-
-    /**
-     * <p>Callback method that is called just before rendering takes place.
-     * This method will <strong>only</strong> be called for the page that
-     * will actually be rendered (and not, for example, on a page that
-     * handled a postback and then navigated to a different page).  Customize
-     * this method to allocate resources that will be required for rendering
-     * this page.</p>
-     */
-    public void prerender() {
-    }
-
-    /**
-     * <p>Callback method that is called after rendering is completed for
-     * this request, if <code>init()</code> was called (regardless of whether
-     * or not this was the page that was actually rendered).  Customize this
-     * method to release resources acquired in the <code>init()</code>,
-     * <code>preprocess()</code>, or <code>prerender()</code> methods (or
-     * acquired during execution of an event handler).</p>
-     */
-    public void destroy() {
-    }
-
-    /**
-     * <p>Automatically managed component initialization.  <strong>WARNING:</strong>
-     * This method is automatically generated, so any user-specified code inserted
-     * here is subject to being replaced.</p>
-     */
-    private void _init() {
-    }
 }
 

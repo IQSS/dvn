216bcad9ec83a41d415710c1566b316414f3c50a
Gustavo Durand <gdurand@hmdc.harvard.edu>
2012-01-20 23:07:31 +0000

    added logic for deifferent returnPages (will review in future)

--- dvn-app/trunk/src/DVN-web/src/edu/harvard/iq/dvn/core/web/login/EditAccountPage.java
+++ dvn-app/trunk/src/DVN-web/src/edu/harvard/iq/dvn/core/web/login/EditAccountPage.java
@@ -77,60 +77,19 @@ public class EditAccountPage extends VDCBaseBean implements java.io.Serializable
     
     
     
-    /**
-     * <p>Callback method that is called whenever a page is navigated to,
-     * either directly via a URL, or indirectly via page navigation.
-     * Customize this method to acquire resources that will be needed
-     * for event handlers and lifecycle methods, whether or not this
-     * page is performing post back processing.</p>
-     *
-     * <p>Note that, if the current request is a postback, the property
-     * values of the components do <strong>not</strong> represent any
-     * values submitted with this request.  Instead, they represent the
-     * property values that were saved for this view when it was rendered.</p>
-     */
     public void init() {
-
-        returnPage = "/login/AccountPage.xhtml?faces-redirect=true&userId="+userId;
+        if ("viewAccount".equals(returnPage)) {
+            returnPage = "/login/AccountPage.xhtml?faces-redirect=true&userId="+userId;
+        } else {
+            returnPage = "/login/AccountOptionsPage?faces-redirect=true&userId="+userId;
+        }
+        
         editUserService.setUser(userId);        
         user = editUserService.getUser();       
         
     }
-    
-    /**
-     * <p>Callback method that is called after the component tree has been
-     * restored, but before any event processing takes place.  This method
-     * will <strong>only</strong> be called on a postback request that
-     * is processing a form submit.  Customize this method to allocate
-     * resources that will be required in your event handlers.</p>
-     */
-    public void preprocess() {
-    }
-    
-    /**
-     * <p>Callback method that is called just before rendering takes place.
-     * This method will <strong>only</strong> be called for the page that
-     * will actually be rendered (and not, for example, on a page that
-     * handled a postback and then navigated to a different page).  Customize
-     * this method to allocate resources that will be required for rendering
-     * this page.</p>
-     */
-    public void prerender() {
-    }
-    
-    /**
-     * <p>Callback method that is called after rendering is completed for
-     * this request, if <code>init()</code> was called (regardless of whether
-     * or not this was the page that was actually rendered).  Customize this
-     * method to release resources acquired in the <code>init()</code>,
-     * <code>preprocess()</code>, or <code>prerender()</code> methods (or
-     * acquired during execution of an event handler).</p>
-     */
-    public void destroy() {
-    }
-    
+       
      
- 
     
   
     public void validateUserName(FacesContext context, 
@@ -161,18 +120,12 @@ public class EditAccountPage extends VDCBaseBean implements java.io.Serializable
         if (getVDCSessionBean().getLoginBean().getUser().getId().equals(user.getId())) {
             this.getVDCSessionBean().getLoginBean().setUser(user);
         }
-        // Save userId as requestAttribute so it can be used by AccountPage
-        this.getRequestMap().put("userId",user.getId());
-        StatusMessage msg = new StatusMessage();
-        msg.setMessageText("User account updated successfully.");
-        msg.setStyleClass("successMessage");
-        getRequestMap().put("statusMessage",msg);    
+
+        getExternalContext().getFlash().put("message","User account updated successfully."); 
         return returnPage;
     }
     
     public String cancelAction() {
-        // Save userId as requestAttribute so it can be used by AccountPage
-        this.getRequestMap().put("userId",user.getId());
         editUserService.cancel();
         return returnPage;
     }

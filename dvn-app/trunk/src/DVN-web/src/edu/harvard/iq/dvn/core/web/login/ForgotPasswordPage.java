b9a24d5663729d1fd70e46991714fbfbf83791b3
Gustavo Durand <gdurand@hmdc.harvard.edu>
2012-02-21 19:34:21 +0000

    now goes direct to Home Page (based on context)

--- dvn-app/trunk/src/DVN-web/src/edu/harvard/iq/dvn/core/web/login/ForgotPasswordPage.java
+++ dvn-app/trunk/src/DVN-web/src/edu/harvard/iq/dvn/core/web/login/ForgotPasswordPage.java
@@ -49,21 +49,7 @@ import javax.inject.Named;
 @ViewScoped
 @Named("ForgotPasswordPage")
 public class ForgotPasswordPage extends VDCBaseBean implements java.io.Serializable  {
- 
-    private int __placeholder;
     
-    /**
-     * <p>Automatically managed component initialization.  <strong>WARNING:</strong>
-     * This method is automatically generated, so any user-specified code inserted
-     * here is subject to being replaced.</p>
-     */
-    private void _init() throws Exception {
-      }
-
-  
-    
- 
-    @EJB EditUserService editUserService;
     @EJB UserServiceLocal userService;
     String userName;
     
@@ -80,55 +66,13 @@ public class ForgotPasswordPage extends VDCBaseBean implements java.io.Serializa
     public void setUserName(String userName) {
         this.userName = userName;
     }
-       
-    
-  
-    
-    /**
-     * <p>Callback method that is called after the component tree has been
-     * restored, but before any event processing takes place.  This method
-     * will <strong>only</strong> be called on a postback request that
-     * is processing a form submit.  Customize this method to allocate
-     * resources that will be required in your event handlers.</p>
-     */
-    public void preprocess() {
-    }
-    
-    /**
-     * <p>Callback method that is called just before rendering takes place.
-     * This method will <strong>only</strong> be called for the page that
-     * will actually be rendered (and not, for example, on a page that
-     * handled a postback and then navigated to a different page).  Customize
-     * this method to allocate resources that will be required for rendering
-     * this page.</p>
-     */
-    public void prerender() {
-    }
-    
-    /**
-     * <p>Callback method that is called after rendering is completed for
-     * this request, if <code>init()</code> was called (regardless of whether
-     * or not this was the page that was actually rendered).  Customize this
-     * method to release resources acquired in the <code>init()</code>,
-     * <code>preprocess()</code>, or <code>prerender()</code> methods (or
-     * acquired during execution of an event handler).</p>
-     */
-    public void destroy() {
-    }
-    
 
-    
-    public EditUserService getEditUserService() {
-        return editUserService;
-    }
     
     public void validateUserName(FacesContext context, 
                           UIComponent toValidate,
                           Object value) {
  
     String name = (String) value;
-    
- 
     VDCUser user = userService.findByUserName(name);
    
     if (user==null) {
@@ -141,17 +85,16 @@ public class ForgotPasswordPage extends VDCBaseBean implements java.io.Serializa
 }
     
     public String submit() {
-         VDCUser user = userService.findByUserName(this.userName);
-         userService.updatePassword(user.getId());
-      
-        StatusMessage msg = new StatusMessage();
-        msg.setMessageText("Your new password has been sent to the email address associated with this account.");
-        msg.setStyleClass("successMessage");
-        getRequestMap().put("statusMessage",msg);
-       
-        
-        //return "passwordSent";
-        return "/login/PasswordSentPage?faces-redirect=true";
+        VDCUser user = userService.findByUserName(this.userName);
+        userService.updatePassword(user.getId());
+
+        getExternalContext().getFlash().put("successMessage","Your new password has been sent to the email address associated with this account.");
+
+        if (getVDCRequestBean().getCurrentVDC() != null) {
+            return "/StudyListingPage?faces-redirect=true" + getNavigationVDCSuffix();   
+        } else {
+            return "/HomePage?faces-redirect=true";
+        }
     }
     
   

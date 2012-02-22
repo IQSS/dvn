dd07338e29e108be28288d260684acbcda27385b
Gustavo Durand <gdurand@hmdc.harvard.edu>
2012-02-22 22:48:47 +0000

    uses new VDCRender,flash

--- dvn-app/trunk/src/DVN-web/src/edu/harvard/iq/dvn/core/web/login/LogoutPage.java
+++ dvn-app/trunk/src/DVN-web/src/edu/harvard/iq/dvn/core/web/login/LogoutPage.java
@@ -51,69 +51,16 @@ public class LogoutPage extends VDCBaseBean implements java.io.Serializable  {
     }
 
 
-    /** 
-     * <p>Callback method that is called after the component tree has been
-     * restored, but before any event processing takes place.  This method
-     * will <strong>only</strong> be called on a postback request that
-     * is processing a form submit.  Customize this method to allocate
-     * resources that will be required in your event handlers.</p>
-     */
-    public void preprocess() {
-    }
-
-    /** 
-     * <p>Callback method that is called just before rendering takes place.
-     * This method will <strong>only</strong> be called for the page that
-     * will actually be rendered (and not, for example, on a page that
-     * handled a postback and then navigated to a different page).  Customize
-     * this method to allocate resources that will be required for rendering
-     * this page.</p>
-     */
-    public void prerender() {
-    }
-
-    /** 
-     * <p>Callback method that is called after rendering is completed for
-     * this request, if <code>init()</code> was called (regardless of whether
-     * or not this was the page that was actually rendered).  Customize this
-     * method to release resources acquired in the <code>init()</code>,
-     * <code>preprocess()</code>, or <code>prerender()</code> methods (or
-     * acquired during execution of an event handler).</p>
-     */
-    public void destroy() {
-    }
-    
-   
  
 
     public void init() {
         super.init();
         // Do logout here
         doLogout(getVDCSessionBean(), getSessionMap());
-        success=true;
-        
+        getVDCRenderBean().getFlash().put("successMessage","You are now logged out.");        
     }
 
-    /**
-     * Holds value of property success.
-     */
-    private boolean success;
 
-    /**
-     * Getter for property success.
-     * @return Value of property success.
-     */
-    public boolean isSuccess() {
-        return this.success;
-    }
-
-    /**
-     * Setter for property success.
-     * @param success New value of property success.
-     */
-    public void setSuccess(boolean success) {
-        this.success = success;
-    }
     
     public static void doLogout(VDCSessionBean vdcSessionBean, Map sessionMap) {
         vdcSessionBean.setLoginBean(null);

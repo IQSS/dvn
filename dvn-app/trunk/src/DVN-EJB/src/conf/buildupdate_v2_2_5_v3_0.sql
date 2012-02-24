919c7dc3b367d24c69a70e27d3bcc80e18064363
Gustavo Durand <gdurand@hmdc.harvard.edu>
2012-02-24 22:09:15 +0000

    dcm sort order no longer needed for default DDI fields

--- dvn-app/trunk/src/DVN-EJB/src/conf/buildupdate_v2_2_5_v3_0.sql
+++ dvn-app/trunk/src/DVN-EJB/src/conf/buildupdate_v2_2_5_v3_0.sql
@@ -126,7 +126,6 @@ UPDATE StudyField SET FieldInputLevelString='optional' WHERE id=98;
 update templatefield set version = 1 where version is null;
 
 alter table templateField ADD dcmSortOrder  int;
-update templatefield set dcmsortorder = studyfield_id - 44 where studyfield_id > 44 and studyfield_id < 65;
 
 alter table template ADD COLUMN enabled boolean;
 update template set enabled=true;

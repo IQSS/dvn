df4ddaeb3d29082b145fff894d17b863098c85ca
Gustavo Durand <gdurand@hmdc.harvard.edu>
2012-02-27 22:59:41 +0000

    new entities for controlled vocabulary

---
+++ dvn-app/trunk/src/DVN-EJB/src/java/edu/harvard/iq/dvn/core/study/ControlledVocabulary.java
@@ -0,0 +1,103 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.harvard.iq.dvn.core.study;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author gdurand
+ */
+@Entity
+public class ControlledVocabulary implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(columnDefinition="TEXT") 
+    private String description;
+    @OneToMany(mappedBy="controlledVocabulary",cascade={CascadeType.REMOVE, CascadeType.MERGE, CascadeType.PERSIST})
+    private List<ControlledVocabularyValue> controlledVocabularyValues;
+    @OneToMany(mappedBy="controlledVocabulary")
+    private List<TemplateField> templateFields;    
+    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<ControlledVocabularyValue> getControlledVocabularyValues() {
+        return controlledVocabularyValues;
+    }
+
+    public void setControlledVocabularyValues(List<ControlledVocabularyValue> controlledVocabularyValues) {
+        this.controlledVocabularyValues = controlledVocabularyValues;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<TemplateField> getTemplateFields() {
+        return templateFields;
+    }
+
+    public void setTemplateFields(List<TemplateField> templateFields) {
+        this.templateFields = templateFields;
+    }
+    
+    
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof ControlledVocabulary)) {
+            return false;
+        }
+        ControlledVocabulary other = (ControlledVocabulary) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "edu.harvard.iq.dvn.core.study.ControlledVocabulary[ id=" + id + " ]";
+    }
+    
+}

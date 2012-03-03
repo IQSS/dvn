a5807752c85b0e5538d55545b7c52c2d6143df66
Gustavo Durand <gdurand@hmdc.harvard.edu>
2012-03-03 22:18:32 +0000

    added default ordering for template fields

--- dvn-app/trunk/src/DVN-EJB/src/java/edu/harvard/iq/dvn/core/study/Template.java
+++ dvn-app/trunk/src/DVN-EJB/src/java/edu/harvard/iq/dvn/core/study/Template.java
@@ -42,6 +42,7 @@ import java.util.List;
 @Entity
 public class Template implements java.io.Serializable {
     @OneToMany (mappedBy="template",cascade={CascadeType.REMOVE, CascadeType.MERGE, CascadeType.PERSIST})
+    @OrderBy("dcmSortOrder")
     private List<TemplateField> templateFields;
     
     

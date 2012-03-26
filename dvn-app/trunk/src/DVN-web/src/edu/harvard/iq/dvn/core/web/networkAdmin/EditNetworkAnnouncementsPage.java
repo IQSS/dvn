7b6387d9b36a424f5e787cf331ae5e0e3ca16639
Gustavo Durand <gdurand@hmdc.harvard.edu>
2012-03-26 18:58:14 +0000

    convert forward to redirect

--- dvn-app/trunk/src/DVN-web/src/edu/harvard/iq/dvn/core/web/networkAdmin/EditNetworkAnnouncementsPage.java
+++ dvn-app/trunk/src/DVN-web/src/edu/harvard/iq/dvn/core/web/networkAdmin/EditNetworkAnnouncementsPage.java
@@ -78,39 +78,8 @@ public class EditNetworkAnnouncementsPage extends VDCBaseBean implements java.io
             VDCNetwork vdcnetwork = getVDCRequestBean().getVdcNetwork();
             this.setChkEnableNetworkAnnouncements(vdcnetwork.isDisplayAnnouncements()) ;
             this.setNetworkAnnouncements( vdcnetwork.getAnnouncements());        
-        success = false;
-    }
-    /** 
-     * <p>Callback method that is called after the component tree has been
-     * restored, but before any event processing takes place.  This method
-     * will <strong>only</strong> be called on a postback request that
-     * is processing a form submit.  Customize this method to allocate
-     * resources that will be required in your event handlers.</p>
-     */
-    public void preprocess() {
     }
 
-    /** 
-     * <p>Callback method that is called just before rendering takes place.
-     * This method will <strong>only</strong> be called for the page that
-     * will actually be rendered (and not, for example, on a page that
-     * handled a postback and then navigated to a different page).  Customize
-     * this method to allocate resources that will be required for rendering
-     * this page.</p>
-     */
-    public void prerender() {
-    }
-    
-    /** 
-     * <p>Callback method that is called after rendering is completed for
-     * this request, if <code>init()</code> was called (regardless of whether
-     * or not this was the page that was actually rendered).  Customize this
-     * method to release resources acquired in the <code>init()</code>,
-     * <code>preprocess()</code>, or <code>prerender()</code> methods (or
-     * acquired during execution of an event handler).</p>
-     */
-    public void destroy() {
-    }
     
     private String networkAnnouncements;
     
@@ -133,7 +102,6 @@ public class EditNetworkAnnouncementsPage extends VDCBaseBean implements java.io
     }
     
     public String save_action() {
-        success = true;
         if (validateAnnouncementsText()) {
             setChkEnableNetworkAnnouncements(chkEnableNetworkAnnouncements);
             setNetworkAnnouncements(networkAnnouncements);
@@ -143,59 +111,24 @@ public class EditNetworkAnnouncementsPage extends VDCBaseBean implements java.io
             vdcnetwork.setAnnouncements(this.getNetworkAnnouncements());
             vdcNetworkService.edit(vdcnetwork);
             getVDCRenderBean().getFlash().put("successMessage", "Successfully updated the network description.  Go to the Homepage to see your changes.");
-            return "myNetworkOptions";
-        } else {
-
-            success = false;
-            return "result";
+            return "/networkAdmin/NetworkOptionsPage.xhtml?faces-redirect=true";
         }
+        
+        return "";
 
     }
     
     public String cancel_action(){
-        if (getVDCRequestBean().getCurrentVDCId() == null)
-            return "cancelNetwork";
-        else
-            return "cancelVDC";
+        return "/networkAdmin/NetworkOptionsPage.xhtml?faces-redirect=true";
     }
     
-    //UTILITY METHODS
-    
-    /** validateAnnouncementsText
-     *
-     *
-     * @author wbossons
-     *
-     */
-    
-    private String SUCCESS_MESSAGE = new String("Update Successful! Go to the Homepage to see your changes.");
-    /**
-     * Holds value of property success.
-     */
-    private boolean success;
 
-    /**
-     * Getter for property success.
-     * @return Value of property success.
-     */
-    public boolean isSuccess() {
-        return this.success;
-    }
-
-    /**
-     * Setter for property success.
-     * @param success New value of property success.
-     */
-    public void setSuccess(boolean success) {
-        this.success = success;
-    }
     
     public boolean validateAnnouncementsText() {
         boolean isAnnouncements = true;
         String elementValue = networkAnnouncements;
         if ( (elementValue == null || elementValue.equals("")) && (isChkEnableNetworkAnnouncements()) ) {
             isAnnouncements = false;
-            success = false;
             FacesMessage message = new FacesMessage("To enable announcements, you must also enter announcements in the field below.  Please enter announcements as either plain text or html.");
             FacesContext context = FacesContext.getCurrentInstance();
             context.addMessage("editNetworkAnnouncementsForm:networkAnnouncements", message);

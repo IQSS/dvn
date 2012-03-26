272d637d5a68aa06454e6800593dcc374210d25f
Gustavo Durand <gdurand@hmdc.harvard.edu>
2012-03-26 19:16:02 +0000

    convert forward to redirect and general cleanup

--- dvn-app/trunk/src/DVN-web/src/edu/harvard/iq/dvn/core/web/ContactUsPage.java
+++ dvn-app/trunk/src/DVN-web/src/edu/harvard/iq/dvn/core/web/ContactUsPage.java
@@ -50,20 +50,13 @@ import net.tanesha.recaptcha.ReCaptchaImpl;
 import net.tanesha.recaptcha.ReCaptchaResponse;
 import net.tanesha.recaptcha.http.SimpleHttpLoader;
 
-/**
- * <p>Page bean that corresponds to a similarly named JSP page.  This
- * class contains component definitions (and initialization code) for
- * all components that you have defined on this page, as well as
- * lifecycle methods and event handlers where you may add behavior
- * to respond to incoming events.</p>
- */
+
 @ViewScoped
 @Named("ContactUsPage")
 public class ContactUsPage extends VDCBaseBean implements java.io.Serializable {
     @EJB MailServiceLocal mailService;
     @EJB CaptchaServiceLocal captchService;
     
-    private String ERROR_MESSAGE   = new String("An Error Occurred.");
     private String SUCCESS_MESSAGE = new String("An e-mail has been sent successfully!");
     private String EMAIL_ERROR_MESSAGE = new String("An error occurred. The message was not sent.");
     private Captcha c;
@@ -85,52 +78,12 @@ public class ContactUsPage extends VDCBaseBean implements java.io.Serializable {
     }
 
 
-
-    /** 
-     * <p>Callback method that is called after the component tree has been
-     * restored, but before any event processing takes place.  This method
-     * will <strong>only</strong> be called on a postback request that
-     * is processing a form submit.  Customize this method to allocate
-     * resources that will be required in your event handlers.</p>
-     */
-    public void preprocess() {
-    }
-
-    /** 
-     * <p>Callback method that is called just before rendering takes place.
-     * This method will <strong>only</strong> be called for the page that
-     * will actually be rendered (and not, for example, on a page that
-     * handled a postback and then navigated to a different page).  Customize
-     * this method to allocate resources that will be required for rendering
-     * this page.</p>
-     */
-    public void prerender() {
-    }
-
-    /** 
-     * <p>Callback method that is called after rendering is completed for
-     * this request, if <code>init()</code> was called (regardless of whether
-     * or not this was the page that was actually rendered).  Customize this
-     * method to release resources acquired in the <code>init()</code>,
-     * <code>preprocess()</code>, or <code>prerender()</code> methods (or
-     * acquired during execution of an event handler).</p>
-     */
-    public void destroy() {
-    }
-    
-     /**
-     * <p>Automatically managed component initialization.  <strong>WARNING:</strong>
-     * This method is automatically generated, so any user-specified code inserted
-     * here is subject to being replaced.</p>
-     */
     public void init() {
         super.init();
          this.setFullName((getVDCSessionBean().getLoginBean() != null) ? getVDCSessionBean().getLoginBean().getUser().getFirstName() + " " + getVDCSessionBean().getLoginBean().getUser().getLastName() : "");
         this.setEmailAddress((getVDCSessionBean().getLoginBean() != null) ? getVDCSessionBean().getLoginBean().getUser().getEmail() : "");
         this.setEmailBody((this.getEmailBody() != null) ? this.getEmailBody() : "");
         this.setSelectedSubject((this.getSelectedSubject() != null) ? this.getSelectedSubject() : "");
-        success = false;
-        exception = false;
     }
 
     private HtmlCommandButton btnSend = new HtmlCommandButton();
@@ -222,48 +175,9 @@ public class ContactUsPage extends VDCBaseBean implements java.io.Serializable {
     public void setEmailBody(String emailBody) {
         this.emailBody = emailBody;
     }
-    
-     /**
-     * Holds value of property success.
-     */
-    private boolean success;
 
-    /**
-     * Getter for property success.
-     * @return Value of property success.
-     */
-    public boolean isSuccess() {
-        return this.success;
-    }
 
-    /**
-     * Setter for property success.
-     * @param success New value of property success.
-     */
-    public void setSuccess(boolean success) {
-        this.success = success;
-    }
-    
-    /**
-     * Holds value of property success.
-     */
-    private boolean exception;
 
-    /**
-     * Getter for property success.
-     * @return Value of property success.
-     */
-    public boolean isException() {
-        return this.exception;
-    }
-
-    /**
-     * Setter for property success.
-     * @param success New value of property success.
-     */
-    public void setException(boolean exception) {
-        this.exception = exception;
-    }
     private String emailRecipient;
     
     public String getEmailRecipient(){
@@ -307,47 +221,30 @@ public class ContactUsPage extends VDCBaseBean implements java.io.Serializable {
 
     public String send_action() {
         String msg  = SUCCESS_MESSAGE;
-        Map map = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
-        HttpServletRequest req = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
-        success = true;
+        boolean success = true;
         try {
             String fromAddress = "\"" + fullName + "\"<" + emailAddress.trim() + ">";
             mailService.sendMail(fromAddress, getToEmailAddress(), (getVDCRequestBean().getCurrentVDCId()==null) ? getVDCRequestBean().getVdcNetwork().getName()  + " Dataverse Network: " + selectedSubject.trim() : getVDCRequestBean().getCurrentVDC().getName() + " dataverse: " + selectedSubject.trim(), emailBody.trim());
+  
+            getVDCRenderBean().getFlash().put("successMessage",SUCCESS_MESSAGE);
+            getVDCRenderBean().getFlash().put("fullName",fullName);
+            getVDCRenderBean().getFlash().put("emailAddress",emailAddress);
+            getVDCRenderBean().getFlash().put("selectedSubject",selectedSubject);
+            getVDCRenderBean().getFlash().put("emailBody",emailBody);
+            return "/ContactUsConfirmPage.xhtml?faces-redirect=true";             
+            
+            
         } catch (Exception e) {
-            success     = false;
-            exception   = true;
-            msg = EMAIL_ERROR_MESSAGE;
-            ExceptionMessageWriter.logException(e);
-        } finally {
-            ExceptionMessageWriter.addGlobalMessage(msg);
-            if (success) {
-                getVDCRenderBean().getFlash().put("success",success);
-                getVDCRenderBean().getFlash().put("fullName",fullName);
-                getVDCRenderBean().getFlash().put("emailAddress",emailAddress);
-                getVDCRenderBean().getFlash().put("selectedSubject",selectedSubject);
-                getVDCRenderBean().getFlash().put("emailBody",emailBody);
-                return "success"; 
-            } else {
-                return "result";
-            }
+            getVDCRenderBean().getFlash().put("warningMessage",EMAIL_ERROR_MESSAGE);
+            return "";
+
         }
         
     }
     
-    public String cancel_action(){
-        if (getVDCRequestBean().getCurrentVDCId() == null)
-            return "cancelNetwork";
-        else
-            return "cancelVDC";
-    }
+
       
-        /* validateSubject
-     *
-     **<p> Utility method to validate the Subject
-     * selected.</p>
-     *
-     * @author Wendy Bossons
-     */
+
     public void validateSubject(FacesContext context, 
                           UIComponent toValidate,
                           Object value) {

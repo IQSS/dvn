b99f61231d1cebbb7338db3acc55609a74c82fd7
Stephen Kraffmiller <skraffmiller@hmdc.harvard.edu>
2012-05-25 20:09:51 +0000

    force user to login as dv admin to edit guest book questionnaire

--- dvn-app/trunk/src/DVN-EJB/src/conf/buildupdate_v3_0_v3_1.sql
+++ dvn-app/trunk/src/DVN-EJB/src/conf/buildupdate_v3_0_v3_1.sql
@@ -1,2 +1,3 @@
-ALTER TABLE template
-ADD description character varying(255);
+ALTER TABLE template ADD description character varying(255);
+
+INSERT INTO pagedef ( name, path, role_id, networkrole_id ) VALUES ( 'EditGuestbookQuestionnairePage', '/admin/EditGuestbookQuestionnairePage.xhtml', 3,null );

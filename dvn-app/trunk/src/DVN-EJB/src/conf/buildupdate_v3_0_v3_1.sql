6af427a2d1fd34e060bc51fe61d4ade611947c5b
Stephen Kraffmiller <skraffmiller@hmdc.harvard.edu>
2012-06-06 18:01:06 +0000

    Restrict access to GuestBook Response Data page to DV admins only,

--- dvn-app/trunk/src/DVN-EJB/src/conf/buildupdate_v3_0_v3_1.sql
+++ dvn-app/trunk/src/DVN-EJB/src/conf/buildupdate_v3_0_v3_1.sql
@@ -1,3 +1,5 @@
 ALTER TABLE template ADD description character varying(255);
 
 INSERT INTO pagedef ( name, path, role_id, networkrole_id ) VALUES ( 'EditGuestbookQuestionnairePage', '/admin/EditGuestbookQuestionnairePage.xhtml', 3,null );
+
+INSERT INTO pagedef ( name, path, role_id, networkrole_id ) VALUES ( 'GuestBookResponseDataPage', '/admin/GuestBookResponseDataPage.xhtml', 3,null );

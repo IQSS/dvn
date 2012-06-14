841ff355f86d6c2aa9ef77c3af0503eb8ff2f8a6
Leonid Andreev <leonid@hmdc.harvard.edu>
2012-06-14 20:54:13 +0000

    New copies of EditStudyPage and TemplateFormPage, that support controlled vocabulary on all the non-compound, non-multiple, "html" study fields. Also, the perl scripts that were used to modify the pages.

---
+++ dvn-app/trunk/tools/scripts/script_addcontrolvocab_studyedit.pl
@@ -0,0 +1,163 @@
+#!/usr/bin/perl
+
+$openflag = 0; 
+$replacement_counter = 0; 
+$noprintflag = 0; 
+$panelidcounter = 62; 
+
+$new_vocab_div = qq{
+                    <div jsfc="ice:panelGroup" id="groupPanel%PANELID%">
+                      <ice:inputText id="input_%FIELDNAME%" size="45" value="#{EditStudyPage.metadata.%FIELDNAME%}" 
+                                     required="#{EditStudyPage.studyMap[sfc.%FIELDNAME%].required and EditStudyPage.validateRequired}" 
+                                     rendered="#{empty(EditStudyPage.studyMap[sfc.%FIELDNAME%].templateField.controlledVocabulary)}"
+                                     requiredMessage="This field is required." />
+                      <ice:message styleClass="errorMessage" for="input_%FIELDNAME%"/>                      
+                      <ice:selectOneMenu immediate="false" id="%FIELDNAME%_SelectOne"
+                                          rendered="#{!empty(EditStudyPage.studyMap[sfc.%FIELDNAME%].templateField.controlledVocabulary)}"
+                                          required="#{EditStudyPage.studyMap[sfc.%FIELDNAME%].required and EditStudyPage.validateRequired}" 
+                                           value="#{EditStudyPage.metadata.%FIELDNAME%}"  
+                                           requiredMessage="This field is required." >
+                                <f:selectItem itemLabel="No Value" itemValue=""/>
+                                <f:selectItems value="#{EditStudyPage.studyMap[sfc.%FIELDNAME%].templateField.controlledVocabulary.selectItems}"/>
+                       </ice:selectOneMenu>
+                       <ice:message styleClass="errorMessage" for="%FIELDNAME%_SelectOne"/> 
+                    </div>
+};
+
+
+
+%htmltags = (
+	"relatedMaterial",1,
+	"relatedStudies",1,
+	"otherReferences",1,
+	"unitOfAnalysis",1,
+	"universe",1,
+	"kindOfData",1,
+	"timeMethod",1,
+	"dataCollector",1,
+	"frequencyOfDataCollection",1,
+	"samplingProcedure",1,
+	"deviationsFromSampleDesign",1,
+	"collectionMode",1,
+	"researchInstrument",1,
+	"dataSources",1,
+	"originOfSources",1,
+	"characteristicOfSources",1,
+	"accessToSources",1,
+	"dataCollectionSituation",1,
+	"actionsToMinimizeLoss",1,
+	"controlOperations",1,
+	"weighting",1,
+	"cleaningOperations",1,
+	"studyLevelErrorNotes",1,
+	"responseRate",1,
+	"samplingErrorEstimates",1,
+	"otherDataAppraisal",1,
+	"placeOfAccess",1,
+	"originalArchive",1,
+	"availabilityStatus",1,
+	"collectionSize",1,
+	"studyCompletion",1,
+	"confidentialityDeclaration",1,
+	"specialPermissions",1,
+	"restrictions",1,
+	"contact",1,
+	"citationRequirements",1,
+	"depositorRequirements",1,
+	"conditions",1,
+	"disclaimer",1
+); 
+
+
+while (<>) 
+{
+    if ( $openflag == 1 )
+    {
+	if ($htmlfieldmode == 1) 
+	{
+	    if ( /<div/ )
+	    {
+		$divcounter++; 
+
+		if ($divcounter == 2)
+		{
+		    print STDERR "entering the div mode...\n";
+		    $noprintflag = 1; 
+		    $vocab_entry = $new_vocab_div; 
+			
+		    $vocab_entry =~s/%FIELDNAME%/$field/g;
+		    $ufield = $field; 
+		    $ufield =~s/^(.)/uc($1)/ge;
+		    $vocab_entry =~s/%FIELDNAMEUPPERCASE%/$ufield/g;
+		    
+		    $vocab_entry =~s/metadata\.samplingErrorEstimates/metadata.samplingErrorEstimate/g; 
+
+		    $vocab_entry =~s/%PANELID%/$panelidcounter/g; 
+		    $panelidcounter++; 
+			
+		    print "\t\t" . $vocab_entry; 
+
+		    $replacement_counter++; 
+		}
+
+	    }
+
+	    if ($noprintflag == 0)
+	    {
+		print; 
+	    }
+	    else 
+	    {
+		if (/<\/ice:panelGrid/)
+		{
+		    print; 
+
+		    $htmlfieldmode = 0;
+		    $noprintflag = 0; 
+		}
+	    }
+
+	}
+	else 
+	{
+	    print; 
+	}
+
+	if ( /EditStudyPage.studyMap\[sfc\.([^\]]*)\]/ )
+	{
+	    $field = $1; 
+	    print STDERR $field . "\n";
+	    if ($htmltags{$field} == 1)
+	    {
+		unless ($htmlfieldmode)
+		{
+		    print STDERR "html mode on\n";
+		    $divcounter = 0;
+		    $htmlfieldmode = 1; 
+		}
+	    }
+	    else
+	    {
+		$htmlfieldmode = 0; 
+	    }
+	}
+    }
+    else 
+    {
+	print; 
+	if ( /<ice:panelGrid / )
+	{
+	    $openflag = 1; 
+	    $field = ""; 
+	}
+
+    }
+	    
+    if ( /<\/ice:panelGrid/ )
+    {
+	$openflag = 0; 
+    }
+}
+
+
+print STDERR "\n\nReplaced " . $replacement_counter . " entries.\n";
---
+++ dvn-app/trunk/tools/scripts/script_addcontrolvocab_templateform.pl
@@ -0,0 +1,214 @@
+#!/usr/bin/perl
+
+$openflag = 0; 
+$counter = 0; 
+$noprintflag = 0; 
+
+$select_vocab_column = qq{<ice:column>
+                  <ice:inputText id="input_%FIELDNAME%" size="45" value="#{TemplateFormPage.template.metadata.%FIELDNAME%}" 
+                                 rendered="#{empty(TemplateFormPage.studyMap[sfc.%FIELDNAME%].templateField.controlledVocabulary)}" styleClass="formHtmlEnabled"/>
+                  <ice:message styleClass="errorMessage" for="input_%FIELDNAME%"/>
+
+                  <ice:selectOneMenu immediate="false"   id="selectOne%FIELDNAMEUPPERCASE%" 
+                                     rendered="#{!empty(TemplateFormPage.studyMap[sfc.%FIELDNAME%].templateField.controlledVocabulary)}"                                          
+                                           value="#{TemplateFormPage.template.metadata.%FIELDNAME%}" 
+                                           >
+                            <f:selectItem itemLabel="No Value" itemValue=""/>
+                            <f:selectItems value="#{TemplateFormPage.studyMap[sfc.%FIELDNAME%].templateField.controlledVocabulary.selectItems}"/>
+                 </ice:selectOneMenu>  
+                </ice:column>
+                <ice:column>
+                 <ice:commandLink value="Select Vocabulary"
+                                       action="#{TemplateFormPage.openPopupStandard(TemplateFormPage.studyMap[sfc.%FIELDNAME%].templateField)}"/>
+                  <ice:selectOneMenu immediate="false" partialSubmit="true" 
+                                        value="#{TemplateFormPage.studyMap[sfc.%FIELDNAME%].templateField.fieldInputLevelString}">
+                            <f:attribute name="fieldName" value="%FIELDNAME%" />
+                           <f:selectItems value="#{TemplateFormPage.fieldInputLevelSelectItems}"/>
+                  </ice:selectOneMenu>
+                </ice:column>
+}; 
+
+
+$select_vocab_column_textarea = qq{<ice:column>
+                  <ice:inputTextarea cols="90" rows="4"
+                                     id="input_%FIELDNAME%"
+                                     value="#{TemplateFormPage.template.metadata.%FIELDNAME%}"                                     
+                                     styleClass="formHtmlEnabled">
+                  </ice:inputTextarea>
+
+                  <ice:message styleClass="errorMessage" for="input_%FIELDNAME%"/>
+
+                  <ice:selectOneMenu immediate="false"   id="selectOne%FIELDNAMEUPPERCASE%" 
+                                     rendered="#{!empty(TemplateFormPage.studyMap[sfc.%FIELDNAME%].templateField.controlledVocabulary)}"                                          
+                                           value="#{TemplateFormPage.template.metadata.%FIELDNAME%}" 
+                                           >
+                            <f:selectItem itemLabel="No Value" itemValue=""/>
+                            <f:selectItems value="#{TemplateFormPage.studyMap[sfc.%FIELDNAME%].templateField.controlledVocabulary.selectItems}"/>
+                 </ice:selectOneMenu>  
+                </ice:column>
+                <ice:column>
+                 <ice:commandLink value="Select Vocabulary"
+                                       action="#{TemplateFormPage.openPopupStandard(TemplateFormPage.studyMap[sfc.%FIELDNAME%].templateField)}"/>
+                  <ice:selectOneMenu immediate="false" partialSubmit="true" 
+                                        value="#{TemplateFormPage.studyMap[sfc.%FIELDNAME%].templateField.fieldInputLevelString}">
+                            <f:attribute name="fieldName" value="%FIELDNAME%" />
+                           <f:selectItems value="#{TemplateFormPage.fieldInputLevelSelectItems}"/>
+                  </ice:selectOneMenu>
+                </ice:column>
+}; 
+
+
+%htmltags = (
+	"relatedMaterial",1,
+	"relatedStudies",1,
+	"otherReferences",1,
+	"unitOfAnalysis",1,
+	"universe",1,
+	"kindOfData",1,
+	"timeMethod",1,
+	"dataCollector",1,
+	"frequencyOfDataCollection",1,
+	"samplingProcedure",1,
+	"deviationsFromSampleDesign",1,
+	"collectionMode",1,
+	"researchInstrument",1,
+	"dataSources",1,
+	"originOfSources",1,
+	"characteristicOfSources",1,
+	"accessToSources",1,
+	"dataCollectionSituation",1,
+	"actionsToMinimizeLoss",1,
+	"controlOperations",1,
+	"weighting",1,
+	"cleaningOperations",1,
+	"studyLevelErrorNotes",1,
+	"responseRate",1,
+	"samplingErrorEstimates",1,
+	"otherDataAppraisal",1,
+	"placeOfAccess",1,
+	"originalArchive",1,
+	"availabilityStatus",1,
+	"collectionSize",1,
+	"studyCompletion",1,
+	"confidentialityDeclaration",1,
+	"specialPermissions",1,
+	"restrictions",1,
+	"contact",1,
+	"citationRequirements",1,
+	"depositorRequirements",1,
+	"conditions",1,
+	"disclaimer",1
+); 
+
+%textareatags = (
+	"unitOfAnalysis", 1,
+	"universe", 1,
+	"kindOfData", 1,
+	"samplingProcedure", 1,
+	"deviationsFromSampleDesign", 1,
+	"accessToSources", 1,
+	"confidentialityDeclaration", 1,
+	"specialPermissions", 1,
+	"restrictions", 1,
+	"citationRequirements", 1,
+	"depositorRequirements", 1,
+	"conditions", 1,
+	"disclaimer", 1,
+);
+
+while (<>) 
+{
+    if ( $openflag == 1 )
+    {
+	if ($htmlfieldmode == 1) 
+	{
+	    if ( /<ice:column/ )
+	    {
+		$columncounter++; 
+
+		if ($columncounter == 2)
+		{
+		    print STDERR "entering the column mode...\n";
+		    $noprintflag = 1; 
+
+		    if ( $textareatags{$field} == 1 )
+		    {
+			$vocab_entry = $select_vocab_column_textarea;
+		    } 
+		    else 
+		    {
+			$vocab_entry = $select_vocab_column; 
+		    }
+			
+		    $vocab_entry =~s/%FIELDNAME%/$field/g;
+		    $ufield = $field; 
+		    $ufield =~s/^(.)/uc($1)/ge;
+		    $vocab_entry =~s/%FIELDNAMEUPPERCASE%/$ufield/g;
+		    
+		    $vocab_entry =~s/metadata\.samplingErrorEstimates/metadata.samplingErrorEstimate/g; 
+			
+		    print "\t\t" . $vocab_entry; 
+		    $counter++; 
+		}
+
+	    }
+
+	    if ($noprintflag == 0)
+	    {
+		print; 
+	    }
+	    else 
+	    {
+		if (/<\/ice:panelGrid/)
+		{
+		    print; 
+
+		    $htmlfieldmode = 0;
+		    $noprintflag = 0; 
+		}
+	    }
+
+	}
+	else 
+	{
+	    print; 
+	}
+
+	if ( /TemplateFormPage.studyMap\[sfc\.([^\]]*)\]/ )
+	{
+	    $field = $1; 
+	    print STDERR $field . "\n";
+	    if ($htmltags{$field} == 1)
+	    {
+		unless ($htmlfieldmode)
+		{
+		    print STDERR "html mode on\n";
+		    $columncounter = 0;
+		    $htmlfieldmode = 1; 
+		}
+	    }
+	    else
+	    {
+		$htmlfieldmode = 0; 
+	    }
+	}
+    }
+    else 
+    {
+	print; 
+	if ( /<ice:panelGrid / )
+	{
+	    $openflag = 1; 
+	    $field = ""; 
+	}
+
+    }
+	    
+    if ( /<\/ice:panelGrid/ )
+    {
+	$openflag = 0; 
+    }
+}
+
+print STDERR "\n\nReplaced " . $counter . " entries.\n";
+

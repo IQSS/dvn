8dc8bebcffa50e94f43e537fb1b4803c8ff3ba83
Stephen Kraffmiller <skraffmiller@hmdc.harvard.edu>
2012-08-03 18:06:04 +0000

    Removed automatic insert of guestbook questionnaire records for all dvs that lacked them

--- dvn-app/trunk/src/DVN-EJB/src/conf/buildupdate_v3_1_v3_2.sql
+++ dvn-app/trunk/src/DVN-EJB/src/conf/buildupdate_v3_1_v3_2.sql
@@ -1,14 +1,6 @@
 ALTER TABLE guestbookresponse ADD downloadtype character varying(255);
 ALTER TABLE guestbookresponse ADD sessionId character varying(255);
 
-/*for any dv that lacks a guest book - create one*/
-INSERT INTO guestbookquestionnaire(
-              enabled, firstnamerequired, lastnamerequired, emailrequired, institutionrequired, 
-             positionrequired, vdc_id)
-SELECT true, true, true, true, 
-            false, false, id
-FROM vdc
-WHERE id not in (select vdc_id from guestbookquestionnaire);
 
 /*Allow null vdc_id so that a network guestbook may be created*/
 ALTER TABLE guestbookquestionnaire alter column vdc_id drop not null;

0fdb3f7b844d0b6c90d9f9c198580fceee965004
Michael Heppler <mheppler@hmdc.harvard.edu>
2012-08-29 14:35:49 +0000

    Checking in progress for the OptionsPage redesign. Fixed broken jQuery on the EditBannerFooterPage and removed JavascriptCall on backing bean that didn't seem necessary (or work). [ref 2475]

--- dvn-app/trunk/src/DVN-web/src/edu/harvard/iq/dvn/core/web/admin/EditBannerFooterPage.java
+++ dvn-app/trunk/src/DVN-web/src/edu/harvard/iq/dvn/core/web/admin/EditBannerFooterPage.java
@@ -1,22 +1,23 @@
 /*
-   Copyright (C) 2005-2012, by the President and Fellows of Harvard College.
+ * Dataverse Network - A web application to distribute, share and analyze quantitative data.
+ * Copyright (C) 2007
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses
+ * or write to the Free Software Foundation,Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA 02110-1301 USA
+ */
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-         http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-
-   Dataverse Network - A web application to share, preserve and analyze research data.
-   Developed at the Institute for Quantitative Social Science, Harvard University.
-   Version 3.0.
-*/
 /*
  * EditBannerFooterPage.java
  *
@@ -26,7 +27,6 @@ package edu.harvard.iq.dvn.core.web.admin;
 
 import com.icesoft.faces.component.ext.HtmlInputHidden;
 import com.icesoft.faces.component.ext.HtmlInputTextarea;
-import com.icesoft.faces.context.effects.JavascriptContext;
 import edu.harvard.iq.dvn.core.vdc.VDC;
 import edu.harvard.iq.dvn.core.vdc.VDCNetwork;
 import edu.harvard.iq.dvn.core.vdc.VDCNetworkServiceLocal;
@@ -34,7 +34,6 @@ import edu.harvard.iq.dvn.core.vdc.VDCServiceLocal;
 import edu.harvard.iq.dvn.core.web.common.VDCBaseBean;
 import javax.ejb.EJB;
 import javax.faces.bean.ViewScoped;
-import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -207,8 +206,6 @@ public class EditBannerFooterPage extends VDCBaseBean  implements java.io.Serial
 
     public void setDisplayInFrame(boolean displayInFrame) {
         this.displayInFrame = displayInFrame;
-        // add javascript call on each partial submit to trigger jQuery
-        JavascriptContext.addJavascriptCall(getFacesContext(), "initOpenScholarDataverse();");
     }
 
     public String getParentSite() {
@@ -218,5 +215,7 @@ public class EditBannerFooterPage extends VDCBaseBean  implements java.io.Serial
     public void setParentSite(String parentSite) {
         this.parentSite = parentSite;
     }
-      
+    
+  
 }
+

adb38f12d2639c473e626ed7a26e423e806760f0
sekmiller <skraffmiller@hmdc.harvard.edu>
2013-03-21 13:52:06 -0400

    Update Database to add download count to network stats and template "displayOnCreateDataverse"

--- src/DVN-EJB/src/conf/buildupdate_v3_3_v3_4.sql
+++ src/DVN-EJB/src/conf/buildupdate_v3_3_v3_4.sql
@@ -3,4 +3,8 @@ ALTER TABLE vdcnetwork ADD COLUMN systememail character varying(255);
 
 UPDATE vdcnetwork SET systememail = contactemail WHERE id = 1;
 
-
+Alter TABLE vdcnetworkstats add downloadcount bigint;
+
+Alter TABLE template add displayOnCreateDataverse boolean;
+
+update template set displayOnCreateDataverse = true where vdc_id is null;

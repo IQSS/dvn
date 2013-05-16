5939d79577cb5e057de0d2a88aea482ce9593191
Leonid Andreev <leonid@hmdc.harvard.edu>
2013-05-16 17:53:16 -0400

    fixed a typo in a sql query.

--- src/DVN-EJB/src/java/edu/harvard/iq/dvn/core/study/StudyServiceBean.java
+++ src/DVN-EJB/src/java/edu/harvard/iq/dvn/core/study/StudyServiceBean.java
@@ -2449,7 +2449,7 @@ public class StudyServiceBean implements edu.harvard.iq.dvn.core.study.StudyServ
     public Long getMaxStudyTableId () {
         Long lastId = null; 
         
-        String queryStr = "SELECT * FROM study ORDER by id DESC LIMIT";
+        String queryStr = "SELECT * FROM study ORDER by id DESC LIMIT 1";
         Query query = em.createNativeQuery(queryStr);
         try {
             lastId = (Long)query.getSingleResult();

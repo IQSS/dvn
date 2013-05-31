c0616eee379216d982cbf4560ac1042377f8f4d5
Philip Durbin <philip_durbin@harvard.edu>
2013-05-31 15:27:27 -0400

    FITS search should not currently be part of basic search per #3005

--- src/DVN-EJB/src/java/edu/harvard/iq/dvn/core/index/Indexer.java
+++ src/DVN-EJB/src/java/edu/harvard/iq/dvn/core/index/Indexer.java
@@ -2159,6 +2159,11 @@ public class Indexer implements java.io.Serializable  {
             for (String indexedFieldName : allfields) {
                 //logger.fine("INDEXREADER: "+indexedFieldName);
                 if (!"varName".equals(indexedFieldName)
+                        && !"fits-Column-Comment".equals(indexedFieldName)
+                        && !"fits-Column-Label".equals(indexedFieldName)
+                        && !"fits-Column-UCD".equals(indexedFieldName)
+                        && !"fits-Comment".equals(indexedFieldName)
+                        && !"fits-Extension-Name".equals(indexedFieldName)
                         && !"varLabel".equals(indexedFieldName)
                         && !"varId".equals(indexedFieldName)
                         && !"id".equals(indexedFieldName)

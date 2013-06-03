b97c964d375ed2bc0ea859d8d2c066c9e3838316
gdurand <gdurand@iq.harvard.edu>
2013-06-03 14:59:16 -0400

    added queries to fix prexisiting vdcs advanced search fields (Redmine issue: 2931)

--- src/DVN-EJB/src/conf/buildupdate_v3_4_v3_5.sql
+++ src/DVN-EJB/src/conf/buildupdate_v3_4_v3_5.sql
@@ -24,3 +24,14 @@ Update template set vdcsubnetwork_id = 0;
 INSERT INTO pagedef ( name, path, role_id, networkrole_id ) VALUES ( 'EditSubnetworkPage', '/networkAdmin/EditSubnetworkPage.xhtml', null, 2 );
 
 INSERT INTO pagedef ( name, path, role_id, networkrole_id ) VALUES ( 'ManageSubnetworksPage', '/networkAdmin/ManageSubnetworksPage.xhtml', null, 2 );
+
+-- Data changes for advanced search
+update vdc_adv_search_fields set study_field_id = 99 where study_field_id = 3; -- author to authorName
+update vdc_adv_search_fields set study_field_id = 100 where study_field_id = 5; -- producer to producerName
+update vdc_adv_search_fields set study_field_id = 101 where study_field_id = 15; -- distributor to distributorName
+update vdc_adv_search_fields set study_field_id = 104 where study_field_id = 27; -- keyword to keywordValue
+update vdc_adv_search_fields set study_field_id = 105 where study_field_id = 30; -- topicClassification to topicClassValue
+update vdc_adv_search_fields set study_field_id = 113 where study_field_id = 79; -- publication to publicationCitation
+
+insert into vdc_adv_search_fields (vdc_id, study_field_id)  select distinct vdc_id, 89 from vdc_adv_search_fields where vdc_id not in 
+(select vdc_id from vdc_adv_search_fields where study_field_id=89) -- add publication ReplicationFor

73d5068746cc3b2bf468b11ffda6f4d0c504f8c3
gdurand <gdurand@iq.harvard.edu>
2013-06-06 17:33:32 -0400

    modified VDCNetwork to create VDCNetworkStats at the constructor

--- src/DVN-EJB/src/java/edu/harvard/iq/dvn/core/vdc/VDCNetworkServiceLocal.java
+++ src/DVN-EJB/src/java/edu/harvard/iq/dvn/core/vdc/VDCNetworkServiceLocal.java
@@ -96,8 +96,6 @@ public interface VDCNetworkServiceLocal extends java.io.Serializable  {
 
     public java.util.List<java.lang.Object> getStudyListBySubnetwork(java.lang.Long networkId, boolean released);
 
-    public void create(edu.harvard.iq.dvn.core.vdc.VDCNetworkStats vDCNetworkStats);
-
     public java.lang.Long getTotalFilesBySubnetwork(java.lang.Long networkId, boolean released);
 
     public java.lang.Long getTotalDownloadsBySubnetwork(java.lang.Long networkId, boolean released);

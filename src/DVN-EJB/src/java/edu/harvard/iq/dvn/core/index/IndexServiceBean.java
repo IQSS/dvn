d637da56cbc3ec5d04ed3e950ab5766cd430b0e0
Leonid Andreev <leonid@hmdc.harvard.edu>
2013-06-14 19:28:00 -0400

    TOTALLY FINAL fix for the EJB update of the cross linking table.

--- src/DVN-EJB/src/java/edu/harvard/iq/dvn/core/index/IndexServiceBean.java
+++ src/DVN-EJB/src/java/edu/harvard/iq/dvn/core/index/IndexServiceBean.java
@@ -567,7 +567,7 @@ public class IndexServiceBean implements edu.harvard.iq.dvn.core.index.IndexServ
                             // remove the existing links in the database: 
                             logger.info("study "+i+" no longer cross-linked to any subnetworks.");
                             //linkedStudy.setLinkedToNetworks(null);
-                            studyService.setsetLinkedToNetworks(linkedStudy, null);
+                            linkedStudy = studyService.setLinkedToNetworks(linkedStudy.getId(), null);
                            
                             reindexNecessary = true; 
                         } else if (linkedStudy.isReleased()) {
@@ -585,7 +585,8 @@ public class IndexServiceBean implements edu.harvard.iq.dvn.core.index.IndexServ
                                 
                                 // Update it in the database: 
                                 //linkedStudy.setLinkedToNetworks(newLinkedToNetworks(subNetworks, linkedVdcNetworkMap[i]));
-                                studyService.setsetLinkedToNetworks(linkedStudy, newLinkedToNetworks(subNetworks, linkedVdcNetworkMap[i])); 
+                                linkedStudy = studyService.setLinkedToNetworks(linkedStudy.getId(), newLinkedToNetworks(subNetworks, linkedVdcNetworkMap[i]));
+
                                 //studyService.updateStudy(linkedStudy);
                                 
                                 reindexNecessary = true; 
@@ -624,8 +625,7 @@ public class IndexServiceBean implements edu.harvard.iq.dvn.core.index.IndexServ
                                 // attempt; so that it'll hopefully get caught
                                 // by the next reindexing now. 
                                 //linkedStudy.setLinkedToNetworks(currentCrossLinks);
-                                studyService.setsetLinkedToNetworks(linkedStudy, currentCrossLinks); 
-
+                                linkedStudy = studyService.setLinkedToNetworks(linkedStudy.getId(), currentCrossLinks);
                             }
                         }
                     }

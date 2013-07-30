709eefa2a9057d7a9677cdf8bb86e9ddd85cbf5a
Philip Durbin <philip_durbin@harvard.edu>
2013-07-30 14:56:08 -0400

    exploring getMediaResourceRepresentation, refactor auth

--- src/DVN-web/src/edu/harvard/iq/dvn/api/datadeposit/MediaResourceManagerImpl.java
+++ src/DVN-web/src/edu/harvard/iq/dvn/api/datadeposit/MediaResourceManagerImpl.java
@@ -28,9 +28,11 @@ import edu.harvard.iq.dvn.core.study.StudyFileServiceLocal;
 import edu.harvard.iq.dvn.core.study.StudyServiceLocal;
 import edu.harvard.iq.dvn.core.util.FileUtil;
 import edu.harvard.iq.dvn.core.vdc.VDC;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
@@ -69,8 +71,35 @@ public class MediaResourceManagerImpl implements MediaResourceManager {
     private static final Logger logger = Logger.getLogger(MediaResourceManagerImpl.class.getCanonicalName());
 
     @Override
-    public MediaResource getMediaResourceRepresentation(String string, Map<String, String> map, AuthCredentials ac, SwordConfiguration sc) throws SwordError, SwordServerException, SwordAuthException {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public MediaResource getMediaResourceRepresentation(String uri, Map<String, String> map, AuthCredentials authCredentials, SwordConfiguration swordConfiguration) throws SwordError, SwordServerException, SwordAuthException {
+
+        VDCUser vdcUser = swordAuth.auth(authCredentials);
+        String globalId = extractGlobalID(uri);
+        EditStudyService editStudyService;
+        Context ctx;
+        try {
+            ctx = new InitialContext();
+            editStudyService = (EditStudyService) ctx.lookup("java:comp/env/editStudy");
+        } catch (NamingException ex) {
+            throw new SwordServerException("problem looking up editStudyService");
+        }
+        logger.info("looking up study with globalId " + globalId);
+        Study study = editStudyService.getStudyByGlobalId(globalId);
+        if (study != null) {
+            VDC dv = study.getOwner();
+            if (isAuthorizedToEditStudy(vdcUser, dv, study)) {
+                InputStream fixmeInputStream = new ByteArrayInputStream("FIXME: replace with zip of all study files".getBytes());
+                String contentType = "application/zip";
+                String packaging = UriRegistry.PACKAGE_SIMPLE_ZIP;
+                boolean isPackaged = true;
+                MediaResource mediaResource = new MediaResource(fixmeInputStream, contentType, packaging, isPackaged);
+                return mediaResource;
+            } else {
+                throw new SwordError("user " + vdcUser.getUserName() + " is not authorized to modify study with global ID " + study.getGlobalId());
+            }
+        } else {
+            throw new SwordError("couldn't find study with global ID of " + globalId);
+        }
     }
 
     @Override
@@ -82,6 +111,172 @@ public class MediaResourceManagerImpl implements MediaResourceManager {
 
         VDCUser vdcUser = swordAuth.auth(authCredentials);
 
+        String globalId = extractGlobalID(uri);
+        EditStudyService editStudyService;
+        Context ctx;
+        try {
+            ctx = new InitialContext();
+            editStudyService = (EditStudyService) ctx.lookup("java:comp/env/editStudy");
+        } catch (NamingException ex) {
+            throw new SwordServerException("problem looking up editStudyService");
+        }
+        logger.info("looking up study with globalId " + globalId);
+        Study study = editStudyService.getStudyByGlobalId(globalId);
+        Long studyId;
+        try {
+            studyId = study.getId();
+        } catch (NullPointerException ex) {
+            throw new SwordError("couldn't find study with global ID of " + globalId);
+        }
+        VDC dv = study.getOwner();
+        if (isAuthorizedToEditStudy(vdcUser, dv, study)) {
+            editStudyService.setStudyVersion(studyId);
+            editStudyService.save(dv.getId(), vdcUser.getId());
+
+            EditStudyFilesService editStudyFilesService;
+            try {
+                editStudyFilesService = (EditStudyFilesService) ctx.lookup("java:comp/env/editStudyFiles");
+            } catch (NamingException ex) {
+                throw new SwordServerException("problem looking up editStudyFilesService");
+            }
+            logger.info("deleting current files");
+            editStudyFilesService.setStudyVersionByGlobalId(globalId);
+            List studyFileEditBeans = editStudyFilesService.getCurrentFiles();
+            for (Iterator it = studyFileEditBeans.iterator(); it.hasNext();) {
+                StudyFileEditBean studyFileEditBean = (StudyFileEditBean) it.next();
+                studyFileEditBean.setDeleteFlag(true);
+                logger.info("marked for deletion: " + studyFileEditBean.getStudyFile().getFileName());
+            }
+            editStudyFilesService.save(dv.getId(), vdcUser.getId());
+
+            String tempDirectory = swordConfiguration.getTempDirectory();
+            String uploadDirPath = tempDirectory + File.separator + "uploads" + File.separator + study.getId().toString();
+            File uploadDir = new File(uploadDirPath);
+            if (!uploadDir.exists()) {
+                if (!uploadDir.mkdirs()) {
+                    throw new SwordServerException("couldn't create directory: " + uploadDir.getAbsolutePath());
+                }
+            }
+            String filename = uploadDirPath + File.separator + deposit.getFilename();
+            /**
+             * @todo: fix known "first character is removed from filename" issue
+             * with sword2-server-1.0-classes.jar (SWORD v2 :: Common Server
+             * Library) described at
+             * https://github.com/swordapp/JavaServer2.0/pull/2
+             *
+             * The author of the jar has not published it (
+             * http://www.mail-archive.com/sword-app-tech@lists.sourceforge.net/msg00318.html
+             * ) so we build it ourselves from the "master" branch of the
+             * official repo: https://github.com/swordapp/JavaServer2.0
+             *
+             */
+            logger.info("attempting write to " + filename);
+            ZipInputStream ziStream = new ZipInputStream(deposit.getInputStream());
+            ZipEntry zEntry;
+            FileOutputStream tempOutStream = null;
+            List<StudyFileEditBean> fbList = new ArrayList<StudyFileEditBean>();
+
+            try {
+                // copied from createStudyFilesFromZip in AddFilesPage
+                while ((zEntry = ziStream.getNextEntry()) != null) {
+                    // Note that some zip entries may be directories - we 
+                    // simply skip them:
+                    if (!zEntry.isDirectory()) {
+
+                        String fileEntryName = zEntry.getName();
+                        logger.info("file found: " + fileEntryName);
+
+                        String dirName = null;
+                        String finalFileName = null;
+
+                        int ind = fileEntryName.lastIndexOf('/');
+
+                        if (ind > -1) {
+                            finalFileName = fileEntryName.substring(ind + 1);
+                            if (ind > 0) {
+                                dirName = fileEntryName.substring(0, ind);
+                                dirName = dirName.replace('/', '-');
+                            }
+                        } else {
+                            finalFileName = fileEntryName;
+                        }
+
+
+                        File tempUploadedFile = null;
+                        try {
+                            tempUploadedFile = FileUtil.createTempFile(tempDirectory, finalFileName);
+                        } catch (Exception ex) {
+                            Logger.getLogger(MediaResourceManagerImpl.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+
+                        tempOutStream = new FileOutputStream(tempUploadedFile);
+
+                        byte[] dataBuffer = new byte[8192];
+                        int i = 0;
+
+                        while ((i = ziStream.read(dataBuffer)) > 0) {
+                            tempOutStream.write(dataBuffer, 0, i);
+                            tempOutStream.flush();
+                        }
+
+                        tempOutStream.close();
+
+                        // We now have the unzipped file saved in the upload directory;
+
+                        StudyFileEditBean tempFileBean = new StudyFileEditBean(tempUploadedFile, studyService.generateFileSystemNameSequence(), study);
+                        tempFileBean.setSizeFormatted(tempUploadedFile.length());
+
+                        // And, if this file was in a legit (non-null) directory, 
+                        // we'll use its name as the file category: 
+
+                        if (dirName != null) {
+                            tempFileBean.getFileMetadata().setCategory(dirName);
+                        }
+
+                        fbList.add(tempFileBean);
+
+                    } else {
+                        logger.info("directory found: " + zEntry.getName());
+                    }
+                }
+            } catch (IOException ex) {
+                logger.info("Problem getting zip entry");
+            }
+            StudyFileServiceLocal studyFileService;
+            try {
+                studyFileService = (StudyFileServiceLocal) ctx.lookup("java:comp/env/studyFileService");
+            } catch (NamingException ex) {
+                throw new SwordServerException("problem looking up studyFileService");
+            }
+            studyFileService.addFiles(study.getLatestVersion(), fbList, vdcUser);
+            if (!uploadDir.delete()) {
+                logger.info("Unable to delete " + uploadDir.getAbsolutePath());
+            }
+            /**
+             * @todo: when should we release the study?
+             */
+//        studyService.setReleased(studyId);
+            DepositReceipt fakeDepositReceipt = new DepositReceipt();
+            IRI fakeIri = new IRI("fakeIriFromBinaryDeposit");
+            fakeDepositReceipt.setLocation(fakeIri);
+            fakeDepositReceipt.setEditIRI(fakeIri);
+            return fakeDepositReceipt;
+        } else {
+            throw new SwordError("user " + vdcUser.getUserName() + " is not authorized to modify study with global ID " + study.getGlobalId());
+        }
+    }
+
+    @Override
+    public void deleteMediaResource(String string, AuthCredentials ac, SwordConfiguration sc) throws SwordError, SwordServerException, SwordAuthException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public DepositReceipt addResource(String string, Deposit dpst, AuthCredentials ac, SwordConfiguration sc) throws SwordError, SwordServerException, SwordAuthException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    private String extractGlobalID(String uri) throws SwordServerException, SwordError {
         URI uriReference;
         try {
             uriReference = new URI(uri);
@@ -100,30 +295,16 @@ public class MediaResourceManagerImpl implements MediaResourceManager {
             namingAuthority = parts[6];
             String uniqueLocalName = parts[7];
             globalId = namingAuthority + "/" + uniqueLocalName;
+            return globalId;
         } catch (ArrayIndexOutOfBoundsException ex) {
             throw new SwordError("could not extract global ID from collection URI: " + uri);
         }
+    }
 
-        EditStudyService editStudyService;
-        Context ctx;
-        try {
-            ctx = new InitialContext();
-            editStudyService = (EditStudyService) ctx.lookup("java:comp/env/editStudy");
-        } catch (NamingException ex) {
-            throw new SwordServerException("problem looking up editStudyService");
-        }
-        logger.info("looking up study with globalId " + globalId);
-        Study study = editStudyService.getStudyByGlobalId(globalId);
-        Long studyId;
-        try {
-            studyId = study.getId();
-        } catch (NullPointerException ex) {
-            throw new SwordError("couldn't find study with global ID of " + globalId);
-        }
-        VDC dv = study.getOwner();
+    boolean isAuthorizedToEditStudy(VDCUser vdcUser, VDC dv, Study study) throws SwordError {
         /**
-         * @todo: is there a better way to check if user is authorized to edit a
-         * study?
+         * @todo: we are relying on the fact that the vdcUser has write access
+         * to only one "journal" dataverse. Need a better way
          */
         boolean authorizedToEditStudy = false;
         Collection<Study> ownedStudies = dv.getOwnedStudies();
@@ -133,149 +314,11 @@ public class MediaResourceManagerImpl implements MediaResourceManager {
                 break;
             }
         }
-        if (!authorizedToEditStudy) {
-            throw new SwordError("user " + vdcUser.getUserName() + " is not authorized to modify study with global ID " + globalId);
+        if (authorizedToEditStudy) {
+            return true;
+        } else {
+            throw new SwordError("user " + vdcUser.getUserName() + " is not authorized to modify study with global ID " + study.getGlobalId());
         }
-        editStudyService.setStudyVersion(studyId);
-        editStudyService.save(dv.getId(), vdcUser.getId());
 
-        EditStudyFilesService editStudyFilesService;
-        try {
-            editStudyFilesService = (EditStudyFilesService) ctx.lookup("java:comp/env/editStudyFiles");
-        } catch (NamingException ex) {
-            throw new SwordServerException("problem looking up editStudyFilesService");
-        }
-        logger.info("deleting current files");
-        editStudyFilesService.setStudyVersionByGlobalId(globalId);
-        List studyFileEditBeans = editStudyFilesService.getCurrentFiles();
-        for (Iterator it = studyFileEditBeans.iterator(); it.hasNext();) {
-            StudyFileEditBean studyFileEditBean = (StudyFileEditBean) it.next();
-            studyFileEditBean.setDeleteFlag(true);
-            logger.info("marked for deletion: " + studyFileEditBean.getStudyFile().getFileName());
-        }
-        editStudyFilesService.save(dv.getId(), vdcUser.getId());
-
-        String tempDirectory = swordConfiguration.getTempDirectory();
-        String uploadDirPath = tempDirectory + File.separator + "uploads" + File.separator + study.getId().toString();
-        File uploadDir = new File(uploadDirPath);
-        if (!uploadDir.exists()) {
-            if (!uploadDir.mkdirs()) {
-                throw new SwordServerException("couldn't create directory: " + uploadDir.getAbsolutePath());
-            }
-        }
-        String filename = uploadDirPath + File.separator + deposit.getFilename();
-        /**
-         * @todo: fix known "first character is removed from filename" issue
-         * with sword2-server-1.0-classes.jar (SWORD v2 :: Common Server
-         * Library) described at
-         * https://github.com/swordapp/JavaServer2.0/pull/2
-         *
-         * The author of the jar has not published it (
-         * http://www.mail-archive.com/sword-app-tech@lists.sourceforge.net/msg00318.html
-         * ) so we build it ourselves from the "master" branch of the official
-         * repo: https://github.com/swordapp/JavaServer2.0
-         *
-         */
-        logger.info("attempting write to " + filename);
-        ZipInputStream ziStream = new ZipInputStream(deposit.getInputStream());
-        ZipEntry zEntry;
-        FileOutputStream tempOutStream = null;
-        List<StudyFileEditBean> fbList = new ArrayList<StudyFileEditBean>();
-
-        try {
-            // copied from createStudyFilesFromZip in AddFilesPage
-            while ((zEntry = ziStream.getNextEntry()) != null) {
-                // Note that some zip entries may be directories - we 
-                // simply skip them:
-                if (!zEntry.isDirectory()) {
-
-                    String fileEntryName = zEntry.getName();
-                    logger.info("file found: " + fileEntryName);
-
-                    String dirName = null;
-                    String finalFileName = null;
-
-                    int ind = fileEntryName.lastIndexOf('/');
-
-                    if (ind > -1) {
-                        finalFileName = fileEntryName.substring(ind + 1);
-                        if (ind > 0) {
-                            dirName = fileEntryName.substring(0, ind);
-                            dirName = dirName.replace('/', '-');
-                        }
-                    } else {
-                        finalFileName = fileEntryName;
-                    }
-
-
-                    File tempUploadedFile = null;
-                    try {
-                        tempUploadedFile = FileUtil.createTempFile(tempDirectory, finalFileName);
-                    } catch (Exception ex) {
-                        Logger.getLogger(MediaResourceManagerImpl.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-
-                    tempOutStream = new FileOutputStream(tempUploadedFile);
-
-                    byte[] dataBuffer = new byte[8192];
-                    int i = 0;
-
-                    while ((i = ziStream.read(dataBuffer)) > 0) {
-                        tempOutStream.write(dataBuffer, 0, i);
-                        tempOutStream.flush();
-                    }
-
-                    tempOutStream.close();
-
-                    // We now have the unzipped file saved in the upload directory;
-
-                    StudyFileEditBean tempFileBean = new StudyFileEditBean(tempUploadedFile, studyService.generateFileSystemNameSequence(), study);
-                    tempFileBean.setSizeFormatted(tempUploadedFile.length());
-
-                    // And, if this file was in a legit (non-null) directory, 
-                    // we'll use its name as the file category: 
-
-                    if (dirName != null) {
-                        tempFileBean.getFileMetadata().setCategory(dirName);
-                    }
-
-                    fbList.add(tempFileBean);
-
-                } else {
-                    logger.info("directory found: " + zEntry.getName());
-                }
-            }
-        } catch (IOException ex) {
-            logger.info("Problem getting zip entry");
-        }
-        StudyFileServiceLocal studyFileService;
-        try {
-            studyFileService = (StudyFileServiceLocal) ctx.lookup("java:comp/env/studyFileService");
-        } catch (NamingException ex) {
-            throw new SwordServerException("problem looking up studyFileService");
-        }
-        studyFileService.addFiles(study.getLatestVersion(), fbList, vdcUser);
-        if (!uploadDir.delete()) {
-            logger.info("Unable to delete " + uploadDir.getAbsolutePath());
-        }
-        /**
-         * @todo: when should we release the study?
-         */
-//        studyService.setReleased(studyId);
-        DepositReceipt fakeDepositReceipt = new DepositReceipt();
-        IRI fakeIri = new IRI("fakeIriFromBinaryDeposit");
-        fakeDepositReceipt.setLocation(fakeIri);
-        fakeDepositReceipt.setEditIRI(fakeIri);
-        return fakeDepositReceipt;
-    }
-
-    @Override
-    public void deleteMediaResource(String string, AuthCredentials ac, SwordConfiguration sc) throws SwordError, SwordServerException, SwordAuthException {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public DepositReceipt addResource(String string, Deposit dpst, AuthCredentials ac, SwordConfiguration sc) throws SwordError, SwordServerException, SwordAuthException {
-        throw new UnsupportedOperationException("Not supported yet.");
     }
 }
